5c6a483647b72b2f7ed112371065406e416b29dd
Михаил <n5250@ya.ru>
2016-02-12 11:41:11 +0800

    Добавлен тест направлений, разрешен null для поля Researches.tube_weight

--- directions/tests.py
+++ directions/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+import users.models as users
+import directions.models as dm
 
-# Create your tests here.
+class DirectionsTests(TestCase):
+    fixtures = ['dbBase.json', 'clients.json']
+
+    def test_create_direction(self):
+        self.assertTrue(self.client.login(username='kamshekinaea', password='qazxswedc121153'))
+
+        response = self.client.get('/dashboard/directions')
+
+        self.assertContains(response, 'Категория пациентов')
