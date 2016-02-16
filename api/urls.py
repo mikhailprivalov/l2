a8b47c106cb0ca5e451aad7ae439d36f9347cd0f
Михаил <n5250@ya.ru>
2016-02-16 10:04:48 +0800

    Добавлены комментарии в преставления API. Фикс api sapphire

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^send$', views.send),
-    url(r'^results$', views.results),
+    url(r'^send$', views.send),  #  SYSMEX
+    url(r'^results$', views.results), #  SAPPHIRE
     url(r'^get_order$', views.get_order),
 ]
