44e475513ba9d67fe51e0a0dc5d8f26a3be48333
Привалов М.С <n5250@ya.ru>
2016-07-06 09:36:57 +0800

    Fix тега

--- dashboard/templatetags/version.py
+++ dashboard/templatetags/version.py
@@ -1,10 +1,9 @@
 from django import template
 import time
 import os
-import laboratory
-
+import lis
 register = template.Library()
 
 @register.simple_tag
 def version_date():
-    return "%s %s" % (laboratory.VERSION, time.strftime('%m/%d/%Y', time.gmtime(os.path.getmtime('../.git'))))
+    return "%s %s" % (lis.VERSION, time.strftime('%m/%d/%Y', time.gmtime(os.path.getmtime('../.git'))))
