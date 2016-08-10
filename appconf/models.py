10fc5290a5083e96db3487b3279965bc22aff384
Привалов М.С <n5250@ya.ru>
2016-08-10 14:54:11 +0800

    Упрощение модели

--- appconf/models.py
+++ appconf/models.py
@@ -6,15 +6,17 @@ from django.db import models
 class Setting(models.Model):
     name = models.CharField(max_length=100)
     value = models.CharField(max_length=500)
-    value_type = models.CharField(max_length=1, choices=(('s', 'string'), ('i', 'integer'), ('b', 'boolean')))
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey()
+    value_type = models.CharField(max_length=1, choices=(('s', 'string'), ('i', 'integer'),
+                                                         ('f', 'float'), ('b', 'boolean')))
+
+    def __str__(self):
+        return "%s = %s (%s)" % (self.name, self.value, self.get_value_type_display())
 
     def actual_value(self):
         types = {
             's': str,
             'i': int,
-            'b': (lambda v: v.lower().startswith('t') or v.startswith('1'))
+            'b': (lambda v: v.lower().startswith('t') or v.startswith('1')),
+            'f': float
         }
         return types[self.value_type](self.value)
