5e962c4f039422beb89be4130adf4155b757bcc2
Привалов М.С <n5250@ya.ru>
2016-08-10 14:49:46 +0800

    Reformat

--- appconf/manager.py
+++ appconf/manager.py
@@ -1,6 +1,7 @@
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
+
 class SettingManager(models.Manager):
     def dict_for_object(self, object):
         ct = ContentType.get_for_model(object)
