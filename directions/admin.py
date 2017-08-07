c0a6c22ed4134057feb46023400e72eeb35f7f33
Привалов М.С <n5250@ya.ru>
2017-08-07 14:37:50 +0800

    To admin

--- directions/admin.py
+++ directions/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import IstochnikiFinansirovaniya, Napravleniya, TubesRegistration, Issledovaniya, Result
+from .models import IstochnikiFinansirovaniya, Napravleniya, TubesRegistration, Issledovaniya, Result, FrequencyOfUseResearches
 
 admin.site.register(IstochnikiFinansirovaniya)  # Активация формы добавления и изменения источников финансировнаия
 admin.site.register(Napravleniya)
 admin.site.register(TubesRegistration)
 admin.site.register(Issledovaniya)
 admin.site.register(Result)
+admin.site.register(FrequencyOfUseResearches)
