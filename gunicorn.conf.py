ba9a0fadd70479533d2d1ccf46539452d2b267a0
Привалов М.С <n5250@ya.ru>
2017-09-14 21:01:55 +0800

    bugfix

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,3 +4,8 @@ bind = '127.0.0.1:8000'
 workers = multiprocessing.cpu_count() * 2 + 1
 user = "user"
 timeout = 3600
+
+try:
+    from laboratory.local_gunicorn import *
+except ImportError:
+    pass
