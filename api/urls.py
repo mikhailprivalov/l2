8661ae24f75844a2191434e2fa413627a82a8057
Привалов М.С <n5250@ya.ru>
2017-09-26 09:23:26 +0800

    Remove deprecated

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^send$', views.send),  #  SYSMEX
-    url(r'^results$', views.results), #  SAPPHIRE
-    url(r'^get_order$', views.get_order),
+    url(r'^send$', views.send),
 ]
