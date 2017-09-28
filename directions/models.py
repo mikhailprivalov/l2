3c321401ac4368e86144ecfd4029c4f2db51236d
Привалов М.С <n5250@ya.ru>
2017-09-28 15:33:09 +0800

    Добавлено поле прикреплённого результата

--- directions/models.py
+++ directions/models.py
@@ -174,6 +174,7 @@ class Napravleniya(models.Model):
     history_num = models.CharField(max_length=255, default=None, blank=True, null=True, help_text='Номер истории')
     rmis_case_id = models.CharField(max_length=255, default=None, blank=True, null=True, help_text='РМИС: Номер случая')
     rmis_hosp_id = models.CharField(max_length=255, default=None, blank=True, null=True, help_text='РМИС: ЗОГ')
+    rmis_result_file_id = models.CharField(max_length=255, default=None, blank=True, null=True, help_text='РМИС: ID прикреплённого результата')
     doc_print = models.ForeignKey(DoctorProfile, default=None, blank=True, null=True, related_name="doc_print",
                                   help_text='Профиль, который был использован при печати')
     doc_who_create = models.ForeignKey(DoctorProfile, default=None, blank=True, null=True,
