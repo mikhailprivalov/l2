0d4c4b3c9ab951e49c9842a89deb37261d1c79d2
Привалов М.С <n5250@ya.ru>
2017-10-02 10:56:39 +0800

    improve decorator

--- laboratory/decorators.py
+++ laboratory/decorators.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import user_passes_test
 
 def group_required(*group_names):
     def in_group(u):
+        if "admin" in group_names:
+            return u.is_active and u.is_superuser
         return u.is_active and (u.is_superuser or bool(u.groups.filter(name__in=group_names)))
 
     return user_passes_test(in_group)
