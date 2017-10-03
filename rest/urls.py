d4d413d4de77e8809f27b1ebef5f48576adc559f
Привалов М.С <n5250@ya.ru>
2017-10-03 11:24:56 +0800

    add users serializer and view set

--- rest/urls.py
+++ rest/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 
+from rest.l2_view_sets import UserViewSet
+
 router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
