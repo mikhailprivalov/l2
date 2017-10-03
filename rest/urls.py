c91ce98eaee376fa1879283b708ee7030f40d4bf
Привалов М.С <n5250@ya.ru>
2017-10-03 11:30:28 +0800

    bugfix

--- rest/urls.py
+++ rest/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 
+from rest.l2_serializers import DoctorSerializer, PodrazdeleniyaSerializer
 from rest.l2_view_sets import UserViewSet
 
 router = routers.DefaultRouter()
