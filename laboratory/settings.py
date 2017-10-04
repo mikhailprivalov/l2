6e39ba71a494d16fe63ec930e9fae3a723265f68
Привалов М.С <n5250@ya.ru>
2017-10-04 12:35:31 +0800

    bugfix

--- laboratory/settings.py
+++ laboratory/settings.py
@@ -66,7 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages'
+                'django.contrib.messages.context_processors.messages',
                 'context_processors.utils.card_bases',
             ]
         },
