f8d7f9c5d5bc6b80df30945db9a616baf74fee8b
Привалов М.С <n5250@ya.ru>
2017-10-05 14:46:40 +0800

    bugfix

--- rmis_integration/client.py
+++ rmis_integration/client.py
@@ -631,8 +631,8 @@ class Directions(BaseRequester):
                                                                      "normis": '1'}),
                                          self.main_client.get_addr(
                                              "referral-attachments-ws/rs/referralAttachments/" + direction.rmis_number + "/Результат/Resultat.pdf"))
-            direction.result_rmis_send = True
-            direction.save()
+        direction.result_rmis_send = True
+        direction.save()
         return direction.result_rmis_send
 
     def check_and_send_all(self, stdout: OutputWrapper = None, without_results=False):
