13ef665fc30095aa55acb3295ebda09410b88656
Привалов М.С <n5250@ya.ru>
2017-10-20 00:12:07 +0800

    Реализация команды очистки базы от ненужных карт и синхронизации с РМИС

---
+++ rmis_integration/management/commands/rmis_sync_patients.py
@@ -0,0 +1,29 @@
+import datetime
+from django.core.management import BaseCommand
+
+from clients.models import Card, Individual
+from rmis_integration.client import Client as RC
+
+
+class Command(BaseCommand):
+    help = "Синхронизация пациентов с РМИС"
+
+    def handle(self, *args, **options):
+        self.stdout.write("{:%Y-%m-%d %H:%M}".format(datetime.datetime.now()) + " - TRIGGER")
+
+        objs = Card.objects.filter(napravleniya__isnull=True, is_archive=True)
+        cnt = objs.count()
+        _, cnt = objs.delete()
+        cnt = cnt.get("clients.Card", 0)
+        self.stdout.write("Архивных карт без направлений удалено: {}".format(cnt))
+
+        objs = Individual.objects.filter(card__isnull=True)
+        cnt = objs.count()
+        _, cnt = objs.delete()
+        cnt = cnt.get("clients.Individual", 0)
+        self.stdout.write("Пацтентов без карт удалено: {}".format(cnt))
+
+        c = RC()
+        for card in Card.objects.filter(base__is_rmis=True, is_archive=False):
+            self.stdout.write(c.patients.sync_data(card))
+
