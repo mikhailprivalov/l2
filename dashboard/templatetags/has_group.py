340763e3f73fad6ffd9df7f90717597cc82fc576
Привалов М.С <n5250@ya.ru>
2017-10-19 10:10:06 +0800

    Bugfix

--- dashboard/templatetags/has_group.py
+++ dashboard/templatetags/has_group.py
@@ -6,5 +6,7 @@ register = template.Library()
 
 @register.filter(name='has_group')
 def has_group(user, group_name):
+    if not Group.objects.filter(name=group_name).exists():
+        return False
     group = Group.objects.get(name=group_name)
     return True if group in user.groups.all() else False
