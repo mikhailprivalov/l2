b67caf22a8214f9da2e6926af082f670972700a5
Привалов М.С <n5250@ya.ru>
2017-10-27 11:23:39 +0800

    Fix

--- researches/admin.py
+++ researches/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-from researches.models import Researches, Tubes
+from researches.models import Tubes
 
-admin.site.register(Researches)  # Активация формы редактирования справочника исследований в админке
 admin.site.register(Tubes)  # Активация формы редактирования типов пробирок в админке
