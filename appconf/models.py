4b35cc89f10d5f2bee8ac84c265f0f99d7bc188a
Привалов М.С <n5250@ya.ru>
2017-10-27 11:12:29 +0800

    Fix

--- appconf/models.py
+++ appconf/models.py
@@ -2,7 +2,8 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.template.defaultfilters import truncatechars
 
-class Setting(models.Model):
+
+class SettingR(models.Model):
     name = models.CharField(max_length=100, unique=True, db_index=True)
     value = models.TextField()
     value_type = models.CharField(max_length=1, choices=(('s', 'string'), ('i', 'integer'),
@@ -10,7 +11,8 @@ class Setting(models.Model):
     hide_value_in_list = models.BooleanField(default=False)
 
     def __str__(self):
-        return "%s = %s (%s)" % (self.name, "**скрыто**" if self.hide_value_in_list else truncatechars(self.value, 150), self.get_value_type_display())
+        return "%s = %s (%s)" % (self.name, "**скрыто**" if self.hide_value_in_list else truncatechars(self.value, 150),
+                                 self.get_value_type_display())
 
     def get_value(self):
         val = self.value
@@ -21,3 +23,7 @@ class Setting(models.Model):
             'f': float
         }
         return types[self.value_type](val)
+
+    class Meta:
+        verbose_name = 'Параметр'
+        verbose_name_plural = 'Параметры'
