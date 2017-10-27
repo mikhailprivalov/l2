86d2e39dd48a742acdb2dca4f7aeb48b19b68bbb
Привалов М.С <n5250@ya.ru>
2017-10-27 11:55:31 +0800

    bugfix

--- laboratory/settings.py
+++ laboratory/settings.py
@@ -63,7 +63,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
-        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
