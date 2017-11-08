3a5d992608a2ed5e3096bc7a181504b430411603
Привалов М.С <n5250@ya.ru>
2017-11-08 16:47:29 +0800

    Add lock

--- rmis_integration/management/commands/rmis_resend_range.py
+++ rmis_integration/management/commands/rmis_resend_range.py
@@ -31,13 +31,12 @@ class Command(BaseCommand):
         f = r[0]
         t = r[1]
         c = Client()
-        lock = threading.RLock()
-        lock.acquire()
+        sema.acquire()
         for d in Napravleniya.objects.filter(pk__gte=f, pk__lte=t):
             thread = threading.Thread(target=task, args=(d, self.stdout))
             threads.append(thread)
             thread.start()
-        lock.release()
+        sema.release()
         thread = threading.Thread(target=resend, args=(self.stdout,))
         threads.append(thread)
         thread.start()
