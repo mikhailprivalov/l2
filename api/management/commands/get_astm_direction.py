cd1bf29289947b8e409b19a98d8b4282a383d0ae
Привалов М.С <n5250@ya.ru>
2017-11-08 14:56:03 +0800

    Улучшение функций

--- api/management/commands/get_astm_direction.py
+++ api/management/commands/get_astm_direction.py
@@ -2,7 +2,7 @@ from django.core.management import BaseCommand
 import simplejson as json
 
 from api.models import Analyzer
-from api.to_astm import get_iss_direction
+from api.to_astm import get_astm
 from directions.models import Napravleniya
 
 
@@ -10,8 +10,8 @@ class Command(BaseCommand):
     help = "Получение направления в формате ASTM"
 
     def add_arguments(self, parser):
-        parser.add_argument('direction_id', type=int)
+        parser.add_argument('directions', type=str)
         parser.add_argument('analyzer_id', type=int)
 
     def handle(self, *args, **options):
-        self.stdout.write(json.dumps(get_iss_direction(Napravleniya.objects.get(pk=options['direction_id']), Analyzer.objects.get(pk=options['analyzer_id']), True)))
+        self.stdout.write(json.dumps(get_astm(Napravleniya.objects.get(pk__in=options['direction_id'].split(",")), Analyzer.objects.get(pk=options['analyzer_id']), True)))
--- api/to_astm.py
+++ api/to_astm.py
@@ -1,10 +1,26 @@
+from typing import List
+
+from astm import codec
 from collections import defaultdict
+from django.utils import timezone
 import directions.models as directions
 import directory.models as directory
 import api.models as api
 
 
-def get_iss_direction(direction: directions.Napravleniya, analyzer: api.Analyzer, full=False):
+def get_astm_header() -> list:
+    return ['H', [[None], [None, '&']], None, None, ['1', '2.00'], None, None, None, None, None, None, 'P', '1.00', timezone.now().strftime("%Y%m%d%H%M%S")]
+
+
+def get_leave() -> list:
+    return ['L', 1, 'N']
+
+
+def get_patient() -> list:
+    return ['P', 1, None, None, ['', '', '', '']]
+
+
+def get_iss_direction(direction: directions.Napravleniya, analyzer: api.Analyzer, full=False) -> list:
     r = []
     n = 0
     iss_list = directions.Issledovaniya.objects.filter(napravleniye=direction)
@@ -24,5 +40,13 @@ def get_iss_direction(direction: directions.Napravleniya, analyzer: api.Analyzer
             researches[tube.pk].append(rel.astm_field)
         for tpk in researches:
             n += 1
-            r.append(['O', n, tpk, [[None, x, None, None] for x in researches[tpk]], researches])
+            r.append(['O', n, tpk, None, [[None, x, None, None] for x in researches[tpk]]])
     return r
+
+
+def encode(m) -> list:
+    return codec.encode(m)
+
+
+def get_astm(directions_list: List[directions.Napravleniya], analyzer: api.Analyzer, full=False) -> list:
+    return encode([get_astm_header(), get_patient()] + [get_iss_direction(x, analyzer, full) for x in directions_list] + [get_leave()])
