f189ccb43d5328a651e5567f673d98836970262c
Привалов М.С <n5250@ya.ru>
2017-11-29 13:31:23 +0800

    Day or month picker

--- l2-frontend/src/JournalGetMaterialModal.vue
+++ l2-frontend/src/JournalGetMaterialModal.vue
@@ -7,7 +7,48 @@
           <h4 class="modal-title">Печать отчёта забора биоматериала</h4>
         </div>
         <div class="modal-body">
-
+          <div class="row">
+            <div class="col-xs-4" style="padding-right: 3px;">
+              <select class="selectpicker" data-width="100%" v-model="type">
+                <option value="d">За день</option>
+                <option value="m">За месяц</option>
+              </select>
+            </div>
+            <div class="col-xs-8">
+              <div v-if="type == 'd'">
+                <input type="text" class="form-control no-context" name="dateday-type"/>
+              </div>
+              <div class="row" v-if="type == 'm'">
+                <div class="col-xs-6" style="padding-right: 3px;">
+                  <select class="selectpicker" data-width="100%">
+                    <option value="0">Январь</option>
+                    <option value="1">Февраль</option>
+                    <option value="2">Март</option>
+                    <option value="3">Апрель</option>
+                    <option value="4">Май</option>
+                    <option value="5">Июнь</option>
+                    <option value="6">Июль</option>
+                    <option value="7">Август</option>
+                    <option value="8">Сентябрь</option>
+                    <option value="9">Октябрь</option>
+                    <option value="10">Ноябрь</option>
+                    <option value="11">Декабрь</option>
+                  </select>
+                </div>
+                <div class="col-xs-6">
+                  <input type="number" class="form-control year" id="m-year" value="" min="2015" max="2100"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <div class="row">
+            <div class="col-xs-9"></div>
+            <div class="col-xs-3">
+              <button type="button" class="btn btn-primary-nb btn-blue-nb" data-dismiss="modal">Закрыть</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -18,7 +59,9 @@
   export default {
     name: 'journal-get-material-modal',
     data() {
-      return {}
+      return {
+        type: 'd'
+      }
     },
     methods: {}
   }
