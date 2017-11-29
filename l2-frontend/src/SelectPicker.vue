8bcc338dc905dedfc21838574cbc77e31d868b53
Привалов М.С <n5250@ya.ru>
2017-11-29 14:50:25 +0800

    Fix

--- l2-frontend/src/SelectPicker.vue
+++ l2-frontend/src/SelectPicker.vue
@@ -1,5 +1,5 @@
 <template>
-  <select v-model="name" data-width="100%" class="selectpicker" @change="func">
+  <select v-model="name" data-width="100%" @change="func">
     <option v-bind:value="option.value" v-for="option in options">{{ option.label }}</option>
   </select>
 </template>
@@ -9,8 +9,30 @@
     name: 'select-picker',
     props: ['options', 'name', 'func'],
     updated: function () {
-      console.log(this);
+      console.log(this)
       $(this.$el).selectpicker('refresh')
+    },
+    directives: {
+      selectpicker: {
+        twoWay: true,
+
+        bind() {
+          $(this.el).selectpicker()
+
+          $(this.el).on('change', function (e) {
+            this.set($(this.el).val())
+          }.bind(this))
+        },
+
+        update(newValue) {
+          $(this.el).val(newValue)
+          $(this.el).selectpicker('val', newValue)
+        },
+
+        unbind() {
+          $(this.el).selectpicker('destroy')
+        }
+      }
     }
   }
 </script>
