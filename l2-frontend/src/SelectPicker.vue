ed2e2d18cb2870986759c02859a423996c32ee72
Привалов М.С <n5250@ya.ru>
2017-11-29 14:45:43 +0800

    Test

--- l2-frontend/src/SelectPicker.vue
+++ l2-frontend/src/SelectPicker.vue
@@ -1,6 +1,6 @@
 <template>
   <select v-model="name" data-width="100%" class="selectpicker" @change="func">
-    <option v-bind:value="option.value" v-for="option in options">{{ option.label }}</option>
+    <option v-bind:value="option.value" v-for="option in options">{{ option.label }}2</option>
   </select>
 </template>
 
