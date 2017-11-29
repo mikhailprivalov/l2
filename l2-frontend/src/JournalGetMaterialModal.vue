f29e8a47eb0221cd3010191492a800d46c241fda
Привалов М.С <n5250@ya.ru>
2017-11-29 13:20:57 +0800

    Fix

--- l2-frontend/src/JournalGetMaterialModal.vue
+++ l2-frontend/src/JournalGetMaterialModal.vue
@@ -6,7 +6,7 @@
           <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button>
           <h4 class="modal-title">Test1</h4>
         </div>
-        <div class="modal-body content">
+        <div class="modal-content">
           Test
         </div>
       </div>
