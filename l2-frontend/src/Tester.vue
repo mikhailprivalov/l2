9ea13199fa54da2e00b0a45528638bd7296cb044
Привалов М.С <n5250@ya.ru>
2017-11-29 09:41:06 +0800

    Fix

--- l2-frontend/src/Tester.vue
+++ l2-frontend/src/Tester.vue
@@ -4,6 +4,7 @@
 
 <script>
   export default {
+    name: 'tester',
     data() {
       return {
         v: 'Test world!'
--- l2-frontend/src/main.js
+++ l2-frontend/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import App from './Tester.vue'
+import Tester from './Tester.vue'
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(Tester)
 })
