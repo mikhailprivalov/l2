ea1dad71c7975d129cbb7317d940307df2c1e8b2
Привалов М.С <n5250@ya.ru>
2017-11-30 13:23:29 +0800

    Fix

--- l2-frontend/src/SelectPicker.vue
+++ l2-frontend/src/SelectPicker.vue
@@ -1,6 +1,6 @@
 <template>
   <select v-selectpicker class="selectpicker" data-width="100%" :multiple="multiple" :data-actions-box="actions_box" data-none-selected-text="Ничего не выбрано" data-select-all-text="Выбрать всё"
-          data-deselect-all-text="Отменить весь выбор">
+          data-deselect-all-text="Отменить весь выбор" :disabled="disabled">
     <option v-bind:value="option.value" v-for="option in options">{{ option.label }}</option>
   </select>
 </template>
@@ -30,6 +30,11 @@
     methods: {
       update_val(v) {
         this.func(v)
+      }
+    },
+    computed: {
+      disabled() {
+        return this.multiple && this.options <= 1
       }
     },
     created() {
