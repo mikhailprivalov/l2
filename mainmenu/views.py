64fa98d2e0cc541edbcd8fb04a4b63b9d0fcbd43
Привалов М.С <n5250@ya.ru>
2017-11-30 16:19:04 +0800

    Fix

--- mainmenu/views.py
+++ mainmenu/views.py
@@ -798,5 +798,5 @@ def get_userdata(doc: DoctorProfile):
     return "%s (%s) - %s" % (doc.fio, doc.user.username, doc.podrazdeleniye.title)
 
 
-def ratelimited(request, e):
-    return render(request, 'dashboard/error.html', {"message": "Запрос выполняется слишком часто, попробуйте позднее " + json.dumps(dict(e))})
+def ratelimited(request):
+    return render(request, 'dashboard/error.html', {"message": "Запрос выполняется слишком часто, попробуйте позднее", "update": True})
