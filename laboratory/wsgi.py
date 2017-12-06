e37b94fe98dcedcf7d0531d90ac1d8860a229264
Привалов М.С <n5250@ya.ru>
2017-12-06 17:46:02 +0800

    Bugfix

--- laboratory/wsgi.py
+++ laboratory/wsgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "laboratory.settings")
 
 application = get_wsgi_application()
