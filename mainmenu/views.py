653bdcd11259d23db10ada4a1636d1e5dc4fa77d
Привалов М.С <n5250@ya.ru>
2017-12-08 10:15:21 +0800

    Bugfix

--- mainmenu/views.py
+++ mainmenu/views.py
@@ -167,7 +167,7 @@ def load_logs(request):
             size = int(request.GET["size"])
         for row in obj.order_by("-pk")[offset:size + offset]:
             tmp_object = {"id": row.pk, "user_fio": "" if not row.user else (row.user.get_fio() + ", " + row.user.user.username),
-                          "user_pk": row.user.pk, "key": row.key, "body": row.body, "type": row.get_type_display(),
+                          "user_pk": -1 if not row.user else row.user.pk, "key": row.key, "body": row.body, "type": row.get_type_display(),
                           "time": timezone.localtime(row.time).strftime("%d.%m.%Y %X")}
             result["data"].append(tmp_object)
     else:
@@ -178,7 +178,7 @@ def load_logs(request):
 
         for row in obj.filter(pk__gt=pkgt).order_by("pk"):
             tmp_object = {"id": row.pk, "user_fio": "" if not row.user else (row.user.get_fio() + ", " + row.user.user.username),
-                          "user_pk": row.user.pk, "key": row.key, "body": row.body, "type": row.get_type_display(),
+                          "user_pk": -1 if not row.user else row.user.pk, "key": row.key, "body": row.body, "type": row.get_type_display(),
                           "time": timezone.localtime(row.time).strftime("%d.%m.%Y %X")}
             result["data"].append(tmp_object)
 
