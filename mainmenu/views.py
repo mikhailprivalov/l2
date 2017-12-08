58f91362023df8751591518d42266e302800d302
Привалов М.С <n5250@ya.ru>
2017-12-08 10:37:46 +0800

    Style fix

--- mainmenu/views.py
+++ mainmenu/views.py
@@ -169,7 +169,7 @@ def load_logs(request):
             offset = int(request.GET["offset"])
             size = int(request.GET["size"])
         for row in obj.order_by("-pk")[offset:size + offset]:
-            tmp_object = {"id": row.pk, "user_fio": "" if not row.user else (row.user.get_fio() + ", " + row.user.user.username),
+            tmp_object = {"id": row.pk, "user_fio": "Система" if not row.user else (row.user.get_fio() + ", " + row.user.user.username),
                           "user_pk": "" if not row.user else row.user.pk, "key": row.key, "body": row.body, "type": row.get_type_display(),
                           "time": timezone.localtime(row.time).strftime("%d.%m.%Y %X")}
             result["data"].append(tmp_object)
@@ -180,7 +180,7 @@ def load_logs(request):
             pkgt = int(request.GET["last_n"])
 
         for row in obj.filter(pk__gt=pkgt).order_by("pk"):
-            tmp_object = {"id": row.pk, "user_fio": "" if not row.user else (row.user.get_fio() + ", " + row.user.user.username),
+            tmp_object = {"id": row.pk, "user_fio": "Система" if not row.user else (row.user.get_fio() + ", " + row.user.user.username),
                           "user_pk": "" if not row.user else row.user.pk, "key": row.key, "body": row.body, "type": row.get_type_display(),
                           "time": timezone.localtime(row.time).strftime("%d.%m.%Y %X")}
             result["data"].append(tmp_object)
