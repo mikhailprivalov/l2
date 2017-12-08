6503cefdd1c08562cd4f5293991256beea26eaf0
Привалов М.С <n5250@ya.ru>
2017-12-08 13:18:52 +0800

    Bugfix

--- api/views.py
+++ api/views.py
@@ -190,7 +190,7 @@ def endpoint(request):
                             ok = True
                         if ok:
                             for fraction_rel in q:
-                                for issled in directions.Issledovaniya.objects.filter(napravleniye=direction, research=fraction_rel.fraction.research, doc_confirmation__isnull=True):
+                                for issled in directions.Issledovaniya.objects.filter(napravleniye__in=direction, research=fraction_rel.fraction.research, doc_confirmation__isnull=True):
                                     if directions.Result.objects.filter(issledovaniye=issled, fraction=fraction_rel.fraction).exists():
                                         fraction_result = directions.Result.objects.get(issledovaniye=issled, fraction=fraction_rel.fraction)
                                     else:
