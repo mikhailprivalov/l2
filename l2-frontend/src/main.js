778995922769274a0158526a94cdba3f155f4093
Привалов М.С <n5250@ya.ru>
2017-12-14 17:10:50 +0800

    Get diff

--- l2-frontend/src/main.js
+++ l2-frontend/src/main.js
@@ -9,6 +9,15 @@ new Vue({
   components: {JournalGetMaterialModal, DepartmentsForm},
   created() {
     this.$store.watch((state) => (state.departments.all), (val, oldVal) => {
+      let diff = []
+      for (let row of val) {
+        for (let in_row of oldVal) {
+          if (in_row.pk === row.pk) {
+            diff.push(row)
+            break
+          }
+        }
+      }
       console.log(val, oldVal)
     }, {deep: true})
     this.$store.dispatch('getAllDepartments').then()
