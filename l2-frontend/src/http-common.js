8b646bfaf44eaebea5719a07f10c07f76a12f545
Привалов М.С <n5250@ya.ru>
2017-12-14 15:46:00 +0800

    Fix

--- l2-frontend/src/http-common.js
+++ l2-frontend/src/http-common.js
@@ -2,7 +2,7 @@ import axions from 'axios'
 import * as Cookies from 'es-cookie'
 
 export const HTTP = axions.create({
-  baseURL: window.location.origin,
+  baseURL: window.location.origin + '/api',
   headers: {
     'X-CSRF-Token': Cookies.get('csrftoken')
   }
