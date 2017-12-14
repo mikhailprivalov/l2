4688497c02cfa2a45d6985da57030ea7618afe59
Привалов М.С <n5250@ya.ru>
2017-12-14 15:03:48 +0800

    disable babel

--- l2-frontend/webpack.config.js
+++ l2-frontend/webpack.config.js
@@ -60,11 +60,11 @@ module.exports = {
           }
         }
       },
-      {
+      /*{
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
-      },
+      },*/
       {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
