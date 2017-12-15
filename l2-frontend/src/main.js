6d9aab6542e705055f349a9e364e446dc9eef086
Привалов М.С <n5250@ya.ru>
2017-12-15 15:37:13 +0800

    Debug

--- l2-frontend/src/main.js
+++ l2-frontend/src/main.js
@@ -12,11 +12,14 @@ new Vue({
     let vm = this
     this.$store.watch((state) => (state.departments.all), () => {
       let diff = vm.$store.getters.diff_departments
-      vm.$store.dispatch(action_types.UPDATE_DEPARTMENTS, 'update', diff).then((ok) => {
+      /*vm.$store.dispatch(action_types.UPDATE_DEPARTMENTS, 'update', diff).then((ok) => {
         if (Array.isArray(ok) && ok.length > 0) {
 
         }
-      })
+      })*/
+      if (diff.length > 0 && diff[0].title !== 'test') {
+        diff[0].title = 'test'
+      }
     }, {deep: true})
     this.$store.dispatch(action_types.GET_ALL_DEPARTMENTS).then()
   }
