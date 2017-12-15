d40eb8f9affa076efc84d5e4fe9ec4bc5e7ae616
Привалов М.С <n5250@ya.ru>
2017-12-15 15:49:52 +0800

    Fix

--- l2-frontend/src/store/modules/departments.js
+++ l2-frontend/src/store/modules/departments.js
@@ -42,7 +42,7 @@ const actions = {
     commit(mutation_types.SET_TYPES, {department_types: answer.types})
   },
 
-  [action_types.UPDATE_DEPARTMENTS]: async ({commit, getters}, type_update, to_update) => {
+  async [action_types.UPDATE_DEPARTMENTS]({commit, getters}, type_update, to_update) {
     to_update = to_update || getters.diff_departments
     type_update = type_update || 'update'
     if (to_update.length === 0)
