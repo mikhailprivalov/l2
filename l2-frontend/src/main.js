ccf95c856b0cba2c8f35624e63d51964d7e04e02
Привалов М.С <n5250@ya.ru>
2017-12-15 15:41:53 +0800

    Debug

--- l2-frontend/src/main.js
+++ l2-frontend/src/main.js
@@ -8,18 +8,30 @@ new Vue({
   el: '#app',
   store,
   components: {JournalGetMaterialModal, DepartmentsForm},
+  data: {
+    timeouts: {}
+  },
   created() {
     let vm = this
     this.$store.watch((state) => (state.departments.all), () => {
       let diff = vm.$store.getters.diff_departments
-      /*vm.$store.dispatch(action_types.UPDATE_DEPARTMENTS, 'update', diff).then((ok) => {
+      vm.$store.dispatch(action_types.UPDATE_DEPARTMENTS, 'update', diff).then((ok) => {
         if (Array.isArray(ok) && ok.length > 0) {
-
+          for (let row of ok) {
+            row.updated = true;
+            ((r) => {
+              if (timeouts.hasOwnProperty(r.pk) && timeouts[r.pk] !== null) {
+                clearTimeout(timeouts[r.pk])
+                timeouts[r.pk] = null
+              }
+              setTimeout(() => {
+                r.updated = false
+                timeouts[r.pk] = null
+              }, 2000)
+            })(row)
+          }
         }
-      })*/
-      if (diff.length > 0 && diff[0].title !== 'test') {
-        diff[0].title = 'test'
-      }
+      })
     }, {deep: true})
     this.$store.dispatch(action_types.GET_ALL_DEPARTMENTS).then()
   }
