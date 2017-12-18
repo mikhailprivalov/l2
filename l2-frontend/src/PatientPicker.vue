9493ef8290dc43b8794a4cd9d4884bd88a6ad522
Привалов М.С <n5250@ya.ru>
2017-12-18 14:51:01 +0800

    Fix

--- l2-frontend/src/PatientPicker.vue
+++ l2-frontend/src/PatientPicker.vue
@@ -17,12 +17,12 @@
     name: 'patient-picker',
     data() {
       return {
-        base: JSON.parse(JSON.stringify(this.$store.getters.bases()[0].pk))
+        base: JSON.parse(JSON.stringify(this.$store.getters.bases[0].pk))
       }
     },
     computed: {
       bases() {
-        return this.$store.getters.bases()
+        return this.$store.getters.bases
       }
     }
   }
