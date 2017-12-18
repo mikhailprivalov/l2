341691f6f185cedf75207cc63f46017d440c5d3f
Привалов М.С <n5250@ya.ru>
2017-12-18 14:20:19 +0800

    Add select

--- l2-frontend/src/PatientPicker.vue
+++ l2-frontend/src/PatientPicker.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
+
+    <div class="input-group">
+      <input type="text" class="form-control" id="search-field" name="search-field" placeholder="Введите запрос">
+      <span class="input-group-btn">
+        <select class="btn">
+          <option>A</option>
+          <option>B</option>
+          <option>C</option>
+        </select>
+      </span>
+      <span class="input-group-btn"><button class="btn btn-blue-nb" type="button">Поиск</button></span>
+    </div>
     // TODO: Patient Picker
   </div>
 </template>
@@ -11,5 +23,11 @@
 </script>
 
 <style scoped>
-
+  .input-group-btn select {
+    border-color: #ccc;
+    margin-top: 0;
+    margin-bottom: 0;
+    padding-top: 7px;
+    padding-bottom: 7px;
+  }
 </style>
