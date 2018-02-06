576cad15430bb74f9ef539413c52291e034567e5
Mikhail Privalov <n5250@ya.ru>
2018-02-06 13:01:30 +0800

    2.7.0.1     ! Совместимость со старой версией плагина для chrome восстановлена

--- laboratory/urls.py
+++ laboratory/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
                   path('direction/researches/update/history/print', directions.views.print_history),
                   path('directory/', include('directory.urls')),
                   path('researches/get/one', researches.views.researches_get_one),
+                  path('dashboard/', include('mainmenu.urls')),
                   path('mainmenu/', include('mainmenu.urls')),
                   path('tubes/get', receivematerial.views.tubes_get),
                   path('results/', include('results.urls')),
