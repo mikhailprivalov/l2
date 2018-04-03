4cb9786367371697d3765688a8059598e0d6ebe9
Mikhail Privalov <n5250@ya.ru>
2018-04-03 21:29:30 +0800

    2.16     + Статистика

---
+++ mq/management/commands/mq_send_range.py
@@ -0,0 +1,20 @@
+from django.core.management import BaseCommand
+
+from mq.publisher import mq_send
+
+
+class Command(BaseCommand):
+    help = "Пакетная отправка объектов в MQ"
+
+    def add_arguments(self, parser):
+        parser.add_argument('model', type=str)
+        parser.add_argument('pk_range', type=str)
+
+    def handle(self, *args, **options):
+        n = options['model']
+        r = options['directions_range'].split('-')
+        f = int(r[0])
+        t = int(r[1])
+        for pk in range(f, t + 1):
+            mq_send("update", n, str(pk))
+            self.stdout.write("{} {} -> MQ".format(n, pk))
