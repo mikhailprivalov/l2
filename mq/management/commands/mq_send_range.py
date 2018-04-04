a022bbce1a966bbcb2b3ce5b27e302b95c495259
Mikhail Privalov <n5250@ya.ru>
2018-04-04 12:52:59 +0800

    2.16     + Статистика

--- mq/management/commands/mq_send_range.py
+++ mq/management/commands/mq_send_range.py
@@ -12,9 +12,20 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         n = options['model']
-        r = options['pk_range'].split('-')
-        f = int(r[0])
-        t = int(r[1])
+        r = options['pk_range']
+        f = 0
+        if r != "*":
+            r = r.split('-')
+            f = int(r[0])
+            t = int(r[1])
+        else:
+            from django.apps import apps
+            na = n.split(".")
+            m = apps.get_model(app_label=na[0], model_name=na[2])
+            t = 0
+            a = m.objects.all().order_by("-pk").first()
+            if a:
+                t = a.pk
         for pk in range(f, t + 1):
             mq_send("updated", n, str(pk))
             self.stdout.write("{} {} -> MQ".format(n, pk))
