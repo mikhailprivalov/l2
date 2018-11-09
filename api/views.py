53e4469efa66335f9b91761afa9a6baf4d9c764b
Mikhail Privalov <n5250@ya.ru>
2018-11-09 10:42:57 +0800

    V

--- api/views.py
+++ api/views.py
@@ -1717,8 +1717,9 @@ def flg(request):
             doc = None
             gi = None
             for u in users.DoctorProfile.objects.filter(podrazdeleniye=i.research.podrazdeleniye):
-                if u.get_fio().lower() == doc_f:
+                if u.get_fio().lower() == doc_f or (not doc and u.has_group('Врач параклиники')):
                     doc = u
+
                 gis = ParaclinicInputField.objects.filter(group__research=i.research, group__title="Заключение")
                 if gis.exists():
                     gi = gis[0]
