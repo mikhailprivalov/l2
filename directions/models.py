4c5de94bf285f4d3803398e9011b49d839bc108e
Sergei Kasianenko <monitor84@list.ru>
2019-04-22 21:59:42 +0800

    Fix

--- directions/models.py
+++ directions/models.py
@@ -475,11 +475,9 @@ class Napravleniya(models.Model):
                     # получить по прайсу и услуге: текущую цену
                     research_coast = contracts.PriceCoast.get_coast_from_price(research.pk, price_obj)
 
-                    discount_end = 0
+                    discount_end = discount
                     if research.prior_discount:
                         discount_end = research.def_discount
-                    else:
-                        discount_end = discount
 
                     research_discount = discount_end * -1
                     research_howmany = count
