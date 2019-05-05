dbae241f2c39df7bce231f462ec2aaa3fedba01c
Mikhail Privalov <n5250@ya.ru>
2019-05-05 20:13:03 +0300

    Replace or append modal

---
+++ l2-frontend/src/ReplaceAppendModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="custom-view-wrapper">
+    <div class="dg-content">Шаблон "{{messageBody}}".</div>
+    <div class="dg-content">Выберите вариант действий:</div><br/>
+
+    <button :options="options" @click="handleShare('replace')" class="btn btn-blue-nb">Заменить значения</button>
+    <button :options="options" @click="handleShare('append')" class="btn btn-blue-nb">Дописать</button>
+    <button :options="options" @click="handleDismiss()" class="btn btn-blue-nb">Отмена</button>
+  </div>
+</template>
+
+<script>
+  import DialogMixin from 'vuejs-dialog/dist/vuejs-dialog-mixin.min'
+
+  export default {
+    mixins: [DialogMixin],
+    methods: {
+      handleShare(platform) {
+        this.proceed(platform)
+      },
+      handleDismiss() {
+        this.cancel()
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  button {
+    width: 100%;
+    margin-bottom: 10px;
+    float: none;
+  }
+</style>
