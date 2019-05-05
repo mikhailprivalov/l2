88767d38b1fa333c5b1bb4b09d24b497a3e8958c
Mikhail Privalov <n5250@ya.ru>
2019-05-05 20:16:18 +0300

    Fix

--- l2-frontend/src/ReplaceAppendModal.vue
+++ l2-frontend/src/ReplaceAppendModal.vue
@@ -3,9 +3,9 @@
     <div class="dg-content">Шаблон "{{messageBody}}".</div>
     <div class="dg-content">Выберите вариант действий:</div><br/>
 
-    <button :options="options" @click="handle('replace')" class="btn btn-blue-nb">Заменить значения</button>
-    <button :options="options" @click="handle('append')" class="btn btn-blue-nb">Дописать</button>
-    <button :options="options" @click="handleDismiss()" class="btn btn-blue-nb">Отмена</button>
+    <button @click="handle('replace')" class="btn btn-blue-nb">Заменить значения</button>
+    <button @click="handle('append')" class="btn btn-blue-nb">Дописать</button>
+    <button @click="handleDismiss()" class="btn btn-blue-nb">Отмена</button>
   </div>
 </template>
 
