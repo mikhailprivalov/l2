361753eb16fee7646669aa76308273727ca67d4f
Sergei Kasianenko <monitor84@list.ru>
2019-08-08 20:51:18 +0800

    Fix

--- laboratory/settings.py
+++ laboratory/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = (
     'forms',
     'contracts',
     'lq',
+    'treatment',
 )
 
 MIDDLEWARE = [
--- treatment/admin.py
+++ treatment/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Medicament
 
-# Register your models here.
+
+class ResMedicament(admin.ModelAdmin):
+    list_display = ('trade_name','international_name','pharm_group','number_registration',)
+    list_display_links = ('trade_name','international_name',)
+    list_filter = ('pharm_group',)
+    search_fields = ('trade_name','international_name','pharm_group',)
+
+admin.site.register(Medicament, ResMedicament)
