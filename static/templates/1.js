63c9b4736ad24cc2c724e64846072507b69cf96c
Sergei Kasianenko <monitor84@list.ru>
2019-09-24 17:02:50 +0800

    Fix

---
+++ static/templates/1.js
@@ -0,0 +1,28 @@
+let k = -1;
+function genresult(th){
+    k = pk = $(th).attr("pk");
+    $("#res-modal h4").text($(th).attr("ftitle"));
+    $("#res-modal #res-modal-dir").text($("#dir_num a").text());
+    $("#hiddenresmodal").hide();
+    $("[name='result-v']:checked").prop("checked", false);
+    $("[name=kpol]").val("");
+    $("[name=kkor]").val("");
+    $("#res-modal").modal();
+}
+
+function updateresmodal(th){
+    val = $(th).val();
+    $("#hiddenresmodal").hide();
+    if(val == "1"){
+        $("#hiddenresmodal").show();
+    }
+}
+const types_directory = {"0": "Отриц.", "1": "Пол.", "2": "Сомнит."};
+function saveresmodal(){
+    checked = $("[name='result-v']:checked");
+    result = types_directory[checked.val()];
+    if(checked.val() == "1"){
+        result += " (КП core {0}; КП NS {1})".f($("[name=kpol]").val(),$("[name=kkor]").val());
+    }
+    $("[data-pk="+k+"]").val(result);
+}
