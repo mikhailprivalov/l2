43cc22505a8a05b4534baef26f758ecf6367af2c
Sergei Kasianenko <monitor84@list.ru>
2019-11-19 11:25:11 +0800

    Fix pacs. Helper - more len

--- api/dicom.py
+++ api/dicom.py
@@ -1,8 +1,16 @@
 from orthanc_rest_client import Orthanc
 from directions.models import Issledovaniya
 from laboratory.settings import DICOM_SEARCH_TAGS, DICOM_SERVER
-from checkdigit import isbn
+from functools import reduce
 
+
+def sum(x, y):
+    return int(x) + int(y)
+
+def check_sum_ean13(num):
+    evensum = reduce(sum, num[-2::-2])
+    oddsum = reduce(sum, num[-1::-2])
+    return (10 - ((evensum + oddsum * 3) % 10)) % 10
 
 def search_dicom_study(direction=None):
     #на входе направление тип integer
@@ -12,10 +20,10 @@ def search_dicom_study(direction=None):
         if dicom_study and dicom_study['study_instance_uid']:
             return f'{DICOM_SERVER}/osimis-viewer/app/index.html?study={dicom_study["study_instance_uid"]}'
         else:
-            num_dir = str(direction + 460000000000)
-            res = isbn.isbn13calculate(num_dir)
-            ean13_dir = num_dir + res
             str_dir = str(direction)
+            ean13_dir = str(direction + 460000000000)
+            check_sum = check_sum_ean13(ean13_dir)
+            ean13_dir = f'{ean13_dir}{check_sum}'
 
             orthanc = Orthanc(DICOM_SERVER)
             for tag in DICOM_SEARCH_TAGS:
