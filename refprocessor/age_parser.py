53f6983d67acbe4a9cd77f9dd3dd51c1d0bc66d7
Mikhail Privalov <n5250@ya.ru>
2020-04-16 01:20:58 +0300

    Age parser V1

---
+++ refprocessor/age_parser.py
@@ -0,0 +1,293 @@
+import re
+from typing import Tuple, Union, List
+
+SIGN_GT = ">"
+SIGN_GTE = ">="
+SIGN_LT = "<"
+SIGN_LTE = "<="
+
+# Знаки больше
+SIGNS_GT = (
+    ">",
+    "&gt;",
+    "старше",
+    "больше",
+    "более",
+)
+# Знаки больше или равно
+SIGNS_GTE = (
+    ">=",
+    "≥",
+    "&ge;",
+    "от",
+    "с",
+)
+
+# Знаки меньше
+SIGNS_LT = (
+    "<",
+    "&lt;",
+    "младше",
+    "меньше",
+    "менее",
+)
+# Знаки меньше или равно
+SIGNS_LTE = (
+    "<=",
+    "≤",
+    "&le;",
+    "до",
+)
+
+SIGNS_ORIG_TO_SIGN = (
+    (SIGNS_GT, SIGN_GT),
+    (SIGNS_GTE, SIGN_GTE),
+    (SIGNS_LT, SIGN_LT),
+    (SIGNS_LTE, SIGN_LTE),
+)
+
+
+class Age:
+    MODE_STRICT = ")"
+    MODE_NON_STRICT = "]"
+
+    def __init__(self, value: Union[int, float, Tuple[Union[int, float], str]], mode=MODE_NON_STRICT):
+        if isinstance(value, tuple):
+            self.value = value[0]
+            self.mode = value[1]
+        else:
+            self.value = value
+            self.mode = mode
+
+    def __eq__(self, other: 'Age'):
+        return self.value == other.value and self.mode == other.mode
+
+    def __str__(self):
+        return f"{self.mode}{self.value}"
+
+
+class AgeRange:
+    RANGE_TEMPLATE = r"^(от )?(\d+)( )?([\w.]+)* ([-–] |до )(\d+)( )?([\w.]+)*$"
+
+    def __init__(self, age_from: Union[Age, int, float, Tuple[Union[int, float], str]], age_to: Union[Age, int, float]):
+        if isinstance(age_from, Age):
+            self.age_from = age_from
+        else:
+            self.age_from = Age(age_from)
+
+        if isinstance(age_to, Age):
+            self.age_to = age_to
+        else:
+            self.age_to = Age(age_to)
+
+    def in_range(self, age: int):
+        if self.age_from.mode == Age.MODE_STRICT and age <= self.age_from.value:
+            return False
+        if self.age_from.mode == Age.MODE_NON_STRICT and age < self.age_from.value:
+            return False
+
+        if self.age_to.mode == Age.MODE_STRICT and age >= self.age_to.value:
+            return False
+        if self.age_to.mode == Age.MODE_NON_STRICT and age > self.age_to.value:
+            return False
+
+        return True
+
+    def __eq__(self, other: 'AgeRange'):
+        return self.age_from == other.age_from and self.age_to == other.age_to
+
+    def __str__(self):
+        return f"{self.age_from} – {self.age_to}"
+
+
+class AgeRight:
+    MODE_DAY = "day"
+    MODE_MONTH = "month"
+    MODE_YEAR = "year"
+    MODE_UNKNOW = "unknow"
+
+    DAY_ORIGS = (
+        "дней",
+        "день",
+        "дня",
+        "д",
+        "д.",
+    )
+
+    MONTH_ORIGS = (
+        "месяц",
+        "месяцев",
+        "месяца",
+        "м",
+        "м.",
+    )
+
+    YEAR_ORIGS = (
+        "год",
+        "года",
+        "лет",
+        "г",
+        "г.",
+        "л",
+        "л.",
+    )
+
+    MODES_FROM_ORIGS = (
+        (DAY_ORIGS, MODE_DAY),
+        (MONTH_ORIGS, MODE_MONTH),
+        (YEAR_ORIGS, MODE_YEAR),
+    )
+
+    def __init__(self, orig_str: str):
+        orig_str = orig_str.strip().lower()
+        if AgeRight.check_is_all(orig_str):
+            self.age_range = AgeRange(0, float('inf'))
+            self.mode = AgeRight.MODE_YEAR
+            return
+
+        constant_simple_year = AgeRight.check_is_constant_simple_year(orig_str)
+        if constant_simple_year:
+            self.age_range = AgeRange(constant_simple_year, constant_simple_year)
+            self.mode = AgeRight.MODE_YEAR
+            return
+
+        simple_year_age = AgeRight.check_is_simple_year_range(orig_str)
+        if simple_year_age:
+            self.age_range = AgeRange(int(simple_year_age.group(1)), int(simple_year_age.group(2)))
+            self.mode = AgeRight.MODE_YEAR
+            return
+
+        orig_str = re.sub(' +', ' ', orig_str)
+
+        constant_age_with_mode = AgeRight.check_is_constant_age_with_mode(orig_str)
+        if constant_age_with_mode:
+            self.age_range = AgeRange(constant_age_with_mode[0], constant_age_with_mode[0])
+            self.mode = constant_age_with_mode[1]
+            return
+
+        constant_age_with_sign = AgeRight.check_is_constant_age_with_sign_and_optional_mode(orig_str)
+        if constant_age_with_sign:
+            self.age_range = AgeRange(constant_age_with_sign[1], constant_age_with_sign[2])
+            self.mode = constant_age_with_sign[0]
+            return
+
+        full_range = AgeRight.check_is_full_range(orig_str)
+        if full_range:
+            self.age_range = AgeRange(full_range[1], full_range[2])
+            self.mode = full_range[0]
+            return
+
+        self.age_range = AgeRange(Age(0, Age.MODE_STRICT), Age(0, Age.MODE_STRICT))
+        self.mode = AgeRight.MODE_UNKNOW
+
+    def test(self, age: List[int]) -> bool:
+        if self.mode == AgeRight.MODE_UNKNOW:
+            return False
+
+        if self.mode == AgeRight.MODE_DAY:
+            if age[1] > 0 or age[2] > 0:
+                return False
+            age_var = age[0]
+        elif self.mode == AgeRight.MODE_MONTH:
+            if age[2] > 0:
+                return False
+            age_var = age[1]
+        else:
+            age_var = age[2]
+
+        return self.age_range.in_range(age_var)
+
+    @staticmethod
+    def check_is_all(orig_str: str) -> bool:
+        return orig_str in ["все", ""]
+
+    @staticmethod
+    def check_is_simple_year_range(orig_str: str):
+        orig_str = orig_str.replace(" ", "")
+        return re.match(r"^(\d+)-(\d+)$", orig_str)
+
+    @staticmethod
+    def check_is_constant_simple_year(orig_str: str) -> Union[bool, int]:
+        orig_str = orig_str.replace(" ", "")
+        if not orig_str.isdigit():
+            return False
+        return int(orig_str)
+
+    @staticmethod
+    def check_is_constant_age_with_mode(orig_str: str) -> Union[bool, Tuple[int, str]]:
+        matched = re.match(r"^(\d+) ([\w.]+)$", orig_str)
+        if not matched:
+            return False
+        value = int(matched.group(1))
+        mode_orig = matched.group(2).lower()
+
+        mode = AgeRight.get_mode_by_string(mode_orig)
+        if mode != AgeRight.MODE_UNKNOW:
+            return value, mode
+
+        return False
+
+    @staticmethod
+    def check_is_constant_age_with_sign_and_optional_mode(orig_str: str) -> Union[bool, Tuple[str, Age, Age]]:
+        matched = re.match(r"^([\w|<>+≤≥&;=]+) (\d+)( )?(\w+)?$", orig_str)
+
+        if matched:
+            g = list(matched.groups())
+            if g[3]:
+                mode = AgeRight.get_mode_by_string(g[3])
+                if mode == AgeRight.MODE_UNKNOW:
+                    return False
+            else:
+                mode = AgeRight.MODE_YEAR
+
+            sign_orig = g[0]
+            sign = AgeRight.get_sign_by_string(sign_orig)
+            if not sign:
+                return False
+
+            value = int(g[1])
+
+            if sign == SIGN_GT:
+                return mode, Age(value=value, mode=Age.MODE_STRICT), Age(value=float('inf'))
+
+            if sign == SIGN_GTE:
+                return mode, Age(value=value), Age(value=float('inf'))
+
+            if sign == SIGN_LT:
+                return mode, Age(value=0), Age(value=value, mode=Age.MODE_STRICT)
+
+            if sign == SIGN_LTE:
+                return mode, Age(value=0), Age(value=value)
+
+        return False
+
+    @staticmethod
+    def check_is_full_range(orig_str: str) -> Union[bool, Tuple[str, int, int]]:
+        matched = re.match(AgeRange.RANGE_TEMPLATE, orig_str)
+
+        if matched:
+            g = list(matched.groups())
+            if g[3] or g[7]:
+                mode = AgeRight.get_mode_by_string(g[3] or g[7])
+                if mode == AgeRight.MODE_UNKNOW:
+                    return False
+            else:
+                mode = AgeRight.MODE_YEAR
+
+            return mode, int(g[1]), int(g[5])
+
+        return False
+
+    @staticmethod
+    def get_mode_by_string(s: str) -> Union[bool, str]:
+        for mode_origs, mode in AgeRight.MODES_FROM_ORIGS:
+            if s in mode_origs:
+                return mode
+        return AgeRight.MODE_UNKNOW
+
+    @staticmethod
+    def get_sign_by_string(s: str) -> Union[None, str]:
+        for signs in SIGNS_ORIG_TO_SIGN:
+            if s in signs[0]:
+                return signs[1]
+        return None
---
+++ refprocessor/test.py
@@ -0,0 +1,195 @@
+import unittest
+
+from refprocessor.age_parser import AgeRight, AgeRange, SIGN_GT, SIGN_GTE, SIGN_LT, SIGN_LTE
+
+
+class CheckAgeRightsFunctions(unittest.TestCase):
+    """Проверка утилитарных функций"""
+
+    def test_detect_mode(self):
+        """Проверка на обнаружение режима возраста"""
+
+        modes = (
+            ("invalid", AgeRight.MODE_UNKNOW),
+            ("дней", AgeRight.MODE_DAY),
+            ("день", AgeRight.MODE_DAY),
+            ("дня", AgeRight.MODE_DAY),
+            ("д", AgeRight.MODE_DAY),
+            ("д.", AgeRight.MODE_DAY),
+            ("месяц", AgeRight.MODE_MONTH),
+            ("месяцев", AgeRight.MODE_MONTH),
+            ("месяца", AgeRight.MODE_MONTH),
+            ("м", AgeRight.MODE_MONTH),
+            ("м.", AgeRight.MODE_MONTH),
+            ("год", AgeRight.MODE_YEAR),
+            ("года", AgeRight.MODE_YEAR),
+            ("лет", AgeRight.MODE_YEAR),
+            ("г", AgeRight.MODE_YEAR),
+            ("г.", AgeRight.MODE_YEAR),
+            ("л", AgeRight.MODE_YEAR),
+            ("л.", AgeRight.MODE_YEAR),
+        )
+
+        for m in modes:
+            mode = AgeRight.get_mode_by_string(m[0])
+            self.assertEqual(m[1], mode, f"Режим '{mode}' для '{m[0]}' должен быть '{m[1]}'")
+
+    def test_detect_sign(self):
+        """Проверка на обнаружение знака"""
+
+        signs = (
+            ("invalid", None),
+
+            (">", SIGN_GT),
+            ("&gt;", SIGN_GT),
+            ("старше", SIGN_GT),
+            ("больше", SIGN_GT),
+            ("более", SIGN_GT),
+
+            (">=", SIGN_GTE),
+            ("≥", SIGN_GTE),
+            ("&ge;", SIGN_GTE),
+            ("от", SIGN_GTE),
+            ("с", SIGN_GTE),
+
+            ("<", SIGN_LT),
+            ("&lt;", SIGN_LT),
+            ("младше", SIGN_LT),
+            ("меньше", SIGN_LT),
+            ("менее", SIGN_LT),
+
+            ("<=", SIGN_LTE),
+            ("≤", SIGN_LTE),
+            ("&le;", SIGN_LTE),
+            ("до", SIGN_LTE),
+        )
+
+        for s in signs:
+            sign = AgeRight.get_sign_by_string(s[0])
+            self.assertEqual(s[1], sign, f"Знак '{sign}' должен быть '{s[1]}'")
+
+
+class ParseAgeRights(unittest.TestCase):
+    """Проверка разбора строки возраста"""
+
+    def test_default_year_constant(self):
+        """Возраст в виде простого числа определяется корректно"""
+
+        right = AgeRight("5 ")
+        correct_range = AgeRange(5, 5)
+        self.assertEqual(correct_range, right.age_range, f"Диапазон должен быть '{correct_range}'")
+        self.assertEqual(AgeRight.MODE_YEAR, right.mode, "Режим должен быть 'year'")
+
+    def test_all_ages(self):
+        """Все возраста определяются корректно"""
+
+        strs = ["все", "Все", " все ", ""]
+
+        correct_range = AgeRange(0, float('inf'))
+
+        for s in strs:
+            right = AgeRight(s)
+            self.assertEqual(correct_range, right.age_range, f"Диапазон должен быть '{correct_range}'")
+            self.assertEqual(AgeRight.MODE_YEAR, right.mode, "Режим должен быть 'year'")
+
+    def test_default_year_ages(self):
+        """Возраст в виде простого диапазона определяется корректно"""
+
+        strs = ["1-10", "1 - 10", " 1 - 10 "]
+
+        correct_range = AgeRange(1, 10)
+
+        for s in strs:
+            right = AgeRight(s)
+            self.assertEqual(correct_range, right.age_range, f"Диапазон должен быть '{correct_range}'")
+            self.assertEqual(AgeRight.MODE_YEAR, right.mode, "Режим должен быть 'year'")
+
+    def test_constant_age_with_mode(self):
+        """Возраст в виде константного числа с режимом определяется корректно"""
+
+        ages = [
+            ["0 дней", 0, AgeRight.MODE_DAY],
+            ["6 месяцев", 6, AgeRight.MODE_MONTH],
+            ["5 л.", 5, AgeRight.MODE_YEAR],
+            [" 100  лет", 100, AgeRight.MODE_YEAR],
+        ]
+
+        for age in ages:
+            correct_range = AgeRange(age[1], age[1])
+            right = AgeRight(age[0])
+            self.assertEqual(correct_range, right.age_range, f"Диапазон должен быть '{correct_range}' для '{age[0]}'")
+            self.assertEqual(age[2], right.mode, f"Режим должен быть 'year' для '{age[0]}'")
+
+    def test_constant_age_with_sign_and_optional_mode(self):
+        """Возраст в виде числа с опциональным режимом и направлением определяется корректно"""
+
+        ages = [
+            ["> 0 дней", (0, ")"), float("inf"), AgeRight.MODE_DAY],
+            ["&gt; 0 дней", (0, ")"), float("inf"), AgeRight.MODE_DAY],
+            ["больше 5 дней", (5, ")"), float("inf"), AgeRight.MODE_DAY],
+            ["более 5 дней", (5, ")"), float("inf"), AgeRight.MODE_DAY],
+            ["от 1 дня", (1, "]"), float("inf"), AgeRight.MODE_DAY],
+            ["≥ 1 дня", (1, "]"), float("inf"), AgeRight.MODE_DAY],
+            ["с 1 дня", (1, "]"), float("inf"), AgeRight.MODE_DAY],
+            [">= 1 дня", (1, "]"), float("inf"), AgeRight.MODE_DAY],
+            ["&ge; 1 дня", (1, "]"), float("inf"), AgeRight.MODE_DAY],
+            ["до 7 дней", 0, (7, "]"), AgeRight.MODE_DAY],
+            ["< 7 дней", 0, (7, ")"), AgeRight.MODE_DAY],
+            ["старше 5 лет", (5, ")"), float("inf"), AgeRight.MODE_YEAR],
+            ["младше 5 лет", 0, (5, ")"), AgeRight.MODE_YEAR],
+            ["меньше 5 лет", 0, (5, ")"), AgeRight.MODE_YEAR],
+            ["менее 5 лет", 0, (5, ")"), AgeRight.MODE_YEAR],
+            ["до 20", 0, (20, "]"), AgeRight.MODE_YEAR],
+            ["< 100", 0, (100, ")"), AgeRight.MODE_YEAR],
+            ["&lt; 100", 0, (100, ")"), AgeRight.MODE_YEAR],
+            ["≤ 100", 0, (100, "]"), AgeRight.MODE_YEAR],
+            ["<= 100", 0, (100, "]"), AgeRight.MODE_YEAR],
+            ["&le; 100", 0, (100, "]"), AgeRight.MODE_YEAR],
+        ]
+
+        for age in ages:
+            correct_range = AgeRange(age[1], age[2])
+            right = AgeRight(age[0])
+            self.assertEqual(correct_range, right.age_range, f"Диапазон должен быть '{correct_range}', а не '{right.age_range}' для '{age[0]}'")
+            self.assertEqual(age[3], right.mode, f"Режим должен быть '{age[3]}' для '{age[0]}'")
+
+    def test_full_defined_range(self):
+        """Проверка полностью определённого диапазона с опциональными частями"""
+
+        ages = [
+            ["10 - 20", 10, 20, AgeRight.MODE_YEAR],
+            ["1 г - 2 г", 1, 2, AgeRight.MODE_YEAR],
+            ["10 лет – 20 лет", 10, 20, AgeRight.MODE_YEAR],
+            ["2г. – 3г.", 2, 3, AgeRight.MODE_YEAR],
+            ["от 3 до 5 дней", 3, 5, AgeRight.MODE_DAY],
+            ["от 3 дней до 5", 3, 5, AgeRight.MODE_DAY],
+            ["от 3 месяцев до 5 месяцев", 3, 5, AgeRight.MODE_MONTH],
+        ]
+
+        for age in ages:
+            correct_range = AgeRange(age[1], age[2])
+            right = AgeRight(age[0])
+            self.assertEqual(correct_range, right.age_range, f"Диапазон должен быть '{correct_range}', а не '{right.age_range}' для '{age[0]}'")
+            self.assertEqual(age[3], right.mode, f"Режим должен быть '{age[3]}' для '{age[0]}'")
+
+
+class CheckAgeChecking(unittest.TestCase):
+    """Проверка обнарпужения вхождения в AgeRight"""
+
+    def test_in_age_right(self):
+        ages = [
+            ["до 5 дней", [0, 0, 0], True],
+            ["от 3 до 8 дней", [4, 0, 0], True],
+            ["от 3 до 10 месяцев", [0, 3, 0], True],
+            ["от 7 до 10 лет", [0, 0, 10], True],
+        ]
+
+        for age in ages:
+            right = AgeRight(age[0])
+            in_range = right.test(age[1])
+            self.assertEqual(age[2], in_range, f"Вхождение должно быть '{age[2]}', а не '{in_range}' для '{age[0]}' '{right.age_range}' и '{age[1]}'")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
