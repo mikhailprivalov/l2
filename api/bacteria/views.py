e7beaacca8525dc13f4189339879dba8cce84137
Sergei Kasianenko <monitor84@list.ru>
2020-04-21 11:42:39 +0800

    Промежуток

---
+++ api/bacteria/views.py
@@ -0,0 +1,36 @@
+
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from directory.models import Culture, GroupCulture, Antibiotic, GroupAntibiotic
+
+
+
+@login_required
+def load_culture(request):
+    type = request.GET.get('type')
+    print(type)
+    elements = []
+    groups = []
+    if type == "all":
+        culture_obj = Culture.objects.all()
+        for i in culture_obj:
+            elements.append({"pk": i.pk, "title": i.title, "group": i.group_culture.pk})
+        group_culture_onj = GroupCulture.objects.all()
+        for g in group_culture_onj:
+            groups.append({"pk": g.pk, "title": g.title})
+    else:
+        culture_obj = Culture.objects.filter(group_culture__title=type)
+        print(culture_obj)
+        for i in culture_obj:
+            elements.append({"pk": i.pk, "title": i.title, "group": i.group_culture.pk})
+
+        group_culture_onj = GroupCulture.objects.filter(title=type)
+
+        for g in group_culture_onj:
+            groups.append({"pk": g.pk, "title": g.title})
+
+
+        return_obj = {"groups":[{"pk": "pk", "name" : "name"}, {"pk": "pk", "name" : "name"}],
+                      "elements": [{"pk": "pk", "title": "title"}] }
+
+    return JsonResponse({"groups":groups, "elements": elements})
--- l2-frontend/src/construct/ConstructBacteria.vue
+++ l2-frontend/src/construct/ConstructBacteria.vue
@@ -1,13 +1,72 @@
 <template>
-    <div ref="root" class="construct-root">
+  <div ref="root" class="construct-root" >
+    <div class="construct-sidebar">
+      <div class="sidebar-select" style="padding-top: 20px; padding-left: 15px">
+<!--        <select-picker-m style="height: 34px;" :options="bacteriaGroups" v-model="bacteriaGroup"/>-->
+        <label style="width: 25%;text-align: left; ">
+                Группа:
+                <select class="form-control">
+                    <option v-for="b in bacteriaGroups" :value="b.pk">{{b.title}}</option>
+                </select>
+                <v-select :clearable="false" label="title" :options="bacteriaGroups" :searchable="true"
+                           v-model="selected"/>
+        </label>
 
+        <input type="text" v-model.lazy="bacteriaGroup">
+        <p>{{bacteriaGroup}}</p>
+        <p>selectedID : {{selectedID}}</p>
+        <button class="btn btn-blue-nb sidebar-footer" @click="load_culture_groups">
+          <i class="glyphicon glyphicon-plus"></i>
+          Добавить
+        </button>
+       </div>
+    </div>
 
   </div>
+
 </template>
 
 <script>
+  import vSelect from 'vue-select'
+
     export default {
-        name: "ConstructBacteria"
+      name: "ConstructBacteria",
+      components: {
+      vSelect,
+    },
+      data() {
+      return {
+        bacteriaGroups: [],
+        bacteriaGroup: 'all',
+        selected:''
+      }
+    },
+    created() {
+         this.load_culture_groups()
+    },
+    methods:{
+        load_culture_groups() {
+        const t = this;
+        t.bacteriaGroups = [];
+        fetch("/api/bacteria/loadculture?type=" + t.bacteriaGroup).then(r => r.json()).then(data => {
+          console.log("22",data.groups)
+          t.bacteriaGroups = data.groups
+        })
+      },
+       someDummyMethod() {
+      console.log('Hello from someDummyMethod');
+    }
+    },
+     watch: {
+        bacteriaGroup() {
+        this.load_culture_groups()
+      }
+    },
+     computed: {
+        selectedID: function () {
+        return this.selected.title
+    }
+  }
     }
 </script>
 
