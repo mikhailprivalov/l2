5699173558f2879f22d58c92b4e4c1c882514ef7
Sergei Kasianenko <monitor84@list.ru>
2020-04-22 20:59:53 +0800

    Промежуточная работа

--- l2-frontend/src/construct/ConstructBacteria.vue
+++ l2-frontend/src/construct/ConstructBacteria.vue
@@ -1,48 +1,36 @@
 <template>
     <div>
-        <label style="width: 25%;text-align: left; ">
-                Группа:
-                <select class="form-control">
-                    <option v-for="b in bacteriaGroups" :value="b.pk">{{b.title}}</option>
-                </select>
-
-                <v-select :clearable="false" label="title" :options="bacteriaGroups" :searchable="true"
-                           v-model="selected"/>
-        </label>
-
-        <input type="text" v-model.lazy="bacteriaGroup">
-        <p>{{bacteriaGroup}}</p>
-        <p>selectedID : {{selectedID}}</p>
-        <button class="btn btn-blue-nb sidebar-footer" @click="load_culture_groups">
-          <i class="glyphicon glyphicon-plus"></i>
-          Добавить
-        </button>
-
+        <div class="r-button">
+          <radio-field v-model="searchTypes" :variants="types" fullWidth />
+        </div>
         <div class="lists">
           <div class="left">
-            <h4>Draggable 1</h4>
+             <v-select :clearable="false" label="title" :options="bacteriaGroups" :searchable="true"
+                           v-model="selected"/>
+            <input type="text" v-model="searchElement" placeholder="Фильтр по названию.."/>
             <draggable class="list-group" :list="list1" group="people" @change="log">
-              <div
-                class="item"
-                v-for="(element, index) in list1"
-                :key="element.title"
-              >
-                {{ element.title }} {{ index }}
+
+              <div class="item" v-for="(element) in filteredList" :key="element.title">
+                {{ element.title }} <i class="glyphicon glyphicon-pencil"></i>
               </div>
+
             </draggable>
+            <button class="btn btn-blue-nb sidebar-footer" @click="load_culture_groups">
+                <i class="glyphicon glyphicon-plus"></i>
+                Добавить
+            </button>
+            <button class="btn btn-blue-nb sidebar-footer" @click="load_culture_groups">
+                Сохранить
+            </button>
+
           </div>
 
-
             <div class="right">
-            <h4>Draggable 2</h4>
-
-                <draggable class="list-group" :list="list2" group="people" @change="log">
-                  <div
-                    class="item"
-                    v-for="(element, index) in list2"
-                    :key="element.title"
-                  >
-                    {{ element.title }} {{ index }}
+               <v-select :clearable="false" label="title" :options="bacteriaGroups2" :searchable="true"
+                           v-model="selected1"/>
+                <draggable class="list-group" :class="['right-top']" :list="list2" group="people" @change="log">
+                  <div class="item" v-for="(element) in list2" :key="element.title">
+                    {{ element.title }}
                   </div>
                 </draggable>
               </div>
@@ -54,20 +42,32 @@
 <script>
   import vSelect from 'vue-select'
   import draggable from "vuedraggable";
+  import RadioField from '../fields/RadioField'
 
     export default {
       name: "ConstructBacteria",
       components: {
       vSelect,
       draggable,
+        RadioField
     },
       data() {
       return {
         bacteriaGroups: [],
-        list1: ['1', '2', '3'],
-        list2: ['2','3','5'],
+        bacteriaGroups2: [],
+        list1: [],
+        list2: [],
         bacteriaGroup: 'all',
-        selected: ''
+        selected: '',
+        selected1: '',
+        searchElement: '',
+        types: [
+            'Бактерии',
+            'Антибиоткики',
+            'Бактериофаги',
+            // 'Среды',
+        ],
+        searchTypes: ""
       }
     },
     created() {
@@ -77,16 +77,17 @@
         load_culture_groups() {
         const t = this;
         t.bacteriaGroups = [];
+        t.bacteriaGroups2 = [];
         fetch("/api/bacteria/loadculture?type=" + t.bacteriaGroup).then(r => r.json()).then(data => {
           console.log("22",data.groups)
           t.bacteriaGroups = data.groups
+          // t.bacteriaGroups2 = JSON.parse(JSON.stringify(t.bacteriaGroups));
+          t.bacteriaGroups2 = {...data.groups}
           t.list1 = data.groups
-          t.list2 = [{"pk": 43, "title": "first"}, {"pk": 44, "title": "second"}, {"pk": 45, "title": "third"},
-            {"pk": 46, "title": "first1"}, {"pk": 47, "title": "second1"}, {"pk": 48, "title": "third1"},
-            {"pk": 49, "title": "first2"}, {"pk": 50, "title": "second2"}, {"pk": 51, "title": "third2"},
-            {"pk": 43, "title": "first3"}, {"pk": 44, "title": "second3"}, {"pk": 45, "title": "third3"},
-            {"pk": 46, "title": "f4irst"}, {"pk": 47, "title": "sec4ond"}, {"pk": 48, "title": "t4hird"},
-            {"pk": 49, "title": "fi5st"}, {"pk": 50, "title": "secon5d"}, {"pk": 51, "title": "th5ird"}]
+          t.list2 = [{"pk": "43", "title": "Бактерия1"}, {"pk": "44", "title": "Бактерия2"},
+            {"pk": "45", "title": "Бактеррррррр ррррррррррррррррррр рррррррррия3"},
+            {"pk": "46", "title": "Бактерия4"}, {"pk": "23", "title": "Бактерия5"}, {"pk": "24", "title": "Бактерия6"},
+            {"pk": "25", "title": "Бактерия7"}, {"pk": "26", "title": "Бактерия8"}]
         })
       },
       log: function(evt) {
@@ -100,6 +101,11 @@
      computed: {
         selectedID: function () {
            return this.selected.title
+    },
+       filteredList() {
+      return this.list1.filter(element => {
+        return element.title.toLowerCase().includes(this.searchElement.toLowerCase())
+      })
     }
   }
 
@@ -107,29 +113,60 @@
 </script>
 
 <style lang="scss" scoped>
-
+  input[type="text"] {
+    width: 45vh;
+}
+  .r-button {
+    /*width: 500px;*/
+    width: 70%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 2%;
+    margin-bottom: 3%;
+  }
+  .sidebar-footer {
+    border-radius: 4px;
+    margin: 0;
+  }
+  .glyphicon {
+    alignment: right;
+  }
   .lists {
     padding-left: 5vw;
+    margin-left: auto;
+    margin-right: auto;
     display: flex;
+    width: 70%;
+
     align-items: flex-start;
 
     .left,
     .right {
-      padding: 20px;
+      padding-left: 40px;
       .list-group {
-        height: 200px;
-        width: 300px;
+        height: 40vh;
+        width: 45vh;
         overflow-y: scroll;
         .item {
-          padding: 5px;
-          border: 1px solid black;
-          background-color: white;
-          display: flex;
-          justify-content: space-between;
-
+          background-color: #fff;
+          padding: 3px;
+          margin: 7px;
+          border-radius: 4px;
+          cursor: pointer;
+          box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+          transition: all .2s cubic-bezier(.25, .8, .25, 1);
+          position: relative;
+          i {
+            cursor: pointer;
+            float: right;
+          }
         }
       }
     }
+  }
+
+  .right-top {
+    margin-top: 3.5vh;
   }
 
 
