2a06b0c08346e2778e5948450556f239013633b5
Sergei Kasianenko <monitor84@list.ru>
2020-06-01 10:24:03 +0800

    fix

--- l2-frontend/src/fields/ConfigureAnesthesiaField.vue
+++ l2-frontend/src/fields/ConfigureAnesthesiaField.vue
@@ -1,37 +1,44 @@
 <template>
-  <div style="margin-top: 10px" v-model="tb_data" :readonly="disabled">
+  <div style="margin-top: 10px">
     <table class="table table-bordered">
+      <colgroup>
+        <col width='535'/>
+        <col width='230'/>
+        <col width='120'/>
+      </colgroup>
       <thead>
       <tr>
-        <th class="first-column">Наименование</th>
-        <th class="second-column">Категория</th>
-        <th class="third-column">Показать по умолчанию</th>
+        <th>Наименование</th>
+        <th>Категория</th>
+        <th>Показать по умолчанию</th>
         <th></th>
       </tr>
       </thead>
       <tbody>
       <tr class="tr-body" v-for="(val, index) in tb_data">
-        <td class="first-column"><input class="no-outline" type="text" style="width:100%" v-model="val['title']"
+        <td><input class="no-outline" type="text" style="width:100%" v-model="val.title"
                                         placeholder="Введите наименование"></td>
-        <td class="second-column">
-          <select v-model="val['type']">
+        <td>
+          <select v-model="val.type">
             <option disabled value="">Выберите один из вариантов</option>
             <option>Показатели человека</option>
             <option>Сильнодействующие</option>
             <option>Наркотические</option>
           </select>
         </td>
-        <td class="third-column" align="center">
-          <input type="checkbox" v-model="val['default']">
+        <td align="center">
+          <input type="checkbox" v-model="val.default">
         </td>
         <td>
-          <button class="btn btn-default btn-primary-nb" v-if="tb_data.length > 0" v-on:click="delete_row(index)"
-                  v-tippy="{ placement : 'bottom'}" title="Удалить строку"><i class="fa fa-times"/></button>
+          <button class="btn btn-default btn-primary-nb btn-delete" @click="delete_row(index)"
+                  v-tippy="{ placement : 'bottom'}" title="Удалить строку">
+          <i class="fa fa-minus"/>
+          </button>
         </td>
       </tr>
       </tbody>
     </table>
-    <button class="btn btn-blue-nb" @click="add_new_row">
+    <button class="btn btn-blue-nb add-row" @click="add_new_row">
       Добавить
     </button>
   </div>
@@ -59,7 +66,7 @@
       add_new_row() {
         this.tb_data.push({"header":'', "title": '', "type": '', "default": false});
       },
-      delete_row: function (index) {
+       delete_row(index) {
         this.tb_data.splice(index, 1);
       },
       changeValue(newVal) {
@@ -82,54 +89,23 @@
 
 <style scoped lang="scss">
 
-  td, th {
-    padding-right: 4px;
-    padding-left: 4px;
-    padding-top: 1px;
-    padding-bottom: 2px;
-  }
-
-  .first-column {
-    width: 535px;
-  }
-  .tr-body{
+  .tr-body {
     height: 10px;
   }
 
-  .second-column {
-    width: 230px;
-  }
-
-  .third-column {
-    width: 120px;
-  }
-
-  input[type=text] {
-    border-top-style: hidden;
-    border-right-style: hidden;
-    border-left-style: hidden;
-    /*border-bottom-style: groove;*/
-    border-bottom-style: hidden;
+  input {
+    border: none;
     margin-left: 3px;
     margin-right: 3px;
-    /*margin-top: 8px;*/
     width: 100%;
   }
 
-  input:focus,
-  input:active {
-    /*border-bottom: 2px solid #56616c;*/
-    background-color: #66afe9;
-  }
-
-  .no-outline:focus {
-    outline: none;
-  }
-
-  .btn {
-    padding: 5px 4px;
-    width: 100px;
+  .add-row {
     float: right;
+  }
+
+  .btn-delete {
+    padding: 0;
   }
 
 </style>
