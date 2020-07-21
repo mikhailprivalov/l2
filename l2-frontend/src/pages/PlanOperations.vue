f51b050e0c0e803a99e8a25cfadf144ab3687305
Mikhail Privalov <n5250@ya.ru>
2020-07-21 21:48:57 +0300

    Filters base

--- l2-frontend/src/pages/PlanOperations.vue
+++ l2-frontend/src/pages/PlanOperations.vue
@@ -1,19 +1,17 @@
 <template>
   <div>
     <h3>{{title}}</h3>
-    <table class="table">
+    <table class="table" style="table-layout: fixed">
       <colgroup>
-        <col width='70'/>
-        <col width='70'/>
-        <col width='100'/>
-        <col width='100'/>
-        <col width='100'/>
+        <col width='201'/>
+        <col />
+        <col width='180'/>
+        <col width='180'/>
       </colgroup>
       <thead>
       <tr>
-        <th><input class="form-control" type="date"></th>
-        <th><input class="form-control" type="date"></th>
-        <th><input class="form-control"></th>
+        <th><date-range v-model="filter_date"/></th>
+        <th></th>
         <th>Отделение</th>
         <th>Анестезиолог</th>
       </tr>
@@ -54,12 +52,15 @@
 
 
 <script>
+  import DateRange from "../ui-cards/DateRange";
   import PlanOperationEdit from '../modals/PlanOperationEdit'
   import plans_point from '../api/plans-point'
+  import moment from "moment";
 
   export default {
     components: {
       PlanOperationEdit,
+      DateRange,
     },
     name: "PlanOperations",
     data() {
@@ -67,7 +68,8 @@
         title: 'План операций',
         edit_plan_operations: false,
         pk_plan: '',
-        data_plans: ''
+        data_plans: '',
+        filter_date: [moment().format('DD.MM.YYYY'), moment().add(7, 'days').format('DD.MM.YYYY')],
       }
     },
     mounted() {
@@ -81,10 +83,15 @@
         this.pk_plan = -1
       },
       async load_data(){
+        let [d1, d2] = this.filter_date;
+        d1 = d1.split('.');
+        d1 = `${d1[2]}-${d1[1]}-${d1[0]}`;
+        d2 = d2.split('.');
+        d2 = `${d2[2]}-${d2[1]}-${d2[0]}`;
         const {result} = await plans_point.getPlansByParams({
-          'start_date': '2020-07-17',
-          'end_date': '2020-07-19',
-          'doc_operate_pk': 1940,
+          'start_date': d1,
+          'end_date': d2,
+          'doc_operate_pk': -1,
           'doc_anesthetist_pk': -1,
           'department': -1
         })
