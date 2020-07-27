d8a49459bc12ec4ead31806a320346bd1d5dacf0
Mikhail Privalov <n5250@ya.ru>
2020-07-27 14:54:13 +0300

    UNIT TESTS

--- utils/xh.py
+++ utils/xh.py
@@ -2,15 +2,14 @@ def fix(s: str):
     return s.replace('<br>', '<br/>')
 
 
+SQUARE_BRACKETS_WEIGHTS = {'[': -1, ']': 1}
+
+
 def check_valid_square_brackets(v: str):
     count = 0
-    weights = {'[': -1, ']': 1}
     for s in v:
-        if s in weights:
-            count += weights[s]
+        if s in SQUARE_BRACKETS_WEIGHTS:
+            count += SQUARE_BRACKETS_WEIGHTS[s]
             if count > 0:
                 return False
-    if count < 0:
-        return False
-
-    return True
+    return count == 0
