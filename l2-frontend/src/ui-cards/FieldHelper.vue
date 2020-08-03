a23262b625f589a3217bc86459df086ffb96b3d5
Mikhail Privalov <n5250@ya.ru>
2020-08-03 23:52:43 +0300

    Normalizing environment

---
+++ l2-frontend/src/ui-cards/FieldHelper.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <template v-if="Boolean(mode)">
+
+    </template>
+  </div>
+</template>
+
+<script>
+  const fieldModes = {
+    3: 'formula', 11: 'fraction', 13: 'field', 14: 'field', 23: 'field-and-group'
+  };
+
+  export default {
+    name: "FieldHelper",
+    props: {
+      fieldType: {
+        type: Number,
+        required: true,
+      },
+      value: {
+        type: String,
+        required: true,
+      }
+    },
+    computed: {
+      mode() {
+        return fieldModes[this.type];
+      },
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
