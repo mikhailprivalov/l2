f7ab9cb9096af42ff0999203d5db2d72d272b904
Sergei Kasianenko <monitor84@list.ru>
2020-10-24 17:24:20 +0800

    Doc_cal and listwait: methods-save, change_status, get

--- doctor_call/models.py
+++ doctor_call/models.py
@@ -1,19 +1,91 @@
 from django.db import models
-from clients.models import Card
+from clients.models import Card, District
 from directory.models import Researches
 from users.models import DoctorProfile
+import datetime
+from laboratory.utils import current_time
+import slog.models as slog
+import simplejson as json
 
 
 class DoctorCall(models.Model):
     client = models.ForeignKey(Card, db_index=True, help_text='Пациент', on_delete=models.CASCADE)
-    research = models.ForeignKey(Researches, null=True, blank=True, help_text='Вид исследования из справочника', db_index=True, on_delete=models.CASCADE)
-    create_at = models.DateTimeField(auto_now_add=True, help_text='Дата создания', db_index=True)
+    research = models.ForeignKey(Researches, null=True, blank=True, help_text='Вид исследования из справочника', on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True, help_text='Дата создания')
     exec_at = models.DateTimeField(auto_now_add=True, help_text='Дата вызова на дом', db_index=True)
     comment = models.TextField()
     cancel = models.BooleanField(default=False, blank=True, help_text='Отмена вызова')
-    doc_who_create = models.ForeignKey(DoctorProfile, default=None, blank=True, null=True, related_name="doc_who_create", help_text='Создатель направления', on_delete=models.SET_NULL)
-    doc = models.ForeignKey(DoctorProfile, db_index=True, null=True, help_text='Лечащий врач', on_delete=models.CASCADE)
+    doc_who_create = models.ForeignKey(DoctorProfile, default=None, blank=True, null=True, help_text='Создатель вызова на дом', on_delete=models.SET_NULL)
+    district = models.ForeignKey(District, default=None, null=True, blank=True, db_index=True, help_text="Участок", on_delete=models.SET_NULL)
 
     class Meta:
         verbose_name = 'Вызов'
         verbose_name_plural = 'Вызова на дом'
+
+    @staticmethod
+    def doctor_call_save(data, doc_who_create):
+        patient_card = Card.objects.filter(pk=data['card_pk'])[0]
+        research_obj = Researches.objects.get(pk=data['research'])
+        district_obj = District.objects.get(pk=data.get('district'))
+        doc_call = DoctorCall(client=patient_card, research=research_obj, exec_at=datetime.datetime.strptime(data['date'], '%Y-%m-%d'), comment=data['comment'],
+                              doc_who_create=doc_who_create, cancel=False, district=district_obj)
+        doc_call.save()
+
+        slog.Log(
+            key=doc_call.pk,
+            type=80003,
+            body=json.dumps(
+                {
+                    "card_pk": data['card_pk'],
+                    "research": research_obj.title,
+                    "distric": district_obj.title,
+                    "date": datetime.datetime.strptime(data['date']),
+                    "comment": data['comment'],
+                }
+            ),
+            user=doc_who_create,
+        ).save()
+        return doc_call.pk
+
+    def doctor_call_cancel(data, doc_who_create):
+        doc_call = DoctorCall.objects.filter(pk=data['pk_doc_call'])[0]
+        doc_call.doc_who_create = doc_who_create
+        doc_call.cancel = not doc_call.canceled
+        doc_call.save()
+
+        slog.Log(
+            key=doc_call.pk,
+            type=80004,
+            body=json.dumps(
+                {
+                    "card_pk": doc_call.client.pk,
+                    "status": doc_call.cancel
+                }
+            ),
+            user=doc_who_create,
+        ).save()
+        return doc_call.pk
+
+
+    @staticmethod
+    def doctor_call_get(data):
+        if data.get('d1', None):
+            d1 = datetime.datetime.strptime(data.get('d1'), '%d.%m.%Y')
+        else:
+            d1 = current_time()
+        if data.get('d2', None):
+            d2 = datetime.datetime.strptime(data.get('d2'), '%d.%m.%Y')
+        else:
+            d2 = current_time()
+
+        start_date = datetime.datetime.combine(d1, datetime.time.min)
+        end_date = datetime.datetime.combine(d2, datetime.time.max)
+        if data.get('district', None):
+            district_obj = District.objects.filter(pk__in=data.get('district'))
+            result = DoctorCall.objects.filter(district__in=district_obj, exet_at____range=(start_date, end_date)).order_by("district")
+        elif data.get('patient_pk', None):
+            result = DoctorCall.objects.filter(client__pk=data.get('patient_pk')).order_by("exec_at")
+        else:
+            result = DoctorCall.objects.filter(exet_at____range=(start_date, end_date)).order_by("exet_at, district")
+
+        return result
--- list_wait/models.py
+++ list_wait/models.py
@@ -2,17 +2,96 @@ from django.db import models
 from clients.models import Card
 from directory.models import Researches
 from users.models import DoctorProfile
+import datetime
+from laboratory.utils import current_time
+import slog.models as slog
+import simplejson as json
 
 
 class ListWait(models.Model):
+    STATUS = (
+        (0, "Ожидает"),
+        (1, "Выполнено"),
+        (2, "Отменено"),
+    )
     client = models.ForeignKey(Card, db_index=True, help_text='Пациент', on_delete=models.CASCADE)
-    research = models.ForeignKey(Researches, null=True, blank=True, help_text='Вид исследования из справочника', db_index=True, on_delete=models.CASCADE)
-    create_at = models.DateTimeField(auto_now_add=True, help_text='Дата создания', db_index=True)
+    research = models.ForeignKey(Researches, null=True, blank=True, db_index=True, help_text='Вид исследования из справочника', on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True, help_text='Дата создания')
     exec_at = models.DateTimeField(auto_now_add=True, help_text='Дата создания', db_index=True)
     comment = models.TextField()
-    cancel = models.BooleanField(default=False, blank=True, help_text='Отмена ожидания')
-    doc_who_create = models.ForeignKey(DoctorProfile, default=None, blank=True, null=True, related_name="doc_who_create", help_text='Создатель направления', on_delete=models.SET_NULL)
+    status = models.BooleanField(choices=STATUS, db_index=True)
+    doc_who_create = models.ForeignKey(DoctorProfile, default=None, blank=True, null=True, help_text='Создатель листа ожидания', on_delete=models.SET_NULL)
 
     class Meta:
         verbose_name = 'Лист ожидания'
         verbose_name_plural = 'Лист ожидания'
+
+    @staticmethod
+    def list_wait_save(data, doc_who_create):
+        patient_card = Card.objects.filter(pk=data['card_pk'])[0]
+        research_obj = Researches.objects.get(pk=data['research'])
+        list_wait = ListWait(client=patient_card,
+                             research=research_obj,
+                             exec_at=datetime.datetime.strptime(data['date'], '%Y-%m-%d'),
+                             comment=data['comment'],
+                             doc_who_create=doc_who_create,
+                             status=0)
+        list_wait.save()
+
+        slog.Log(
+            key=list_wait.pk,
+            type=80005,
+            body=json.dumps(
+                {
+                    "card_pk": data['card_pk'],
+                    "research": research_obj.title,
+                    "date": datetime.datetime.strptime(data['date']),
+                    "comment": data['comment'],
+                }
+            ),
+            user=doc_who_create,
+        ).save()
+        return list_wait.pk
+
+    @staticmethod
+    def list_wait_change_status(data, doc_who_create):
+        list_wait = ListWait.objects.filter(pk=data['pk_list_wait'])[0]
+        list_wait.doc_who_create = doc_who_create
+        list_wait.status = data['status']
+        list_wait.save()
+
+        slog.Log(
+            key=list_wait.pk,
+            type=80006,
+            body=json.dumps(
+                {
+                    "card_pk": list_wait.client.pk,
+                    "status": list_wait.status
+                }
+            ),
+            user=doc_who_create,
+        ).save()
+        return list_wait.pk
+
+    @staticmethod
+    def list_wait_get(data):
+        if data.get('d1', None):
+            d1 = datetime.datetime.strptime(data.get('d1'), '%d.%m.%Y')
+        else:
+            d1 = current_time()
+        if data.get('d2', None):
+            d2 = datetime.datetime.strptime(data.get('d2'), '%d.%m.%Y')
+        else:
+            d2 = current_time()
+
+        start_date = datetime.datetime.combine(d1, datetime.time.min)
+        end_date = datetime.datetime.combine(d2, datetime.time.max)
+        if data.get('research', None):
+            research_obj = Researches.objects.filter(pk=data.get('research'))
+            result = ListWait.objects.filter(research=research_obj, exet_at____range=(start_date, end_date)).order_by("exet_at")
+        elif data.get('patient_pk', None):
+            result = ListWait.objects.filter(client__pk=data.get('patient_pk')).order_by("exec_at")
+        else:
+            result = ListWait.objects.filter(exet_at____range=(start_date, end_date)).order_by("exet_at, research")
+
+        return result
