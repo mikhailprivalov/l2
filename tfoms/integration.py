36d202cae5b508746690fe52ed6db1104edd6d49
Mikhail Privalov <n5250@ya.ru>
2020-11-20 03:20:01 +0300

    logger

--- tfoms/integration.py
+++ tfoms/integration.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List, Optional
 from urllib.parse import urljoin, urlencode
 
@@ -5,6 +6,8 @@ import requests
 
 from appconf.manager import SettingManager
 from tfoms.l2 import check_l2_enp
+
+logger = logging.getLogger(__name__)
 
 
 def get_url(path, query=None):
@@ -44,8 +47,11 @@ def match_patient(family, name, patronymic, birthday) -> List[dict]:
 
 
 def match_enp(enp) -> Optional[dict]:
+    logger.exception(f"tfms: match enp: {enp}")
     if SettingManager.get("l2_patients_is_active", default='f', default_type='b'):
+        logger.exception("l2_patients_is_active")
         resp = check_l2_enp(enp)
+        logger.exception(f"resp: {resp}")
         if not isinstance(resp, dict) or not resp.get('ok') or not resp.get('patient_data'):
             return None
         return resp.get('patient_data')
