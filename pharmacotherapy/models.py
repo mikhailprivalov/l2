2e13c81896d82347e5a2b638789d2d9776b295f2
Sergei Kasianenko <monitor84@list.ru>
2020-11-21 21:52:54 +0800

    procedure_list_times

--- pharmacotherapy/models.py
+++ pharmacotherapy/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-from datetime import date
+from jsonfield import JSONField
+
+from clients.models import Card
+from directions.models import Napravleniya
+from users.models import DoctorProfile
 
 
 class Mnn(models.Model):
@@ -27,16 +31,33 @@ class MethodsReception(models.Model):
 
 
 class ProcedureList(models.Model):
+    history_num = models.ForeignKey(Napravleniya, related_name='history_num', help_text='Номер истории', db_index=True, on_delete=models.CASCADE)
+    diaries_num = models.ForeignKey(Napravleniya, related_name='diaries_num', help_text='Номер дневника', db_index=True, on_delete=models.CASCADE)
+    client = models.ForeignKey(Card, db_index=True, help_text='Пациент', on_delete=models.CASCADE)
     mnn = models.ForeignKey(Mnn, help_text="Стационарная услуга", on_delete=models.CASCADE, db_index=True)
     form_release = models.ForeignKey(FormRelease, help_text="форма выпуска", null=True, on_delete=models.SET_NULL, db_index=True)
     method = models.ForeignKey(MethodsReception, help_text="Способ применения", null=True, on_delete=models.SET_NULL, db_index=True)
     dosage = models.IntegerField(default=0, blank=True)
     units = models.CharField(max_length=16, default="", help_text='Единицы измерения')
-    times_receipt = models.CharField(max_length=255, default="", help_text='Время приема')
-    date_start = models.DateField(default=date.today, help_text="Дата начала", blank=False, null=False, db_index=True)
+    times_medication = JSONField(default=None, blank=True, null=True, help_text="Время приема-статус-исполнитель")
+    date_start = models.DateField(help_text="Дата начала", db_index=True)
     count_day = models.SmallIntegerField(default=1, blank=True)
-    date_end = models.DateField(default=date.today, help_text="Дата окончания включительно", blank=False, null=False, db_index=True)
+    date_end = models.DateField(help_text="Дата окончания включительно", db_index=True)
+    doc_who_create = models.ForeignKey(DoctorProfile, default=None, blank=True, null=True, related_name="doc_who_create", help_text='Создатель направления', on_delete=models.SET_NULL)
+    time_create = models.DateTimeField(auto_now_add=True, help_text='Дата создания', db_index=True)
+    time_cancel = models.DateTimeField(auto_now_add=True, help_text='Дата отмены', db_index=True)
 
     class Meta:
         verbose_name = 'Назначение'
         verbose_name_plural = 'Назначения'
+
+
+class ProcedureListTimes(models.Model):
+    procedure_list = models.ForeignKey(ProcedureList, null=True, blank=True, help_text='Назначение из процедурного листа', on_delete=models.SET_NULL)
+    times_medication = models.DateTimeField(help_text='Время приема', db_index=True)
+    executor = models.ForeignKey(DoctorProfile, default=None, blank=True, null=True, related_name="doc_who_create", help_text='Исполнитель', on_delete=models.SET_NULL)
+    cancel = models.BooleanField(default=False, blank=True, help_text='Отмена времени')
+
+    class Meta:
+        verbose_name = 'Время из процедурного листа'
+        verbose_name_plural = 'Время из процедурного листа'
