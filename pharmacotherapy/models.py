9c0ac38fb4b795de0b05f6f71525c2ca31bb0bc1
Sergei Kasianenko <monitor84@list.ru>
2020-11-22 08:02:23 +0800

    fix-model

--- pharmacotherapy/models.py
+++ pharmacotherapy/models.py
@@ -29,20 +29,18 @@ class MethodsReception(models.Model):
 
 
 class ProcedureList(models.Model):
-    history_num = models.ForeignKey(Napravleniya, default=None, related_name='history_number', help_text='Номер истории', db_index=True, on_delete=models.CASCADE)
-    diaries_num = models.ForeignKey(Napravleniya, default=None, related_name='diaries_number', help_text='Номер дневника', db_index=True, on_delete=models.CASCADE)
-    client = models.ForeignKey(Card, default=None, db_index=True, help_text='Пациент', on_delete=models.CASCADE)
-    mnn = models.ForeignKey(Mnn, help_text="Стационарная услуга", on_delete=models.CASCADE, db_index=True)
-    form_release = models.ForeignKey(FormRelease, help_text="форма выпуска", null=True, on_delete=models.SET_NULL, db_index=True)
-    method = models.ForeignKey(MethodsReception, help_text="Способ применения", null=True, on_delete=models.SET_NULL, db_index=True)
-    dosage = models.IntegerField(default=0, blank=True)
-    units = models.CharField(max_length=16, default="", help_text='Единицы измерения')
-    date_start = models.DateField(help_text="Дата начала", db_index=True)
-    count_day = models.SmallIntegerField(default=1, blank=True)
-    date_end = models.DateField(help_text="Дата окончания включительно", db_index=True)
-    doc_create = models.ForeignKey(DoctorProfile, related_name="doc_create_prescription", default=None, blank=True, null=True, help_text='Создатель назначения', on_delete=models.SET_NULL)
-    time_create = models.DateTimeField(auto_now_add=True, help_text='Дата создания', db_index=True)
-    time_cancel = models.DateTimeField(auto_now_add=True, help_text='Дата отмены', db_index=True)
+    history = models.ForeignKey(Napravleniya, default=None, related_name='history_number', help_text='Номер истории', db_index=True, on_delete=models.CASCADE)
+    diary = models.ForeignKey(Napravleniya, default=None, related_name='diaries_number', help_text='Номер дневника', db_index=True, on_delete=models.CASCADE)
+    card = models.ForeignKey(Card, default=None, db_index=True, help_text='Пациент', on_delete=models.CASCADE)
+    mnn = models.ForeignKey(Mnn, help_text="МНН препарата", on_delete=models.CASCADE, db_index=True)
+    form_release = models.ForeignKey(FormRelease, help_text="Форма выпуска", on_delete=models.CASCADE)
+    method = models.ForeignKey(MethodsReception, help_text="Способ применения", on_delete=models.CASCADE)
+    dosage = models.FloatField(help_text='Дозировка')
+    units = models.CharField(max_length=8, help_text='Единицы измерения')
+    date_start = models.DateField(help_text="Дата начала")
+    date_end = models.DateField(help_text="Дата окончания включительно")
+    doc_create = models.ForeignKey(DoctorProfile, related_name="doc_create_prescription", help_text='Создатель назначения', on_delete=models.CASCADE)
+    time_create = models.DateTimeField(auto_now_add=True, help_text='Дата создания')
 
     class Meta:
         verbose_name = 'Назначение'
@@ -50,10 +48,11 @@ class ProcedureList(models.Model):
 
 
 class ProcedureListTimes(models.Model):
-    prescription = models.ForeignKey(ProcedureList, null=True, blank=True, help_text='Назначение из процедурного листа', on_delete=models.SET_NULL)
+    prescription = models.ForeignKey(ProcedureList, help_text='Назначение из процедурного листа', on_delete=models.CASCADE)
     times_medication = models.DateTimeField(help_text='Время приема', db_index=True)
     executor = models.ForeignKey(DoctorProfile, default=None, blank=True, null=True, related_name="doc_executor_prescription", help_text='Исполнитель', on_delete=models.SET_NULL)
-    cancel = models.BooleanField(default=False, blank=True, help_text='Отмена времени')
+    cancel = models.BooleanField(default=False, blank=True, help_text='Отмена времени приема')
+    who_cancel = models.ForeignKey(DoctorProfile, default=None, blank=True, null=True, related_name="who_cancel", help_text='Кто отменил', on_delete=models.SET_NULL)
 
     class Meta:
         verbose_name = 'Время из процедурного листа'
