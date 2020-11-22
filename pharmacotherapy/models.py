8f5e27f0d280bb2b5efb471ddc7ffd18404df2d4
Sergei Kasianenko <monitor84@list.ru>
2020-11-22 21:33:41 +0800

    pep

--- pharmacotherapy/models.py
+++ pharmacotherapy/models.py
@@ -27,7 +27,6 @@ class FormRelease(models.Model):
         verbose_name_plural = 'Формы выпуска'
 
 
-
 class MethodsReception(models.Model):
     title = models.CharField(max_length=255, default="", help_text='Способ применения')
 
