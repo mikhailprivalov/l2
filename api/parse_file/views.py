61a8b01b99ccddc6774d321e506e0be558a27212
Sergei Kasianenko <monitor84@list.ru>
2020-12-12 15:57:51 +0800

    endpoint

--- api/parse_file/views.py
+++ api/parse_file/views.py
@@ -1,7 +1,11 @@
+from django.http import HttpRequest
+
 from api.parse_file.pdf import extract_text_from_pdf
+import simplejson as json
+from api.views import endpoint
 
 
-def dnk_covid(file_data):
+def dnk_covid(request):
     prefixes = []
     A = [f"A{i}" for i in range(1, 13)]
     prefixes.extend(A)
@@ -26,13 +30,24 @@ def dnk_covid(file_data):
     K = [f"K{i}" for i in range(1, 13)]
     prefixes.extend(K)
 
-    t = extract_text_from_pdf(file_data).replace("\n", "").split("Коронавирусы подобные SARS-CoVВККоронавирус SARS-CoV-2")
-    for i in t:
-        if "+" in i:
-            k = i.split("N")
-            if k[1].split(" ")[0].isdigit():
-                return {"pk": k[1].split(" ")[0], "result": "Положительно"}
-        else:
-            k = i.split("N")
-            if k[1].split(" ")[0].isdigit():
-                return {"pk": k[1].split(" ")[0], "result": "Отрицательно"}
+    req = json.loads(request.body)
+    file_data = req.get('file_data', False)
+    if file_data:
+        t = extract_text_from_pdf(file_data).replace("\n", "").split("Коронавирусы подобные SARS-CoVВККоронавирус SARS-CoV-2")
+        for i in t:
+            if "+" in i:
+                k = i.split("N")
+                if k[1].split(" ")[0].isdigit():
+                    http_func({"pk": k[1].split(" ")[0], "result": "Положительно"}, request.user)
+            else:
+                k = i.split("N")
+                if k[1].split(" ")[0].isdigit():
+                    http_func({"pk": k[1].split(" ")[0], "result": "Отрицательно"}, request.user)
+
+
+def http_func(data, user):
+    json_data = json.dumps(data)
+    http_obj = HttpRequest()
+    http_obj._body = json_data
+    http_obj.user = user
+    endpoint(http_obj)
