2777a29e1f5dd807a759edfd2def33cc717b2603
Sergei Kasianenko <monitor84@list.ru>
2020-12-24 07:04:24 +0800

    .

--- api/parse_file/pdf.py
+++ api/parse_file/pdf.py
@@ -10,17 +10,12 @@ def extract_text_from_pdf(pdf_path):
     fake_file_handle = io.StringIO()
     converter = TextConverter(resource_manager, fake_file_handle)
     page_interpreter = PDFPageInterpreter(resource_manager, converter)
+    for page in PDFPage.get_pages(pdf_path, caching=True, check_extractable=True):
+        page_interpreter.process_page(page)
 
-    with open(pdf_path, 'rb') as fh:
-        for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
-            page_interpreter.process_page(page)
-
-        text = fake_file_handle.getvalue()
+    text = fake_file_handle.getvalue()
 
     converter.close()
     fake_file_handle.close()
 
-    if text:
-        return text
-    else:
-        return None
+    return text
