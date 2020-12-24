b61a1b71cb5b9ba8bdfea29a1a71d644b10ae69b
Sergei Kasianenko <monitor84@list.ru>
2020-12-24 23:05:12 +0800

    parse pdf -> to result

--- api/parse_file/views.py
+++ api/parse_file/views.py
@@ -1,27 +1,34 @@
 import tempfile
-
 from django.http import HttpRequest, JsonResponse
-
 from api.parse_file.pdf import extract_text_from_pdf
 import simplejson as json
 from api.views import endpoint
+from appconf.manager import SettingManager
 
 
 def dnk_covid(request):
     prefixes = []
+    key_dnk = SettingManager.get("dnk_kovid", default='false', default_type='s')
+    to_return = None
     for x in "ABCDEF":
         prefixes.extend([f"{x}{i}" for i in range(1, 13)])
-    req = json.loads(request.body)
-    file_data = req.get('file_data', False)
-    if file_data:
-        text = extract_text_from_pdf(file_data)
+    file = request.FILES['file']
+    if file.content_type == 'application/pdf' and file.size < 100000:
+        with tempfile.TemporaryFile() as fp:
+            fp.write(file.read())
+            text = extract_text_from_pdf(fp)
         if text:
-            text.replace("\n", "").split("Коронавирусы подобные SARS-CoVВККоронавирус SARS-CoV-2")
-        for i in text:
-            k = i.split("N")
-            if len(k) > 1 and k[1].split(" ")[0].isdigit():
-                result = json.dumps({"pk": k[1].split(" ")[0], "result": [{"dnk_SARS": "Положительно" if "+" in i else "Отрицательно"}]})
-                http_func({"key": 'xxxx', "result": result}, request.user)
+            text = text.replace("\n", "").split("Коронавирусы подобные SARS-CoVВККоронавирус SARS-CoV-2")
+        to_return = []
+        if text:
+            for i in text:
+                k = i.split("N")
+                if len(k) > 1 and k[1].split(" ")[0].isdigit():
+                    result = json.dumps({"pk": k[1].split(" ")[0], "result": [{"dnk_SARS": "Положительно" if "+" in i else "Отрицательно"}]})
+                    to_return.append({"pk": k[1].split(" ")[0], "result": "Положительно" if "+" in i else "Отрицательно"})
+                    http_func({"key": key_dnk, "result": result}, request.user)
+
+    return to_return
 
 
 def http_func(data, user):
@@ -32,16 +39,5 @@ def http_func(data, user):
 
 
 def load_file(request):
-    file = request.FILES['sars_report']
-    if file.content_type == 'application/pdf' and file.size < 10000:
-        with tempfile.TemporaryFile() as fp:
-            fp.write(file.read())
-            text = extract_text_from_pdf(fp)
-
-    return JsonResponse({"ok": True, "results": [
-        {"pk": 1111, "result": "Отрицательно"},
-        {"pk": 1112, "result": "Положительно"},
-        {"pk": 1113, "result": "Отрицательно"},
-        {"pk": 1114, "result": "Отрицательно"},
-        {"pk": 1115, "result": "Положительно"},
-    ]})
+    results = dnk_covid(request)
+    return JsonResponse({"ok": True, "results": results})
