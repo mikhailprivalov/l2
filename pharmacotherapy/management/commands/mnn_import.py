263dd5fdafd4bb54671220e71b0a1802ad6ce412
Sergei Kasianenko <monitor84@list.ru>
2021-02-11 08:44:36 +0800

    import mnn

--- pharmacotherapy/management/commands/mnn_import.py
+++ pharmacotherapy/management/commands/mnn_import.py
@@ -16,14 +16,18 @@ class Command(BaseCommand):
         self.stdout.write("Path: " + fp)
         wb = load_workbook(filename=fp)
         ws = wb[wb.sheetnames[0]]
+        print(ws)
         starts = False
         for row in ws.rows:
             cells = [str(x.value) for x in row]
             if not starts:
-                if "мнн" in cells and "торрг" in cells:
+                if "мнн" in cells and "торг" in cells:
                     starts = True
+                    torg = cells.index("торг")
                     mnn = cells.index("мнн")
-                    torg = cells.index("торрг")
             else:
-                Drugs.objects.create(code=cells[mnn], title=cells[torg])
+                print(cells[mnn])
+                if cells[mnn] == "~":
+                    continue
+                Drugs.objects.create(mnn=cells[mnn][:255], trade_name=cells[torg][:255])
                 print(f'добавлен МНН:{cells[mnn]}:{cells[torg]}')
