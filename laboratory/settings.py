ba7ea1aa233e099f77f30dbb7471a7ab5fda0584
Mikhail Privalov <n5250@ya.ru>
2021-02-23 17:10:53 +0300

    requirements

--- laboratory/settings.py
+++ laboratory/settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admindocs',
-    'django_extensions',
     'ajax_select',
     'health',
     'appconf.apps.AppconfConfig',
