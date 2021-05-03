83a4dedbef36afac15f1d213515184de9320a7c9
Sergei Kasianenko <monitor84@list.ru>
2021-05-04 07:52:59 +0800

    add typing

--- directions/forms/forms380.py
+++ directions/forms/forms380.py
@@ -1,6 +1,9 @@
 import os
 from copy import deepcopy
+from typing import Union, List
+
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import QuerySet
 from django.utils.text import Truncator
 from reportlab.graphics import renderPDF
 from reportlab.graphics.barcode import eanbc, qr
@@ -585,7 +588,7 @@ def form_04(c: Canvas, dir: Napravleniya):
     printForm()
 
 
-def form_05(c: Canvas, dir_obj):
+def form_05(c: Canvas, dir_obj: Union[QuerySet, List[Napravleniya]]):
     # Утверждено Приказом Министерства здравоохранения Иркутской области от 22 мая 2013 г. N 83-МПР
     def printForm(dir):
         if sys.platform == 'win32':
