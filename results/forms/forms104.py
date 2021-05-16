764cd6c44ebe2b99a97daf0e58f2cf4ee86562e3
Sergei Kasianenko <monitor84@list.ru>
2021-05-16 19:59:20 +0800

    оптимизация

--- results/forms/forms104.py
+++ results/forms/forms104.py
@@ -8,7 +8,7 @@ from copy import deepcopy
 from reportlab.lib.enums import TA_CENTER, TA_LEFT, TA_JUSTIFY
 import directory.models as directory
 from directions.models import ParaclinicResult, Napravleniya
-from results.prepare_data import text_to_bold, fields_result
+from results.prepare_data import text_to_bold, fields_result_only_title_fields, fields_result
 from directions.models import Issledovaniya
 from laboratory.settings import FONTS_FOLDER
 import os.path
@@ -76,44 +76,7 @@ def form_01(direction, iss: Issledovaniya, fwb, doc, leftnone, user=None):
     address = ind_data['main_address']
     fwb.append(Paragraph(f'{open_bold_tag}Место регистрации:{close_tag_bold} {address}', style_ml))
 
-    for group in directory.ParaclinicInputGroups.objects.filter(research=iss.research).order_by("order"):
-        results = ParaclinicResult.objects.filter(issledovaniye=iss, field__group=group).order_by("field__order")
-        group_title = False
-        fwb.append(Spacer(1, 3 * mm))
-        if results.exists():
-            if group.show_title and group.show_title != "":
-                fwb.append(Paragraph(group.title.replace('<', '&lt;').replace('>', '&gt;'), styleBold))
-                group_title = True
-            for r in results:
-                field_type = r.get_field_type()
-                if field_type == 15:
-                    continue
-                else:
-                    v = r.value.replace('<', '&lt;').replace('>', '&gt;').replace("\n", "<br/>")
-                    if not v:
-                        continue
-                    v = v.replace('&lt;sub&gt;', '<sub>')
-                    v = v.replace('&lt;/sub&gt;', '</sub>')
-                    v = v.replace('&lt;sup&gt;', '<sup>')
-                    v = v.replace('&lt;/sup&gt;', '</sup>')
-                    v = text_to_bold(v)
-                    if field_type == 16:
-                        continue
-                    if field_type == 17:
-                        continue
-                    if field_type == 1:
-                        v = normalize_date(v)
-                    if field_type in [11, 13]:
-                        v = '<font face="PTAstraSerifReg" size="12">{}</font>'.format(v.replace("&lt;br/&gt;", " "))
-                    if r.field.get_title(force_type=field_type) != "":
-                        fwb.append(
-                            Paragraph(
-                                "<font face=\"PTAstraSerifBold\">{}:</font> {}".format(r.field.get_title(force_type=field_type).replace('<', '&lt;').replace('>', '&gt;'), v),
-                                style_ml,
-                            )
-                        )
-                    else:
-                        fwb.append(Paragraph(v, style_ml))
+    fwb = fields_result(iss, fwb)
 
     fwb.append(Spacer(1, 15 * mm))
     fwb.append(Paragraph(f"Медицинский специалист ___________________ {doc_fio}", style))
@@ -192,47 +155,9 @@ def form_02(direction: Napravleniya, iss: Issledovaniya, fwb, doc, leftnone, use
     fwb.append(Paragraph(f'{open_bold_tag}Адрес регистрации:{close_tag_bold} {address}', style_ml))
 
     title_field_result = ["Руководитель медицинской организации"]
-    for group in directory.ParaclinicInputGroups.objects.filter(research=iss.research).order_by("order"):
-        results = ParaclinicResult.objects.filter(issledovaniye=iss, field__group=group).order_by("field__order")
-        group_title = False
-        fwb.append(Spacer(1, 3 * mm))
-        if results.exists():
-            if group.show_title and group.show_title != "":
-                fwb.append(Paragraph(group.title.replace('<', '&lt;').replace('>', '&gt;'), styleBold))
-                group_title = True
-            for r in results:
-                field_type = r.get_field_type()
-                if field_type == 15:
-                    continue
-                else:
-                    v = r.value.replace('<', '&lt;').replace('>', '&gt;').replace("\n", "<br/>")
-                    if not v:
-                        continue
-                    v = v.replace('&lt;sub&gt;', '<sub>')
-                    v = v.replace('&lt;/sub&gt;', '</sub>')
-                    v = v.replace('&lt;sup&gt;', '<sup>')
-                    v = v.replace('&lt;/sup&gt;', '</sup>')
-                    v = text_to_bold(v)
-                    if field_type == 16:
-                        continue
-                    if field_type == 17:
-                        continue
-                    if field_type == 1:
-                        v = normalize_date(v)
-                    if field_type in [11, 13]:
-                        v = '<font face="PTAstraSerifReg" size="12">{}</font>'.format(v.replace("&lt;br/&gt;", " "))
-                    if r.field.get_title(force_type=field_type) != "" and not r.field.get_title(force_type=field_type) in title_field_result:
-                        fwb.append(
-                            Paragraph(
-                                "<font face=\"PTAstraSerifBold\">{}:</font> {}".format(r.field.get_title(force_type=field_type).replace('<', '&lt;').replace('>', '&gt;'), v),
-                                style_ml,
-                            )
-                        )
-                    else:
-                        if not r.field.get_title(force_type=field_type) in title_field_result:
-                            fwb.append(Paragraph(v, style_ml))
+    fwb = fields_result(iss, fwb, title_field_result)
 
-    data_fields_result = fields_result(iss, title_field_result)
+    data_fields_result = fields_result_only_title_fields(iss, title_field_result)
     main_manager = ""
     for i in data_fields_result:
         if i["title"] == "Руководитель медицинской организации":
@@ -307,7 +232,7 @@ def form_03(direction: Napravleniya, iss: Issledovaniya, fwb, doc, leftnone, use
 
     fwb.append(Spacer(1, 3 * mm))
     title_field_result = ["Кому", "От кого", "Отделение", "Дата"]
-    data_fields_result = fields_result(iss, title_field_result)
+    data_fields_result = fields_result_only_title_fields(iss, title_field_result)
     main_manager, from_who, departmnet, date_protocol = "", "", "", ""
     for i in data_fields_result:
         if i["title"] == "Кому":
@@ -364,45 +289,7 @@ def form_03(direction: Napravleniya, iss: Issledovaniya, fwb, doc, leftnone, use
     address = ind_data['main_address']
     fwb.append(Paragraph(f'{open_bold_tag}Адрес регистрации:{close_tag_bold} {address}', style_ml))
 
-    for group in directory.ParaclinicInputGroups.objects.filter(research=iss.research).order_by("order"):
-        results = ParaclinicResult.objects.filter(issledovaniye=iss, field__group=group).order_by("field__order")
-        group_title = False
-        fwb.append(Spacer(1, 3 * mm))
-        if results.exists():
-            if group.show_title and group.show_title != "":
-                fwb.append(Paragraph(group.title.replace('<', '&lt;').replace('>', '&gt;'), styleBold))
-                group_title = True
-            for r in results:
-                field_type = r.get_field_type()
-                if field_type == 15:
-                    continue
-                else:
-                    v = r.value.replace('<', '&lt;').replace('>', '&gt;').replace("\n", "<br/>")
-                    if not v:
-                        continue
-                    v = v.replace('&lt;sub&gt;', '<sub>')
-                    v = v.replace('&lt;/sub&gt;', '</sub>')
-                    v = v.replace('&lt;sup&gt;', '<sup>')
-                    v = v.replace('&lt;/sup&gt;', '</sup>')
-                    v = text_to_bold(v)
-                    if field_type == 16:
-                        continue
-                    if field_type == 17:
-                        continue
-                    if field_type == 1:
-                        v = normalize_date(v)
-                    if field_type in [11, 13]:
-                        v = '<font face="PTAstraSerifReg" size="12">{}</font>'.format(v.replace("&lt;br/&gt;", " "))
-                    if r.field.get_title(force_type=field_type) != "" and not r.field.get_title(force_type=field_type) in title_field_result:
-                        fwb.append(
-                            Paragraph(
-                                "<font face=\"PTAstraSerifBold\">{}:</font> {}".format(r.field.get_title(force_type=field_type).replace('<', '&lt;').replace('>', '&gt;'), v),
-                                style_ml,
-                            )
-                        )
-                    else:
-                        if not r.field.get_title(force_type=field_type) in title_field_result:
-                            fwb.append(Paragraph(v, style_ml))
+    fwb = fields_result(iss, fwb, title_field_result)
 
     fwb.append(Spacer(1, 7 * mm))
     opinion = [
