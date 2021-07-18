e93a11deecfda8aea1ac720664f04c5f576501be
Sergei Kasianenko <monitor84@list.ru>
2021-07-18 17:13:38 +0800

    urls

--- api/urls.py
+++ api/urls.py
@@ -56,6 +56,7 @@ urlpatterns = [
     path('plans/', include('api.plans.urls')),
     path('doctor-call/', include('api.doctor_call.urls')),
     path('extra-notification/', include('api.extra_notification.urls')),
+    path('monitorings/', include('api.monitorings.urls')),
     path('list-wait/', include('api.list_wait.urls')),
     path('procedural-list/', include('api.procedure_list.urls')),
     path('parse-file/', include('api.parse_file.urls')),
