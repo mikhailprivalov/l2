92ac131318b9310d26eabea92ae14165fba2e16e
Mikhail Privalov <n5250@ya.ru>
2021-08-24 02:50:10 +0300

    fixes form

--- l2-frontend/src/ui-cards/SelectedResearches.vue
+++ l2-frontend/src/ui-cards/SelectedResearches.vue
@@ -804,7 +804,8 @@ export default {
       this.researches_direction_params = data;
     },
     async load_direction_params_data(pk) {
-      if (this.researches_direction_params?.[pk].research_data?.research?.status !== 'NOT_LOADED') {
+      const status = this.researches_direction_params?.[pk].research_data?.research?.status || 'NOT_LOADED';
+      if (status !== 'NOT_LOADED') {
         return this.researches_direction_params[pk].research_data.research;
       }
       await this.$store.dispatch(actions.INC_LOADING);
