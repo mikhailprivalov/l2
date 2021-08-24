7facff5832210e8b085d0a826e9aa60db40e3bce
Mikhail Privalov <n5250@ya.ru>
2021-08-25 00:57:15 +0300

    kladrapi base

--- api/fias.py
+++ api/fias.py
@@ -1,11 +1,15 @@
 import pickle
 
+import logging
 import requests
 import simplejson as json
 from django.core.cache import cache
 
 from appconf.manager import SettingManager
 from rmis_integration.client import get_md5
+
+
+logger = logging.getLogger(__name__)
 
 BASE_URL = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/{}"
 
@@ -27,3 +31,27 @@ def suggest(query, resource='address', count=5):
         result = pickle.loads(result, encoding="utf8")
 
     return result
+
+
+def kladrapi_request(data: dict):
+    kladrapi_url = SettingManager.get('kladrapi_url', default='https://kladr-api.ru/api.php', default_type='s')
+    token = SettingManager.get('kladrapi_token', default='', default_type='s')
+    try:
+        if token:
+            data['token'] = token
+
+        key = f"kladrapi_request:{get_md5(json.dumps(data, sort_keys=True))}"
+        result = cache.get(key)
+
+        if not result:
+            result = requests.get(kladrapi_url, params=data).json()
+            if not result or not isinstance(result, dict) or 'result' not in result:
+                return {}
+            cache.set(key, pickle.dumps(result, protocol=4), 24 * 3600)
+        else:
+            result = pickle.loads(result, encoding="utf8")
+
+        return result
+    except Exception as e:
+        logger.exception(e)
+        return {}
