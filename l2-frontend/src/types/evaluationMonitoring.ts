1f7c05a59009bed4d4f3a4eca131aea23a32043f
D00dleman <vbif1099@gmail.com>
2021-09-14 16:22:07 +0800

    Update evaluationMonitoring.ts

--- l2-frontend/src/types/evaluationMonitoring.ts
+++ l2-frontend/src/types/evaluationMonitoring.ts
@@ -1,44 +1,44 @@
 export interface EvaluationMonitoringGrade {
-    grade: string | null,
-    comment: string | null,
-    grader: string | null,
+  grade: string | null,
+  comment: string | null,
+  grader: string | null,
 }
 
 export interface EvaluationMonitoringField {
-    result_id: number,
-    field_id: number,
-    value_aggregate: number,
-    value_text: string,
+  result_id: number,
+  field_id: number,
+  value_aggregate: number,
+  value_text: string,
 }
-  
+
 export interface IEvaluationMonitoringGroup {
-    title: string,
-    fields: EvaluationMonitoringField[],
-    grade: EvaluationMonitoringGrade,
+  title: string,
+  fields: EvaluationMonitoringField[],
+  grade: EvaluationMonitoringGrade,
 }
 
 export class EvaluationMonitoringGroup {
-    title: string;
-    fields: EvaluationMonitoringField[];
-    editing: boolean;
-    grade: EvaluationMonitoringGrade;
-	
-	constructor(group: IEvaluationMonitoringGroup) {
-		this.title = group.title;
-		this.fields = group.fields;
-		this.editing = false;
-		this.grade = group.grade;
+  title: string;
+  fields: EvaluationMonitoringField[];
+  editing: boolean;
+  grade: EvaluationMonitoringGrade;
+
+  constructor(group: IEvaluationMonitoringGroup) {
+    this.title = group.title;
+    this.fields = group.fields;
+    this.editing = false;
+    this.grade = group.grade;
 	}
 	
-	public edit() {
-		this.editing = true;
+  public edit() {
+    this.editing = true;
 	}
 	
-	public cancel_edit() {
-		this.editing = false;
+  public cancel_edit() {
+    this.editing = false;
 	}
 	
-	public can_view_field() {
-		return this.grade.grade !== null && !this.editing;
-	}
+  public can_view_field() {
+    return this.grade.grade !== null && !this.editing;
+  }
 }
