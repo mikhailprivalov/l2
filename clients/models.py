14f416a1daddd1b32b80b12ef9efbf6c4d0d6678
Sergei Kasianenko <monitor84@list.ru>
2021-09-14 23:16:02 +0800

    add param to api

--- clients/models.py
+++ clients/models.py
@@ -945,6 +945,7 @@ class Card(models.Model):
     medbook_type = models.CharField(max_length=6, choices=MEDBOOK_TYPES, blank=True, default=MEDBOOK_TYPES[0][0], help_text="Тип номера мед.книжки")
 
     time_add = models.DateTimeField(default=timezone.now, null=True, blank=True)
+    n3_id = models.CharField(max_length=40, help_text='N3_ID', blank=True, default="")
 
     @property
     def main_address_full(self):
