34939d3f919f8511e4b0341cc6921c5b2d07f07d
D00dleman <vbif1099@gmail.com>
2021-09-14 12:37:08 +0800

    Update EvaluationMonitoringFastEditor.vue

--- l2-frontend/src/ui-cards/EvaluationMonitoringFastEditor.vue
+++ l2-frontend/src/ui-cards/EvaluationMonitoringFastEditor.vue
@@ -1,13 +1,28 @@
 <template>
   <div v-frag>
     <div class="input-group">
-      <input type="number" class="form-control" @keypress.enter="save" v-model="grade"      placeholder="Оценка"      style="width: 15%;" min="0"/>
-      <input type="text"   class="form-control" @keypress.enter="save" v-model="comment"    placeholder="Комментарий" style="width: 85%;"/>
+      <input 
+        type="number" 
+        class="form-control" 
+        @keypress.enter="save" 
+        v-model="grade" 
+        laceholder="Оценка" 
+        style="width: 15%;" 
+        min="0"
+       />
+      <input 
+        type="text" 
+        class="form-control" 
+        @keypress.enter="save" 
+        v-model="comment" 
+        placeholder="Комментарий" 
+        style="width: 85%;"
+       />
       <span class="input-group-btn">
         <button class="btn btn-blue-nb" title="Сохранить" @click="save(false)" v-tippy v-if="!loading">
           <i class="fas fa-save"></i>
         </button>
-        <button class="btn btn-blue-nb" title="Отмена" @click="close()" v-tippy v-if="data.editing">
+        <button class="btn btn-blue-nb" title="Отмена" @click="close()" v-tippy>
          <i class="fas fa-close">×</i>
         </button>
       </span>
@@ -61,20 +76,8 @@ export default class ExtraNotificationFastEditor extends Vue {
 
   $dialog: any;
 
-  get value() {
-    return 123;
-  }
-
-  get editing() {
-    return true;
-  }
-
-  get valid() {
-    return true;
-  }
-
-  async save(withConfirm = false) {
-    if(!/^(\d+|\d+\.\d+|\d+\,\d+)$/.test(String(this.grade))) {
+  async save() {
+    if (!RegExp("^(\\d+|\\d+\\.\\d+|\\d+\\,\\d+)$").test(String(this.grade))) {
       this.$root.$emit('msg', 'error', 'Введите положительное число или ноль в поле оценки');
       return;
     }
@@ -85,9 +88,9 @@ export default class ExtraNotificationFastEditor extends Vue {
     const {
       ok, message,
     } = await this.$api('evaluation_monitoring/add_result', {
-          result_id: this.data.fields[0].result_id,
-          grade: this.grade,
-          comment: this.comment,
+      result_id: this.data.fields[0].result_id,
+      grade: this.grade,
+      comment: this.comment,
     });
     this.$emit('sendData', this.data);
     await this.$store.dispatch(actions.DEC_LOADING);
@@ -96,7 +99,7 @@ export default class ExtraNotificationFastEditor extends Vue {
       this.$root.$emit('msg', 'ok', message);
     } else {
       this.$root.$emit('msg', 'error', message);
-    }  
+    }
   }
 
   close() {
