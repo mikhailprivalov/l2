21c4c99817b07ccb98ee95f9ff13eb94b62d99d6
Sergei Kasianenko <monitor84@list.ru>
2021-09-18 22:59:12 +0800

    new typefield

--- api/directions/sql_func.py
+++ api/directions/sql_func.py
@@ -225,3 +225,14 @@ def get_confirm_direction_patient_year(d_s, d_e, lab_podr, card_pk1, is_lab=Fals
         )
         rows = namedtuplefetchall(cursor)
     return rows
+
+
+# def get_diagnoses(d_type, diag_title, diag_mkb):
+#     with connection.cursor() as cursor:
+#         cursor.execute(
+#         """
+#         """,
+#             params={"d_type": d_type, "diag_title": diag_title, "diag_mkb": diag_mkb},
+#         )
+#         rows = namedtuplefetchall(cursor)
+#     return rows
