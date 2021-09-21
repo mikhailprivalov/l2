0ed5a52ad42306709de7670ddcd3504b913b8a38
Sergei Kasianenko <monitor84@list.ru>
2021-09-21 13:12:08 +0800

    универсальный поиск мкб

--- api/directions/sql_func.py
+++ api/directions/sql_func.py
@@ -233,24 +233,3 @@ def get_confirm_direction_patient_year(d_s, d_e, lab_podr, card_pk1, is_lab=Fals
         )
         rows = namedtuplefetchall(cursor)
     return rows
-
-
-def get_diagnoses(d_type="mkb10.4", diag_title="-1", diag_mkb="-1"):
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """
-        SELECT * FROM public.directions_diagnoses
-            WHERE d_type=%(d_type)s and 
-              CASE
-                WHEN %(diag_title)s != '-1' AND %(diag_mkb)s != '-1' THEN 
-                  code ~* %(diag_mkb)s and title ~* %(diag_title)s
-                WHEN %(diag_title)s != '-1' AND %(diag_mkb)s = '-1' THEN 
-                  title ~* %(diag_title)s
-                WHEN %(diag_title)s = '-1' AND %(diag_mkb)s != '-1' THEN 
-                  code ~* %(diag_mkb)s
-              END
-        """,
-            params={"d_type": d_type, "diag_title": diag_title, "diag_mkb": diag_mkb},
-        )
-        rows = namedtuplefetchall(cursor)
-    return rows
