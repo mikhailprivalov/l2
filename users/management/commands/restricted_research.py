e626d5d8c8c4a8eaf44cbd1c4e7b25195c29db1a
Sergei Kasianenko <monitor84@list.ru>
2021-09-30 12:16:25 +0800

    пакетно запретить всем назначение услуги

---
+++ users/management/commands/restricted_research.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from directory.models import Researches
+import users.models as users
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('research_pk', type=int)
+
+    def handle(self, *args, **kwargs):
+        research_pk = kwargs["research_pk"]
+        r = Researches.objects.get(pk=research_pk)
+        us = users.DoctorProfile.objects.all()
+
+        for u in us:
+            u.restricted_to_direct.add(r)
+            u.save()
