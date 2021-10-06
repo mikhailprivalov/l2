1c323780503cda82d85b4d24fe782f3f97e311d1
Sergei Kasianenko <monitor84@list.ru>
2021-10-06 22:12:37 +0800

    fix

--- l2-frontend/src/ui-cards/FieldHelper.vue
+++ l2-frontend/src/ui-cards/FieldHelper.vue
@@ -78,7 +78,7 @@ const DB_LINKS = {
   '%harmful_factor': 'Фактор вредности пациента',
   '%proto_operation': 'proto_operation',
   '%proto_description': 'proto_description',
-  '%doc_position': 'Должноть врача',
+  '%doc_position': 'Должность врача',
 };
 
 export default {
