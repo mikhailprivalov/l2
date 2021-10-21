626c2e04eeca48125e137244e5b2f1fe5916ef82
Mikhail Privalov <n5250@ya.ru>
2021-10-21 03:44:20 +0300

    fix

--- api/researches/views.py
+++ api/researches/views.py
@@ -704,7 +704,7 @@ def fast_template_save(request):
 def fraction_title(request):
     request_data = json.loads(request.body)
     fraction = Fractions.objects.get(pk=request_data["pk"])
-    return JsonResponse({"fraction": fraction.title, "research": fraction.research.title, "units": fraction.f.get_unit()})
+    return JsonResponse({"fraction": fraction.title, "research": fraction.research.title, "units": fraction.get_unit()})
 
 
 def field_title(request):
