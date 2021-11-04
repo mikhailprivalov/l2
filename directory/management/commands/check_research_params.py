c3117b8b7c9ad04835db174f6dfbca2391b04f31
Sergei Kasianenko <monitor84@list.ru>
2021-11-04 16:46:34 +0800

    fix

--- directory/management/commands/check_research_params.py
+++ directory/management/commands/check_research_params.py
@@ -25,7 +25,7 @@ class Command(BaseCommand):
                                 unit_title = f.unit.title
                                 unit_code = f.unit.code
                             fsli = f.fsli if f.fsli else "ФСЛИ не заполнено"
-                            print(f"{k}@{r.podrazdeleniye.title}@ {r.title}@ {research_code}@ {r.hide}@ - @{f.title}@ {fsli}@ {unit_title}@ {unit_code}")
+                            print(f"{k}@{r.podrazdeleniye.title}@ {r.title}@ {research_code}@ {r.hide}@ - @{f.title}@ {fsli}@ {unit_title}@ {unit_code}") # noqa: T001
         if type_researches == "is_paraclinic":
             researches = Researches.objects.filter(is_paraclinic=True, hide=False)
             k = 0
@@ -33,5 +33,5 @@ class Command(BaseCommand):
                 k += 1
                 research_code = r.code if r.code else "Код НМУ услуги не заполнен"
                 nsi_id = r.nsi_id if r.nsi_id else "НСИ исследования не заполнен"
-                print(f"{k}@{r.title}@ {r.short_title}@{r.podrazdeleniye.title}@{research_code}@ {nsi_id}")
+                print(f"{k}@{r.title}@ {r.short_title}@{r.podrazdeleniye.title}@{research_code}@ {nsi_id}") # noqa: T001
 
