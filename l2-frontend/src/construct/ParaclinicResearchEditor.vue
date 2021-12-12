7fe893bc3d2e4d8f78d4ec0e19473ab156fe7853
Sergei Kasianenko <monitor84@list.ru>
2021-12-12 21:49:33 +0800

    из назнвание поля

--- l2-frontend/src/construct/ParaclinicResearchEditor.vue
+++ l2-frontend/src/construct/ParaclinicResearchEditor.vue
@@ -437,7 +437,7 @@
                       <option value="31" v-if="tfoms_attachment_field_enabled">
                         Сведения о прикреплении застрахованного лица (ТФОМС)
                       </option>
-                      <option value="35">Врач</option>
+                      <option value="35">Лицо от организации придавшее юр. силу документу</option>
                     </select>
                   </label>
                 </div>
