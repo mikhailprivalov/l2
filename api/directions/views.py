049c9db6522d8fd6b8135a0daf98124e6f599ad6
Mikhail Privalov <n5250@ya.ru>
2021-12-25 22:16:08 +0300

    Делаем получение экспертизных данных опциональным

--- api/directions/views.py
+++ api/directions/views.py
@@ -3270,17 +3270,18 @@ def eds_to_sign(request):
 
 
 def get_expertise(pk):
-    iss_pk = Issledovaniya.objects.filter(napravleniye_id=pk).values_list('pk').first()
-    expertise_from_sql = expertise_tree_direction(iss_pk) if iss_pk else []
     expertise_data = []
-    for i in expertise_from_sql:
-        if i.level == 2 and i.is_expertise:
-            not_remarks = False
-            if i.date_confirm:
-                result_protocol = get_json_protocol_data(i.napravleniye_id)
-                content = result_protocol["content"]
-                if content and content.get("Наличие замечаний"):
-                    if content["Наличие замечаний"].lower() == "нет":
-                        not_remarks = True
-            expertise_data.append({"direction": i.napravleniye_id, "confirm": i.date_confirm, "not_remarks": not_remarks})
+    if SettingManager.l2('expertise'):
+        iss_pk = Issledovaniya.objects.filter(napravleniye_id=pk).values_list('pk').first()
+        expertise_from_sql = expertise_tree_direction(iss_pk) if iss_pk else []
+        for i in expertise_from_sql:
+            if i.level == 2 and i.is_expertise:
+                not_remarks = False
+                if i.date_confirm:
+                    result_protocol = get_json_protocol_data(i.napravleniye_id)
+                    content = result_protocol["content"]
+                    if content and content.get("Наличие замечаний"):
+                        if content["Наличие замечаний"].lower() == "нет":
+                            not_remarks = True
+                expertise_data.append({"direction": i.napravleniye_id, "confirm": i.date_confirm, "not_remarks": not_remarks})
     return expertise_data
