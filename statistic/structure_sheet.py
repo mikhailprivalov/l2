9e9264dc5a0fc4bc0e95a58d3be716d7c8fc35b1
Sergei Kasianenko <monitor84@list.ru>
2022-01-07 23:21:19 +0800

    fixReport_Death

--- statistic/structure_sheet.py
+++ statistic/structure_sheet.py
@@ -801,9 +801,8 @@ def statistic_research_death_data(ws1, researches):
 
         ws1.cell(row=r, column=26).value = place_death_details
         # Название стационара
-        ws1.cell(row=r, column=27).value = ""
+        ws1.cell(row=r, column=27).value = i.get("МО", "")
         # ДТП
-
         ws1.cell(row=r, column=28).value = i["ДТП"]
         ws1.cell(row=r, column=29).value = i["Беременность"]
 
@@ -913,7 +912,7 @@ def statistic_research_death_data_card(ws1, researches):
 
         ws1.cell(row=r, column=26).value = place_death_details
         # Название стационара
-        ws1.cell(row=r, column=27).value = ""
+        ws1.cell(row=r, column=27).value = i.get("МО", "")
         # ДТП
         ws1.cell(row=r, column=28).value = i["ДТП"]
         ws1.cell(row=r, column=29).value = i["Беременность"]
