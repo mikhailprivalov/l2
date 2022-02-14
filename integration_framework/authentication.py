ee8be653447245714f426d8716f4611c5cc5beda
Mikhail Privalov <n5250@ya.ru>
2022-02-15 02:42:39 +0800

    check token activity

--- integration_framework/authentication.py
+++ integration_framework/authentication.py
@@ -28,6 +28,8 @@ def can_use_schedule_only(function):
         if not hasattr(request.user, 'can_access_schedule'):
             raise exceptions.AuthenticationFailed('Invalid auth token')
         app: Application = request.user
+        if not app.active:
+            raise exceptions.AuthenticationFailed('The token is expired')
         if not app.can_access_schedule:
             raise exceptions.AuthenticationFailed("The token does not have access to the schedule")
         return function(request, *args, **kwargs)
