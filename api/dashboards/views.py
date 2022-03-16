8abfb45990296806383fddff7e73943d6b56ab14
Sergei Kasianenko <monitor84@list.ru>
2022-03-16 13:48:09 +0800

    добавлено поле: одновление дашборда в секундах

--- api/dashboards/views.py
+++ api/dashboards/views.py
@@ -4,6 +4,7 @@ import logging
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 
+from dashboards.models import Dashboard
 from laboratory.decorators import group_required
 from dashboards.views import exec_query, get_dashboard
 
@@ -22,12 +23,13 @@ def dashboard_list(request):
 @group_required("Просмотр графиков статистики")
 def dashboard_charts(request):
     request_data = json.loads(request.body)
-    dashboard_pk = request_data["dashboard"]
+    dashboard_pk = request_data.get("dashboard", -1)
 
     try:
         result = exec_query(dashboard_pk)
     except Exception as e:
         logger.exception(e)
         return JsonResponse({"ok": False})
+    dash=Dashboard.objects.get(pk=dashboard_pk)
 
-    return JsonResponse({'rows': result, "ok": True})
+    return JsonResponse({'rows': result, "ok": True, "intervalReloadSeconds": dash.interval_reload_seconds})
