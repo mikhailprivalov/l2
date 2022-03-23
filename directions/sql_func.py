a531a08b271e1500387effcfc883cb2292b40963
Sergei Kasianenko <41939763+urchinpro@users.noreply.github.com>
2022-03-23 16:09:12 +0800

    Update directions/sql_func.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- directions/sql_func.py
+++ directions/sql_func.py
@@ -3,7 +3,9 @@ from laboratory.settings import TIME_ZONE
 from utils.db import namedtuplefetchall
 
 
-def check_limit_assign_researches(district_group_id,):
+def check_limit_assign_researches(
+    district_group_id,
+):
 
     with connection.cursor() as cursor:
         cursor.execute(
