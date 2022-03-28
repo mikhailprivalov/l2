eaa904aec4a87095dbf0f6ab4457a4bcb958e20f
Sergei Kasianenko <monitor84@list.ru>
2022-03-28 20:11:24 +0800

    admin: dash

--- dashboards/admin.py
+++ dashboards/admin.py
@@ -21,10 +21,69 @@ class ResDashboardDataSet(admin.ModelAdmin):
         'connect',
         'sql_columns_settings',
     )
+    list_display_links = (
+        'title',
+        'connect',
+        'sql_columns_settings',
+    )
 
+
+class ResDashboardChartData(admin.ModelAdmin):
+    list_display = (
+        'dashboard_title',
+        'chart',
+        'data_set',
+        'order',
+        'hide',
+    )
+    list_display_links = (
+        'dashboard_title',
+        'chart',
+        'data_set',
+        'order',
+        'hide',
+    )
+
+    def dashboard_title(self, obj):
+        if obj.chart.dashboard:
+            return obj.chart.dashboard.title
+        else:
+            return ""
+
+
+class ResDashboardCharts(admin.ModelAdmin):
+    list_display = (
+        'dashboard',
+        'title',
+        'default_type',
+        'order',
+        'hide',
+    )
+    list_display_links = (
+        'dashboard',
+        'title',
+        'default_type',
+        'order',
+        'hide',
+    )
+
+
+class ResDashboard(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'order',
+        'interval_reload_seconds',
+        'hide',
+    )
+    list_display_links = (
+        'title',
+        'order',
+        'interval_reload_seconds',
+        'hide',
+    )
 
 admin.site.register(DatabaseConnectSettings, ResDatabaseConnectSettings)
-admin.site.register(Dashboard)
-admin.site.register(DashboardCharts)
-admin.site.register(DashboardDataSet)
-admin.site.register(DashboardChartData)
+admin.site.register(Dashboard, ResDashboard)
+admin.site.register(DashboardCharts, ResDashboardCharts)
+admin.site.register(DashboardDataSet, ResDashboardDataSet)
+admin.site.register(DashboardChartData, ResDashboardChartData)
