5aea12a0d7360faebdfce370abe390476502110d
Sergei Kasianenko <monitor84@list.ru>
2022-04-12 10:52:54 +0800

    сортировка плана операций

--- api/plans/sql_func.py
+++ api/plans/sql_func.py
@@ -15,7 +15,8 @@ def get_plans_by_params_sql(d_s, d_e, doc_operate_id, doc_anesthetist_id, depart
         t_plans AS 
             (SELECT id as pk_plan, patient_card_id, direction,
             to_char(date AT TIME ZONE %(tz)s, 'DD.MM.YYYY') AS date_char, 
-            type_operation, doc_operate_id, doc_anesthetist_id, canceled, date  FROM plans_planoperations
+            type_operation, doc_operate_id, doc_anesthetist_id, canceled, date
+            FROM plans_planoperations
             WHERE 
             CASE when %(doc_operate_id)s > -1 THEN 
             doc_operate_id = %(doc_operate_id)s AND date AT TIME ZONE %(tz)s BETWEEN %(d_start)s AND %(d_end)s
@@ -34,10 +35,12 @@ def get_plans_by_params_sql(d_s, d_e, doc_operate_id, doc_anesthetist_id, depart
              LEFT JOIN clients_card ON clients_individual.id = clients_card.individual_id
              WHERE clients_card.id in (SELECT patient_card_id FROM t_plans))
         
-        
         SELECT pk_plan, patient_card_id, direction, date_char, type_operation, doc_operate_id, doc_anesthetist_id, canceled,
                ind_family, ind_name, ind_twoname, birthday, date FROM t_plans
-        LEFT JOIN t_patient ON t_plans.patient_card_id = t_patient.card_id ORDER BY date
+        LEFT JOIN t_patient ON t_plans.patient_card_id = t_patient.card_id 
+        LEFT JOIN users_doctorprofile ON users_doctorprofile.id=doc_operate_id
+        ORDER BY date, users_doctorprofile.podrazdeleniye_id
+        
         """,
             params={'d_start': d_s, 'd_end': d_e, 'tz': TIME_ZONE, 'doc_operate_id': doc_operate_id, 'doc_anesthetist_id': doc_anesthetist_id, 'department_id': department},
         )
