c17b1f3cde5dcdf0cb14a234ee5dccb278ee2651
Sergei Kasianenko <monitor84@list.ru>
2022-05-23 20:13:35 +0800

    .

--- results/forms/forms102.py
+++ results/forms/forms102.py
@@ -139,3 +139,167 @@ def form_01(direction, iss, fwb, doc, leftnone, user=None):
                         fwb.append(Paragraph(v, style))
 
     return fwb
+
+
+def form_02(direction, iss, fwb, doc, leftnone, user=None):
+    # ПАТОЛОГО-АНАТОМИЧЕСКОЕ заключение
+    styleSheet = getSampleStyleSheet()
+    style = styleSheet["Normal"]
+    style.fontName = "FreeSans"
+    style.fontSize = 10
+    style.alignment = TA_JUSTIFY
+
+    style_ml = deepcopy(style)
+    style_ml.leftIndent = 5 * mm
+    style_ml.spaceAfter = 0.5 * mm
+
+    styleBold = deepcopy(style)
+    styleBold.fontName = "FreeSansBold"
+
+    hospital_name = SettingManager.get("org_title")
+    hospital_address = SettingManager.get("org_address")
+    hospital_kod_ogrn = SettingManager.get("org_ogrn")
+
+    styleT = deepcopy(style)
+    styleT.alignment = TA_LEFT
+    styleT.fontSize = 10
+    styleT.leading = 4.5 * mm
+
+    opinion = [
+        [
+            Paragraph(f'<font size=10>{hospital_name}<br/>Адрес: {hospital_address}<br/>ОГРН: {hospital_kod_ogrn} <br/> </font>', styleT),
+            Paragraph('<font size=9 >Медицинская документация <br/> Учетная форма № 014/1-у<br/>Утверждена приказом Минздрава России<br/>от 24 марта 2016г. № 179н</font>', styleT),
+        ],
+    ]
+
+    tbl = Table(opinion, 2 * [100 * mm])
+    tbl.setStyle(
+        TableStyle(
+            [
+                ('GRID', (0, 0), (-1, -1), 0.75, colors.white),
+                ('LEFTPADDING', (1, 0), (-1, -1), 35 * mm),
+                ('LEFTPADDING', (0, 0), (0, -1), 15 * mm),
+                ('VALIGN', (0, 0), (-1, -1), 'TOP'),
+            ]
+        )
+    )
+
+    fwb.append(tbl)
+    fwb.append(Spacer(1, 5 * mm))
+
+    history_num = ''
+    if direction.parent and direction.parent.research.is_hospital:
+        history_num = f"(cтационар-{str(direction.parent.napravleniye_id)})"
+
+    styleCenterBold = deepcopy(style)
+    styleCenterBold.alignment = TA_CENTER
+    styleCenterBold.fontSize = 11.5
+    styleCenterBold.leading = 15
+    styleCenterBold.fontName = 'FreeSansBold'
+
+    fwb.append(Paragraph(f'ПРОТОКОЛ № {direction.pk} {history_num} ', styleCenterBold))
+    fwb.append(Paragraph('ПРИЖИЗНЕННОГО ПАТОЛОГО-АНАТОМИЧЕСКОГО<br/> ИССЛЕДОВАНИЯ БИОПСИЙНОГО (ОПЕРАЦИОННОГО) МАТЕРИАЛА', styleCenterBold))
+    short_title = iss.research.short_title
+    fwb.append(Paragraph(f'{short_title.upper()}', styleCenterBold))
+
+    open_bold_tag = "<font face =\"FreeSansBold\">"
+    close_tag_bold = "</font>"
+
+    title_fields = [
+        "Отделение направившее",
+        "ФИО пациента",
+        "Пол",
+        "Дата рождения",
+        "Полис ОМС",
+        "СНИЛС",
+        "Место регистрации",
+        "Местность",
+        "Диагноз по направлению",
+        "Код по МКБ из направления",
+        "Дата забора",
+        "Время забора",
+        "Материал в 10% растворе",
+        "Загрязнен",
+        "Дата поступления",
+        "Время поступления",
+        "Сохранность упаковки",
+        "Дата регистрации",
+        "Время регистрации",
+        "Регистрационный номер",
+        "Медицинские услуги",
+        "Категория сложности",
+        "Дата вырезки",
+        "Время вырезки",
+        "В проводку взято",
+        "Окраски",
+        "Макроскопическое описание",
+        "Микроскопическое описание",
+        "Заключение",
+        "Код по МКБ",
+        "Комментарии к заключению и рекомендации",
+        "Врач-консультант",
+    ]
+    result = fields_result_only_title_fields(iss, title_fields, False)
+    data= {}
+    for i in result:
+        data[i["title"]] = i["value"]
+
+    if not data.get("Отделение направившее"):
+        data["Отделение направившее"] = "не указано"
+
+    fwb.append(Spacer(1, 4 * mm))
+    fwb.append(Paragraph(f'{open_bold_tag}1. Отделение, направившее биопсийный (операционный) материал:{close_tag_bold}{data["Отделение направившее"]}', style_ml))
+    fwb.append(Paragraph(f'{open_bold_tag}2. Фамилия, имя, отчество (при наличии) пациента:{close_tag_bold} {data["ФИО пациента"]}', style_ml))
+    if data["Пол"] == "м":
+        sex = f'{data["Пол"]}-1'
+    else:
+        sex = f'{data["Пол"]}-2'
+    space_symbol = '&nbsp;'
+    fwb.append(Paragraph(f'{open_bold_tag}3. Пол:{close_tag_bold} {sex}, {space_symbol * 5} {open_bold_tag}4. Дата рождения:{close_tag_bold} {data["Дата рождения"]}', style_ml))
+    if not data.get("Полис ОМС"):
+        data["Полис ОМС"] = ""
+    if not data.get("СНИЛС"):
+        data["СНИЛС"] = ""
+    fwb.append(Paragraph(f'{open_bold_tag}5. Полис ОМС:{close_tag_bold}{data["Полис ОМС"]} {space_symbol * 4} {open_bold_tag}6. СНИЛС:{close_tag_bold} {data["СНИЛС"]}', style_ml))
+    fwb.append(Paragraph(f'{open_bold_tag}7. Место регистрации:{close_tag_bold} {data["Место регистрации"]}', style_ml))
+    fwb.append(Paragraph(f'{open_bold_tag}8. Местность:{close_tag_bold} городская — 1, сельская — 2.', style_ml))
+
+    for group in directory.ParaclinicInputGroups.objects.filter(research=iss.research).order_by("order"):
+        results = ParaclinicResult.objects.filter(issledovaniye=iss, field__group=group).order_by("field__order")
+        group_title = False
+        if results.exists():
+            fwb.append(Spacer(1, 1 * mm))
+            if group.show_title and group.show_title != "":
+                fwb.append(Paragraph(group.title.replace('<', '&lt;').replace('>', '&gt;'), styleBold))
+                fwb.append(Spacer(1, 0.25 * mm))
+                group_title = True
+            for r in results:
+                field_type = r.get_field_type()
+                if field_type == 15:
+                    continue
+                else:
+                    v = r.value.replace('<', '&lt;').replace('>', '&gt;').replace("\n", "<br/>")
+                    v = v.replace('&lt;sub&gt;', '<sub>')
+                    v = v.replace('&lt;/sub&gt;', '</sub>')
+                    v = v.replace('&lt;sup&gt;', '<sup>')
+                    v = v.replace('&lt;/sup&gt;', '</sup>')
+                    v = text_to_bold(v)
+                    if field_type == 16:
+                        continue
+                    if field_type == 17:
+                        continue
+                    if field_type == 1:
+                        v = normalize_date(v)
+                    if field_type in [11, 13]:
+                        v = '<font face="FreeSans" size="8">{}</font>'.format(v.replace("&lt;br/&gt;", " "))
+                    if r.field.get_title(force_type=field_type) != "":
+                        fwb.append(
+                            Paragraph(
+                                "<font face=\"FreeSansBold\">{}:</font>{}".format(r.field.get_title(force_type=field_type).replace('<', '&lt;').replace('>', '&gt;'), v),
+                                style_ml if group_title else style,
+                            )
+                        )
+                    else:
+                        fwb.append(Paragraph(v, style))
+
+    return fwb
