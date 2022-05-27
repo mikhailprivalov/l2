a445195423bbf7a6d79c2b73576cb4a147b5c8c8
Sergei Kasianenko <41939763+urchinpro@users.noreply.github.com>
2022-05-27 22:04:02 +0800

    Update staff/admin.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- staff/admin.py
+++ staff/admin.py
@@ -37,6 +37,13 @@ class ResEmployees(admin.ModelAdmin):
         'tabel_number',
     )
     search_fields = ('tabel_number',)
+    list_display_links = (
+        'tabel_number',
+        'person',
+        'department',
+        'tabel_number',
+    )
+    search_fields = ('tabel_number',)
 
     list_filter = ('department',)
 
