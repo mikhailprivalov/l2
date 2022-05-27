1cc1530434e716bc4ebca8cb0b0c6544bff0fe5c
Sergei Kasianenko <monitor84@list.ru>
2022-05-27 11:19:26 +0800

    св-ва расширенного типа поля

--- directory/models.py
+++ directory/models.py
@@ -582,7 +582,8 @@ class ParaclinicInputField(models.Model):
     control_param = models.TextField(default='', blank=True)
     operator_enter_param = models.BooleanField(default=False, help_text='Поле ввода для оператора(лаборанта)', blank=True)
     enable_rows = models.BooleanField(default=False, help_text='Можно добавлять строки', blank=True)
-    set_fields = models.CharField(max_length=500, help_text='Типы полей в наборе - таблицы', blank=True, default=None, null=True)
+    set_fields = models.CharField(max_length=500, help_text='Типы полей в наборе - таблицы {"тип поля: ширина"}', blank=True, default=None, null=True)
+
 
     def get_title(self, force_type=None, recursive=False):
         field_type = force_type or self.field_type
