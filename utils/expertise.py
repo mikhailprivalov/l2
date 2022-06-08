2a1fec05c9f4aa3dd353c878bd293e3dca8afda7
Sergei Kasianenko <monitor84@list.ru>
2022-06-08 09:27:34 +0800

    .

--- utils/expertise.py
+++ utils/expertise.py
@@ -5,6 +5,7 @@ from utils.tree_directions import expertise_tree_direction
 
 
 def get_expertise(pk, with_check_available=False):
+    print("asasasasa")
     expertise_data = {
         'canCreateExpertise': False,
         'serviceId': None,
