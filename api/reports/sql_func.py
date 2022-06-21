2b274dc8344bd891ab8529c38048dec9aca81e99
Sergei Kasianenko <monitor84@list.ru>
2022-06-21 09:11:10 +0800

    Фиксы

--- api/reports/sql_func.py
+++ api/reports/sql_func.py
@@ -32,7 +32,7 @@ def report_buh_gistology(directions):
         LEFT JOIN directory_paraclinicinputfield dpif on dp.field_id = dpif.id
         
         WHERE directions_issledovaniya.napravleniye_id in %(directions)s
-        ORDER BY hh.title, dn.visit_date, dn.id
+        ORDER BY dn.id, dn.visit_date 
 
         """,
             params={'directions': directions, 'tz': TIME_ZONE},
