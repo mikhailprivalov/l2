d7901254e0ff4f681d8a7faae826c09203bce18c
Sergei Kasianenko <monitor84@list.ru>
2022-07-06 19:45:23 +0800

    fix

--- l2-frontend/src/store/modules/directory.ts
+++ l2-frontend/src/store/modules/directory.ts
@@ -31,7 +31,7 @@ const getters = {
   },
   permanentDirectories: (state) => state.permanentDirectories,
   requiredStattalonFields: (state) => state.requiredStattalonFields,
-  researchesPkRequiredStattalonFields: (state) => state.researchesPkRequiredStattalonFields
+  researchesPkRequiredStattalonFields: (state) => state.researchesPkRequiredStattalonFields,
 };
 
 const actions = {
