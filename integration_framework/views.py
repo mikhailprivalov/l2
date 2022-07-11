f232f4801b73679a1d3dd5ddbf3cee9502bd7beb
Mikhail Privalov <n5250@ya.ru>
2022-07-11 20:56:20 +0800

    ECP evnid

--- integration_framework/views.py
+++ integration_framework/views.py
@@ -523,6 +523,13 @@ def make_log(request):
             d = directions.Napravleniya.objects.get(pk=k)
             d.ecp_ok = True
             d.save(update_fields=['ecp_ok'])
+            
+            iss: Issledovaniya
+            for iss in Issledovaniya.objects.filter(napravleniye_id=k):
+                if str(iss.pk) in body.get(k, {}):
+                    if 'ecpServiceId' in body[k][str(iss.pk)]:
+                        iss.ecp_evn_id = body[k][str(iss.pk)]['ecpServiceId']
+                        iss.save(update_fields=['ecp_evn_id'])
 
     return Response({"ok": True})
 
