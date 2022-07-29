275d50c7c28eecf5abaf5a3d984e814b10e7da3d
Mikhail Privalov <n5250@ya.ru>
2022-07-29 03:27:04 +0300

    fix

--- directions/management/commands/sync_confirmed_directions.py
+++ directions/management/commands/sync_confirmed_directions.py
@@ -14,6 +14,6 @@ class Command(BaseCommand):
         for n in d:
             cnt += 1
 
-            print(f"{cnt}/{total} синхронизируем {n.pk}...")
+            print(f"{cnt}/{total} синхронизируем {n.pk}...")  # noqa: T001
             n.sync_confirmed_fields()
-            print(f"total_confirmed={n.total_confirmed} last_confirmed_at={n.last_confirmed_at}")
+            print(f"total_confirmed={n.total_confirmed} last_confirmed_at={n.last_confirmed_at}")  # noqa: T001
