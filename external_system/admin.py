585479e08a7162b6a4fdc4f8603d261474cf283c
Sergei Kasianenko <monitor84@list.ru>
2022-07-30 20:36:35 +0800

    admin external_system

--- external_system/admin.py
+++ external_system/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import FsliRefbookTest, InstrumentalResearchRefbook, BodySiteRefbook
+from .models import FsliRefbookTest, InstrumentalResearchRefbook, BodySiteRefbook, ArchiveMedicalDocuments, TypesMedicalDocuments
 
 admin.site.register(FsliRefbookTest)
 admin.site.register(InstrumentalResearchRefbook)
 admin.site.register(BodySiteRefbook)
+admin.site.register(ArchiveMedicalDocuments)
+admin.site.register(TypesMedicalDocuments)
