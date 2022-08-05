6f533192bf3409cfb54ee1e8c05044fd7efe9da6
Mikhail Privalov <n5250@ya.ru>
2022-08-05 14:48:18 +0300

    Update external_system/admin.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- external_system/admin.py
+++ external_system/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import FsliRefbookTest, InstrumentalResearchRefbook, BodySiteRefbook, ArchiveMedicalDocuments, TypesMedicalDocuments
 
 
-
 class ResArchiveMedicalDocuments(admin.ModelAdmin):
     list_display = (
         'local_uid',
