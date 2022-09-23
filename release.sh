72179f403f225b8b85f02a7cfbdf3d3bf141bfd9
Mikhail Privalov <n5250@ya.ru>
2022-09-23 04:13:41 +0300

    release script

---
+++ release.sh
@@ -0,0 +1,11 @@
+make release
+
+if [ $(git rev-parse --abbrev-ref HEAD) != "develop" ]; then
+    echo "You are not on the develop branch"
+    exit 1
+fi
+
+V=$(sed -n 's/^version = "\(.*\)"/\1/p' pyproject.toml)
+
+git tag -a v$V -m "Release $V"
+git push origin v$V
