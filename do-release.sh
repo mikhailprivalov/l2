cd65ae9caf7b888e470ba5ef6f485476b7468a27
Mikhail Privalov <n5250@ya.ru>
2022-09-23 05:32:20 +0300

    Up version

--- do-release.sh
+++ do-release.sh
@@ -12,5 +12,7 @@ sleep 5
 
 V=$(sed -n 's/^version = "\(.*\)"/\1/p' pyproject.toml)
 
+echo "Releasing version $V"
+
 git tag -a v$V -m "Release $V"
 git push origin v$V
--- laboratory/__init__.py
+++ laboratory/__init__.py
@@ -1,2 +1,2 @@
-__version__ = "2022.9.23+c949d2"
+__version__ = "2022.9.23+955bc0"
 VERSION = __version__
