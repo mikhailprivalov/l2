c949d28c455aa87787e03cfe9350b9a70b99d1ba
Mikhail Privalov <n5250@ya.ru>
2022-09-23 05:29:21 +0300

    Up version

--- laboratory/__init__.py
+++ laboratory/__init__.py
@@ -1,2 +1,2 @@
-__version__ = "2022.9.23+4f9eaf-dirty"
+__version__ = "2022.9.23+e2c8c0"
 VERSION = __version__
--- update-version.sh
+++ update-version.sh
@@ -2,7 +2,7 @@
 DAY=$(date -u '+%-d')
 MONTH=$(date -u '+%-m')
 YEAR=$(date -u '+%Y')
-HASH=$(git describe --always --dirty --abbrev=6 --exclude '*')
+HASH=$(git describe --always --abbrev=6 --exclude '*')
 V="$YEAR.$MONTH.$DAY+$HASH"
 
 newVersion="__version__ = \"$V\""
