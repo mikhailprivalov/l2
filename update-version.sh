e06a53e6f06f52f8577c79a02204ee740744ef49
Mikhail Privalov <n5250@ya.ru>
2022-09-27 00:29:14 +0300

    Print info

--- update-version.sh
+++ update-version.sh
@@ -7,5 +7,8 @@ V="$YEAR.$MONTH.$DAY+$HASH"
 
 newVersion="__version__ = \"$V\""
 
+echo "New version is $V"
+echo "Current directory is $(pwd)"
+
 sed -i '' "1s/^.*$/$newVersion/" "laboratory/__init__.py"
 sed -i '' "s/^version = \".*\"/version = \"$V\"/" "pyproject.toml"
