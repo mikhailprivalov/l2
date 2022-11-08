c12812b572ac7f58388f133ffdb31a2384c73934
Sergei Kasianenko <monitor84@list.ru>
2022-11-08 21:12:58 +0800

    fix

--- forms/forms112.py
+++ forms/forms112.py
@@ -111,9 +111,9 @@ def form_01(request_data):
     objs: List[Union[Spacer, Paragraph, Table, KeepTogether]] = []
 
     if not os.path.join(BASE_DIR, 'forms', 'additionla_pages', type_additional_pdf):
-        additional_data_from_file = os.path.join(BASE_DIR, 'forms', 'additionla_pages', "default")
+        additional_data_from_file = os.path.join(BASE_DIR, 'forms', 'additionla_pages', "default.json")
     else:
-        additional_data_from_file = os.path.join(BASE_DIR, 'forms', 'additional_pages', "default")
+        additional_data_from_file = os.path.join(BASE_DIR, 'forms', 'additional_pages', type_additional_pdf)
     if additional_data_from_file:
         with open(additional_data_from_file) as json_file:
             data = json.load(json_file)
