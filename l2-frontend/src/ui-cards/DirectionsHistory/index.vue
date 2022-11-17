f262f24c7b7c70f6b3f27f2b50b830c464fb5155
Sergei Kasianenko <monitor84@list.ru>
2022-11-17 11:35:23 +0800

    роль "Отмена направлений". по умолчанию разрешена отмена

--- l2-frontend/src/ui-cards/DirectionsHistory/index.vue
+++ l2-frontend/src/ui-cards/DirectionsHistory/index.vue
@@ -622,6 +622,9 @@ export default {
       await this.$store.dispatch(actions.INC_LOADING);
 
       const data = await directionsPoint.cancelDirection({ pk });
+      if (data.forbidden) {
+        this.$root.$emit('msg', 'warning', 'Нет прав на отмену направлений');
+      }
 
       for (const dir of this.directions) {
         if (dir.pk === pk) {
