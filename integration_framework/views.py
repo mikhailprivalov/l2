ac6b8fed45a843624d9f97c2cc3de084990af2f5
Sergei Kasianenko <monitor84@list.ru>
2022-12-12 10:26:36 +0800

    статусы по врачу

--- integration_framework/views.py
+++ integration_framework/views.py
@@ -1742,7 +1742,7 @@ def get_cda_data(pk):
         data = get_json_protocol_data(pk)
     elif check_type_research(pk) == "is_lab":
         data = get_json_labortory_data(pk)
-    elif check_type_research(pk) == "is_paraclinic":
+    elif check_type_research(pk) == "is_para clinic":
         data = get_json_protocol_data(pk, is_paraclinic=True)
     else:
         data = {}
