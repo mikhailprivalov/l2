0a883f3b781cfba6b73a4be0c2fdd41e4da781ce
Sergei Kasianenko <monitor84@list.ru>
2022-12-14 15:42:48 +0800

    fix - current_hosp root_hosp last result

--- api/sql_func.py
+++ api/sql_func.py
@@ -82,7 +82,7 @@ def get_fraction_result(client_id, fraction_id, count=1):
     return row
 
 
-def get_field_result(client_id, field_id, count=1, current_year='1900-01-01 00:00:00', months_ago='-1'):
+def get_field_result(client_id, field_id, count=1, current_year='1900-01-01 00:00:00', months_ago='-1', parent_iss=-1, use_parent_iss='-1'):
     """
     на входе: id-поля, id-карты,
     выход: последний результат поля
@@ -114,9 +114,15 @@ def get_field_result(client_id, field_id, count=1, current_year='1900-01-01 00:0
                      directions_issledovaniya.time_confirmation AT TIME ZONE %(tz)s > (current_date AT TIME ZONE 'ASIA/IRKUTSK' - interval %(months_ago)s)
                      WHEN %(current_year)s = '1900-01-01 00:00:00' THEN directions_issledovaniya.time_confirmation is not Null
                 END
+            AND CASE WHEN %(use_parent_iss)s != '-1' THEN 
+                     directions_napravleniya.parent_id in %(parent_iss)s
+                     WHEN %(use_parent_iss)s = '-1' THEN 
+                     directions_issledovaniya.time_confirmation is not Null
+                END
             ORDER BY directions_issledovaniya.time_confirmation DESC LIMIT %(count_p)s
             """,
-            params={'client_p': client_id, 'field_id': field_id, 'count_p': count, 'tz': TIME_ZONE, 'current_year': current_year, 'months_ago': months_ago},
+            params={'client_p': client_id, 'field_id': field_id, 'count_p': count, 'tz': TIME_ZONE, 'current_year': current_year, 'months_ago': months_ago,
+                    'parent_iss': parent_iss, 'use_parent_iss': use_parent_iss},
         )
 
         row = cursor.fetchall()
