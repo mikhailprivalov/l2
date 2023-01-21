5360ab0270eb0f63097a6f71fea2b95ac0d57afa
Sergei Kasianenko <monitor84@list.ru>
2023-01-21 23:39:41 +0800

    to actions bin APP for init prinQueue

--- l2-frontend/src/App.vue
+++ l2-frontend/src/App.vue
@@ -64,7 +64,6 @@ import CheckBackend from '@/ui-cards/CheckBackend.vue';
 import ChatsDialogs from '@/ui-cards/Chat/ChatsDialogs.vue';
 import * as actions from '@/store/action-types';
 import notifyAudioSrc from '@/assets/notify.mp3';
-import { PRINT_QUEUE_INIT } from '@/store/action-types';
 
 @Component({
   components: { CheckBackend, Navbar, ChatsDialogs },
@@ -92,7 +91,7 @@ import { PRINT_QUEUE_INIT } from '@/store/action-types';
     },
   },
   mounted() {
-    this.$store.dispatch(PRINT_QUEUE_INIT);
+    this.$store.dispatch(actions.PRINT_QUEUE_INIT);
     const urlParams = new URLSearchParams(window.location.search);
     this.embedded = urlParams.get('embedded') === '1';
     if (!this.embedded && !this.hideHeaderWithoutLogin && !this.isEmptyLayout) {
