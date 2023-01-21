32926515cd6292beba5e090b5e81c1809dd6be1a
Sergei Kasianenko <monitor84@list.ru>
2023-01-21 23:31:38 +0800

    Vuex - fix

--- l2-frontend/src/store/modules/printQueue.ts
+++ l2-frontend/src/store/modules/printQueue.ts
@@ -3,6 +3,8 @@ import { setLocalStorageDataJson } from '@/utils';
 import * as mutationTypes from '../mutation-types';
 import * as actionsTypes from '../action-types';
 
+const PRINT_QUEUE_LS_KEY = 'printQueue';
+
 const stateInitial = {
   currentPrintQueue: [],
 };
@@ -13,42 +15,49 @@ const getters = {
 };
 
 const actions = {
-  async [actionsTypes.PRINT_QUEUE_INIT]({ commit }) {
-    commit(mutationTypes.PRINT_QUEUE_INIT);
+  async [actionsTypes.PRINT_QUEUE_INIT]({ commit, dispatch }) {
+    commit(mutationTypes.PRINT_QUEUE_REPLACE_STATE);
+    dispatch(actionsTypes.PRINT_QUEUE_SAVE_LS);
+
+    window.addEventListener('storage', (event) => {
+      if (event.key === PRINT_QUEUE_LS_KEY) {
+        commit(mutationTypes.PRINT_QUEUE_REPLACE_STATE);
+      }
+    });
   },
-  async [actionsTypes.PRINT_QUEUE_ADD_ELEMENT]({ commit }, { id }) {
+  async [actionsTypes.PRINT_QUEUE_ADD_ELEMENT]({ commit, dispatch }, { id }) {
     commit(mutationTypes.PRINT_QUEUE_ADD_ELEMENT, { id });
+    dispatch(actionsTypes.PRINT_QUEUE_SAVE_LS);
   },
-  async [actionsTypes.PRINT_QUEUE_DEL_ELEMENT]({ commit }, { id }) {
+  async [actionsTypes.PRINT_QUEUE_DEL_ELEMENT]({ commit, dispatch }, { id }) {
     commit(mutationTypes.PRINT_QUEUE_DEL_ELEMENT, { id });
+    dispatch(actionsTypes.PRINT_QUEUE_SAVE_LS);
+  },
+  async [actionsTypes.PRINT_QUEUE_SAVE_LS]({ state }) {
+    setLocalStorageDataJson(PRINT_QUEUE_LS_KEY, state.currentPrintQueue);
   },
 };
 
 const mutations = {
-  [mutationTypes.PRINT_QUEUE_INIT](state) {
+  [mutationTypes.PRINT_QUEUE_REPLACE_STATE](state) {
     try {
-      const queue = JSON.parse(localStorage.getItem('queue'));
+      const queue = JSON.parse(localStorage.getItem(PRINT_QUEUE_LS_KEY));
       if (Array.isArray(queue)) {
         state.currentPrintQueue = queue;
-      } else {
-        setLocalStorageDataJson('queue', state.currentPrintQueue);
       }
     } catch (e) {
       state.currentPrintQueue = [];
-      setLocalStorageDataJson('queue', state.currentPrintQueue);
     }
   },
   [mutationTypes.PRINT_QUEUE_ADD_ELEMENT](state, { id }) {
-    if (!getters.idInQueue(id)) {
+    if (!getters.idInQueue(state)(id)) {
       state.currentPrintQueue = [...state.currentPrintQueue, id];
-      setLocalStorageDataJson('queue', state.currentPrintQueue);
     }
   },
   [mutationTypes.PRINT_QUEUE_DEL_ELEMENT](state, { id }) {
     const i = state.currentPrintQueue.indexOf(id);
     if (i >= 0) {
       state.currentPrintQueue.splice(i, 1);
-      setLocalStorageDataJson('queue', state.currentPrintQueue);
     }
   },
 };
