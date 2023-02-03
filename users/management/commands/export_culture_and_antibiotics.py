6c1d7365a02f1b17b34a14398f59e0e7ad5c4e21
Well <Well.heor@yandex.ru>
2023-02-03 11:15:21 +0800

    import culture_and_antibiotics

--- users/management/commands/export_culture_and_antibiotics.py
+++ users/management/commands/export_culture_and_antibiotics.py
@@ -6,31 +6,52 @@ from appconf.manager import SettingManager
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        culture_group = [{
-            "pk": group.pk,
-            "title": group.title
-        } for group in GroupCulture.objects.filter(hide=False)]
-        culture_data = [{
-            "title": culture.title,
-            "group": culture.group_culture_id,
-            "fsli": culture.fsli,
-            "lis": culture.lis
-        } for culture in Culture.objects.filter(hide=False)]
-        antibiotic_groups = [{
-            "pk": group.pk,
-            "title": group.title,
-        } for group in GroupAntibiotic.objects.filter(hide=False)]
-        antibiotics = [{
-            "title": antibiotic.title,
-            "group": antibiotic.group_antibiotic_id,
-            "fsli": antibiotic.fsli,
-            "lis": antibiotic.lis
-        } for antibiotic in Antibiotic.objects.filter(hide=False)]
+        culture_groups = []
+        for group in GroupCulture.objects.all():
+            culture_in_group = []
+            for culture in Culture.objects.filter(group_culture=group):
+                culture_in_group.append({
+                    "title": culture.title,
+                    "fsli": culture.fsli,
+                    "lis": culture.lis
+                })
+            culture_groups.append({
+                "title": group.title,
+                "culture_in_group": culture_in_group,
+            })
+        culture_without_group = []
+        for culture in Culture.objects.filter(group_culture=None):
+            culture_without_group.append({
+                "title": culture.title,
+                "fsli": culture.fsli,
+                "lis": culture.lis
+            })
+        antibiotic_groups = []
+        for group in GroupAntibiotic.objects.all():
+            antibiotic_in_group = []
+            for antibiotic in Antibiotic.objects.filter(group_antibiotic=group):
+                antibiotic_in_group.append({
+                    "title": antibiotic.title,
+                    "fsli": antibiotic.fsli,
+                    "lis": antibiotic.lis
+                })
+            antibiotic_groups.append({
+                "title": group.title,
+                "antibiotic_in_group": antibiotic_in_group
+            })
+        antibiotic_without_group = []
+        for antibiotic in Antibiotic.objects.filter(group_antibiotic=None):
+            antibiotic_without_group.append({
+                "title": antibiotic.title,
+                "fsli": antibiotic.fsli,
+                "lis": antibiotic.lis
+            })
+
         data = {
-            "culture_group": culture_group,
-            "culture": culture_data,
-            "antibiotic_groups": antibiotic_groups,
-            "antibiotics": antibiotics
+            "culture_group": culture_groups,
+            "culture_without_group": culture_without_group,
+            "antibiotic_group": antibiotic_groups,
+            "antibiotic_without_group": antibiotic_without_group,
         }
         dir_tmp = SettingManager.get("dir_param")
         with open(f'{dir_tmp}/culture_and_antibiotics.json', 'w') as fp:
--- users/management/commands/import_culture_and_antibiotics.py
+++ users/management/commands/import_culture_and_antibiotics.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from directory.models import Researches, ParaclinicInputGroups, ParaclinicInputField
+from directory.models import GroupCulture, Culture, GroupAntibiotic, Antibiotic
 import json
 from laboratory import utils
 
@@ -16,4 +16,22 @@ class Command(BaseCommand):
         self.stdout.write("Path: " + fp)
         with open(fp) as json_file:
             data = json.load(json_file)
-            print(data)
+            for group in data["culture_group"]:
+                culture_group = GroupCulture(title=group["title"])
+                culture_group.save()
+                for culture in group["culture_in_group"]:
+                    culture_in_group = Culture(title=culture["title"], group_culture=culture_group.pk, fsli=culture["fsli"], lis=culture["lis"])
+                    culture_in_group.save()
+            for culture in data["culture_without_group"]:
+                culture_without_group = Culture(title=culture["title"], group_culture=None, fsli=culture["fsli"], lis=culture["lis"])
+                culture_without_group.save()
+            for group in data["antibiotic_group"]:
+                antibiotic_group = GroupAntibiotic(title=group["title"])
+                antibiotic_group.save()
+                for antibiotic in group["antibiotic_in_group"]:
+                    antibiotic_in_group = Antibiotic(title=antibiotic["title"], group_antibiotic=antibiotic_group.pk, fsli=antibiotic["fsli"], lis=antibiotic["lis"])
+                    antibiotic_in_group.save()
+            for antibiotic in data["antibiotic_without_group"]:
+                antibiotic_withou_group = Antibiotic(title=antibiotic["title"], group_antibiotic=None, fsli=antibiotic["fsli"], lis=antibiotic["lis"])
+                antibiotic_withou_group.save()
+
