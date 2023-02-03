6b6593c462b02350a39f466805349613eb494e7b
Well <Well.heor@yandex.ru>
2023-02-03 11:52:22 +0800

    .

--- directory/management/commands/export_culture_and_antibiotics.py
+++ directory/management/commands/export_culture_and_antibiotics.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from directory.models import GroupCulture, Culture, GroupAntibiotic, Antibiotic
 import json
+from appconf.manager import SettingManager
 
 
 class Command(BaseCommand):
