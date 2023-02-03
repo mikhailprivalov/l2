70c2ddda9e12bd0b2d6a50ae1b5ca78c9b280c2a
Well <Well.heor@yandex.ru>
2023-02-03 11:27:01 +0800

    Update import_culture_and_antibiotics.py

--- users/management/commands/import_culture_and_antibiotics.py
+++ users/management/commands/import_culture_and_antibiotics.py
@@ -20,7 +20,7 @@ class Command(BaseCommand):
                 culture_group = GroupCulture(title=group["title"])
                 culture_group.save()
                 for culture in group["culture_in_group"]:
-                    culture_in_group = Culture(title=culture["title"], group_culture=culture_group.pk, fsli=culture["fsli"], lis=culture["lis"])
+                    culture_in_group = Culture(title=culture["title"], group_culture=culture_group, fsli=culture["fsli"], lis=culture["lis"])
                     culture_in_group.save()
             for culture in data["culture_without_group"]:
                 culture_without_group = Culture(title=culture["title"], group_culture=None, fsli=culture["fsli"], lis=culture["lis"])
@@ -29,7 +29,7 @@ class Command(BaseCommand):
                 antibiotic_group = GroupAntibiotic(title=group["title"])
                 antibiotic_group.save()
                 for antibiotic in group["antibiotic_in_group"]:
-                    antibiotic_in_group = Antibiotic(title=antibiotic["title"], group_antibiotic=antibiotic_group.pk, fsli=antibiotic["fsli"], lis=antibiotic["lis"])
+                    antibiotic_in_group = Antibiotic(title=antibiotic["title"], group_antibiotic=antibiotic_group, fsli=antibiotic["fsli"], lis=antibiotic["lis"])
                     antibiotic_in_group.save()
             for antibiotic in data["antibiotic_without_group"]:
                 antibiotic_withou_group = Antibiotic(title=antibiotic["title"], group_antibiotic=None, fsli=antibiotic["fsli"], lis=antibiotic["lis"])
