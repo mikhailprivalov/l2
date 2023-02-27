dd8a3514353d68ded8d051fd4b2367ae4217c1c9
Sergei Kasianenko <monitor84@list.ru>
2023-02-27 15:20:01 +0800

    consolidate report by type department

--- clients/management/commands/patient_attach_ecp.py
+++ clients/management/commands/patient_attach_ecp.py
@@ -1,13 +1,10 @@
-import datetime
-
-import openpyxl
 from django.core.management.base import BaseCommand
 from openpyxl import load_workbook
-from openpyxl.styles import NamedStyle, Border, Side, Font, Alignment
-from openpyxl.utils import get_column_letter
-
-from ecp_integration.integration import search_patient_ecp_by_fio, search_patient_ecp_by_person_id, attach_patient_ecp
+from ecp_integration.integration import search_patient_ecp_by_fio, attach_patient_ecp
 from utils.dates import normalize_dots_date
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -34,10 +31,7 @@ class Command(BaseCommand):
                     lastname = cells.index("Фамилия")
                     name = cells.index("Имя")
                     patronymic = cells.index("Отчество")
-                    polis = cells.index("полис")
                     birthday = cells.index("дата рождения")
-                    card_tfoms = cells.index("Номер карты")
-                    distict_tfoms = cells.index("тфомс-участок")
             else:
                 # поиск person_id пациента в ЕЦП
                 patient = {'family': '', 'name': '', 'patronymic': '', 'birthday': ''}
@@ -49,14 +43,14 @@ class Command(BaseCommand):
 
                 person_id = search_patient_ecp_by_fio(patient)
                 if not person_id:
-                    print(f"{r};Нет в ЕЦП; -; -; {cells[lastname]};{cells[name]};{cells[patronymic]};{cells[birthday]}")
+                    logger.error(f"{r};Нет в ЕЦП; -; -; {cells[lastname]};{cells[name]};{cells[patronymic]};{cells[birthday]}")
                     continue
                 district = cells[distict_num].replace("'", '')
-                attach_data = "-#-#-"
+
                 if len(district) < 15:
-                    print(f"{r};Нет учатска в ЕЦП; -; -; {cells[lastname]};{cells[name]};{cells[patronymic]};{cells[birthday]};{district}")
+                    logger.error(f"{r};Нет учатска в ЕЦП; -; -; {cells[lastname]};{cells[name]};{cells[patronymic]};{cells[birthday]};{district}")
                     continue
                 else:
                     attach_data = attach_patient_ecp(person_id, district, "2022-02-22", "10379", "2")
-                print(f'{r};Добавлен участок; {person_id};{attach_data};{cells[lastname]};{cells[name]};{cells[patronymic]};{cells[birthday]}')  # noqa: T001
-        wb.save("attach_ecp.xlsx")
+                attach_data = "None" if attach_data is None else attach_data
+                logger.error(f'{r};Добавлен участок; {person_id};{attach_data};{cells[lastname]};{cells[name]};{cells[patronymic]};{cells[birthday]}')  # noqa: T001
