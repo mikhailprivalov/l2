887b7f2032f4e41e173310e76e56b004eb29e312
Sergei Kasianenko <monitor84@list.ru>
2023-03-16 10:00:04 +0800

    QR-code LK - в отд функцию

--- forms/urls.py
+++ forms/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('pdf', views.pdf),
-    # path('docx', views.docx),
+    path('docx', views.docx),
     path('preview', TemplateView.as_view(template_name='dashboard/stattalon_preview.html')),
     path('extra-nofication', views.extra_nofication),
     path('covid-result', views.covid_result),
