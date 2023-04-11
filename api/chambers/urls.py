f2b9a2bd53727b328810d790449eed427fae04bb
Кирилл <kirill180104@mail.ru>
2023-04-11 15:40:35 +0800

    bed_management

--- api/chambers/urls.py
+++ api/chambers/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('get-unallocated-patients', views.get_unallocated_patients),
     path('get-chambers-and-beds', views.get_chambers_and_beds),
-    # path('get-beds', views.get_beds),
     path('entrance-patient-to-bed', views.entrance_patient_to_bed),
     path('extract-patient-bed', views.extract_patient_bed),
     path('doctor-assigned-patient', views.doctor_assigned_patient),
