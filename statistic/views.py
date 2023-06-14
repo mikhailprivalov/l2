570e26e47af6474e1512d9baeb6d5a76c2cced4a
Sergei Kasianenko <monitor84@list.ru>
2023-06-14 12:40:07 +0800

    fix

--- statistic/views.py
+++ statistic/views.py
@@ -1153,10 +1153,10 @@ def statistic_xls(request):
         d2 = normalize_dots_date(date_end_o)
         extract_researches_id = list(directory.HospitalService.objects.values_list("slave_research_id", flat=True).filter(site_type=7))
         field_id_for_extract_date = list(directory.ParaclinicInputField.objects.values_list("pk", flat=True).filter(group__research__in=extract_researches_id, title="Дата выписки"))
-        result_extract = get_confirm_protocol_by_date_extract(tuple(field_id_for_extract_date), d1, d2) # Найти выписки с датой выписки в периоде
+        result_extract = get_confirm_protocol_by_date_extract(tuple(field_id_for_extract_date), d1, d2)  # Найти выписки с датой выписки в периоде
         result_expertise_data = {i.iss_protocol_extract: {"title_research": i.main_extract_research, "direction_main_extract_dir": i.direction_main_extract_dir} for i in result_extract}
         iss_protocol_extract = list(result_expertise_data.keys())
-        result_expertise_grade = get_expertise_grade(tuple(iss_protocol_extract)) # Результаты экспертизы
+        result_expertise_grade = get_expertise_grade(tuple(iss_protocol_extract))  # Результаты экспертизы
         for i in result_expertise_grade:
             if i.level_value and i.level_value.lower() == "третий":
                 result_expertise_data[i.parent_id]['третий'] = i.grade_value
