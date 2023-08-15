10fd783e240b2e19ebd5b176f6234afc3b1e135a
Sergei Kasianenko <monitor84@list.ru>
2023-08-15 15:18:28 +0800

    education data

---
+++ education/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+import education.models as models
+
+
+class SetInstitutionTitleAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'type',
+    )
+    list_display_links = (
+        'title',
+        'type',
+    )
+
+
+admin.site.register(models.TypeInstitutionEducation)
+admin.site.register(models.InstitutionTitle, SetInstitutionTitleAdmin)
+admin.site.register(models.LevelEducation)
+admin.site.register(models.DocumentTypeEducation)
