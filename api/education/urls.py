246fdd6039fc74b3ba1c89f1a3a558f934e898be
Wellheor <Well.heor@yandex.ru>
2023-08-17 11:21:42 +0800

    .

--- api/education/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    # path('get-columns', views.get_columns),
-]
--- api/education/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import JsonResponse
-
-# from laboratory.local_settings import ENROLLEE_LIST_FIELD
-
-#
-# def get_columns(request):
-#     if not ENROLLEE_LIST_FIELD:
-#         return JsonResponse({"result": None})
-#     return JsonResponse({"result": ENROLLEE_LIST_FIELD})
