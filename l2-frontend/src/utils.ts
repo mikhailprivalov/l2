88a2f903d28d9addd8e6c8cfddfb8b3ee404226d
Sergei Kasianenko <monitor84@list.ru>
2023-09-11 20:44:39 +0800

    fix

--- l2-frontend/src/utils.ts
+++ l2-frontend/src/utils.ts
@@ -368,7 +368,7 @@ export const convertSubjectNameToTitle = (object: any, subjectName: string | nul
   console.log(obj);
   // eslint-disable-next-line no-console
   console.log(subjectName);
-  return [obj.SN, obj.G, obj.SNILS, `${obj.T ? obj.T : ''}`].filter(Boolean).join(' ');
+  return [obj.SN, obj.G, obj.SNILS, obj.T].filter(Boolean).join(' ');
 };
 
 export const validateEmail = (email: string) => Boolean(
