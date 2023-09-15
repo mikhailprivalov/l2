e28672d5678d110943aca5bebb03b7c802203aca
Mikhail Privalov <n5250@ya.ru>
2023-09-15 18:30:08 +0300

    Update ftp_orders/main.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ftp_orders/main.py
+++ ftp_orders/main.py
@@ -424,6 +424,7 @@ class FTPConnection:
             iss.time_save = current_time()
             iss.doc_confirmation_string = ""
             iss.save()
+
     def push_order(self, direction: Napravleniya):
         hl7 = core.Message("ORM_O01", validation_level=VALIDATION_LEVEL.QUIET)
 
