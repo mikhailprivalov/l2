b829d7ee92f21d9bdb95691514959be19fceca36
Mikhail Privalov <n5250@ya.ru>
2023-10-14 04:19:18 +0300

    fraction comment

--- appconf/manager.py
+++ appconf/manager.py
@@ -10,7 +10,7 @@ import appconf.models as appconf
 
 
 class SettingManager:
-    VERSION = f"{laboratory.VERSION}-10"
+    VERSION = f"{laboratory.VERSION}-12"
     WARMUP_TEST_KEY = f'SettingManager:test-warmup:v{VERSION}'
     FULL_CACHE_L2_KEY = f'SettingManager:l2:v{VERSION}'
     FULL_CACHE_EN_KEY = f'SettingManager:en:v{VERSION}'
@@ -170,6 +170,7 @@ class SettingManager:
                     "ftp",
                     "case",
                     "hide_show_count_param",
+                    "fraction_comment",
                 ]
             },
             "consults_module": SettingManager.get("consults_module", default='false', default_type='b'),
