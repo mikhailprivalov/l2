0ded7d5f293e82e855a93c162ffe9602f1a6c611
Sergei Kasianenko <41939763+urchinpro@users.noreply.github.com>
2023-10-20 20:44:49 +0800

    Ref book (#3146)
    * get refbook
    
    * get refbook
    
    * black
    
    * psycopg2

--- integration_framework/urls.py
+++ integration_framework/urls.py
@@ -58,4 +58,5 @@ urlpatterns = [
     path('get-value-field', views.get_value_field),
     path('get-price-data', views.get_price_data),
     path('get-prices-by-date', views.get_prices_by_date),
+    path('get-reference-books', views.get_reference_books),
 ]
