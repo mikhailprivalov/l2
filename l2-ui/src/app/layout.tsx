ef147a21ebd3e846102e3fa9a33b8dcb8dd059f2
Mikhail Privalov <n5250@ya.ru>
2023-11-12 12:10:53 +0300

    NEXT

---
+++ l2-ui/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin', 'cyrillic'] })
+
+export const metadata: Metadata = {
+  title: 'L2 — информационная система',
+  description: 'Многофункциональная система для здравоохранения',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
