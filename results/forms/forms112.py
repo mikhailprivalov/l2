e16d4dd700f17badfe4b52eefb8664fe7dc7ce1b
Well <Well.heor@yandex.ru>
2023-11-14 19:56:24 +0800

    .

--- results/forms/forms112.py
+++ results/forms/forms112.py
@@ -19,13 +19,13 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 
 
-def gen_table(table_data: list, column: Union[list, None]=None, full_grid: bool = False, outer_and_row_grid: bool = False, custom_style=None) -> Table:
+def gen_table(table_data: list, column: Union[list, None] = None, full_grid: bool = False, outer_and_row_grid: bool = False, custom_style=None) -> Table:
     if not table_data:
         return []
     if column:
         table = Table(table_data, column, hAlign='LEFT')
     else:
-        table = Table(table_data,  hAlign='LEFT')
+        table = Table(table_data, hAlign='LEFT')
     table_style = [
         ('VALIGN', (0, 0), (-1, -1), 'TOP')
     ]
@@ -41,7 +41,7 @@ def gen_table(table_data: list, column: Union[list, None]=None, full_grid: bool 
     return table
 
 
-def gen_medicament_table(style_left_bold, style_center, leftnone, title, row_data: list=None):
+def gen_medicament_table(style_left_bold, style_center, leftnone, title, row_data: list = None):
     table_data = [
         [
             Paragraph(f'{title}', style_left_bold),
@@ -67,15 +67,22 @@ def gen_medicament_table(style_left_bold, style_center, leftnone, title, row_dat
     table_data.extend(row_data)
 
     if leftnone:
-        column_params = [7 * mm, 32 * mm, 30 * mm, 15 * mm, 15 * mm, 20 * mm, 21 * mm, 34 * mm]
+        column_params = [7 * mm, 32 * mm, 29 * mm, 18 * mm, 20 * mm, 19 * mm, 21 * mm, 28 * mm]
     else:
-        column_params = [7 * mm, 35 * mm, 30 * mm, 15 * mm, 20 * mm, 20 * mm, 21 * mm, 38 * mm,]
+        column_params = [7 * mm, 37 * mm, 33 * mm, 20 * mm, 20 * mm, 20 * mm, 21 * mm, 28 * mm]
     custom_style = [
         ('SPAN', (0, 0), (-1, 0)),
+        ('BACKGROUND', (0, 0), (-1, 0), colors.HexColor('#99ccff'))
     ]
     table = gen_table(table_data, column_params, True, False, custom_style)
 
     return table
+
+def get_boxed_tag(font_size: Union[int, float]=12):
+    op_boxed_tag = f'<font face="digit8" size={font_size}>'
+    cl_boxed_tag = '</font>'
+    return op_boxed_tag, cl_boxed_tag
+
 
 def form_01(direction: Napravleniya, iss: Issledovaniya, fwb, doc, leftnone, user=None):
     """
@@ -89,8 +96,9 @@ def form_01(direction: Napravleniya, iss: Issledovaniya, fwb, doc, leftnone, use
     pdfmetrics.registerFont(TTFont('PTAstraSerifBold', os.path.join(FONTS_FOLDER, 'PTAstraSerif-Bold.ttf')))
     pdfmetrics.registerFont(TTFont('PTAstraSerifReg', os.path.join(FONTS_FOLDER, 'PTAstraSerif-Regular.ttf')))
     pdfmetrics.registerFont(TTFont('digit8', os.path.join(FONTS_FOLDER, 'digit88table.ttf')))
+    pdfmetrics.registerFont(TTFont('Symbola', os.path.join(FONTS_FOLDER, 'Symbola.ttf')))
 
-    op_boxed_tag = '<font face="digit8" size=15>'
+    op_boxed_tag = f'<font face="digit8" size=15>'
     cl_boxed_tag = '</font>'
     styleSheet = getSampleStyleSheet()
     style = styleSheet["Normal"]
@@ -119,7 +127,7 @@ def form_01(direction: Napravleniya, iss: Issledovaniya, fwb, doc, leftnone, use
     style_left_bold = deepcopy(style_left)
     style_left_bold.fontName = "PTAstraSerifBold"
 
-    protocol_fields = []
+    protocol_fields = ['Вес(кг)', 'Беременность', 'Срок беременности (недель)', 'Аллергия', 'Аллергия на', 'Лечение']
     protocol_data = get_protocol_data(iss, protocol_fields)
 
     space = 5 * mm
@@ -138,28 +146,45 @@ def form_01(direction: Napravleniya, iss: Issledovaniya, fwb, doc, leftnone, use
 
     objs.append(Spacer(1, space * 2))
 
+    empty_checkbox = '<font face="Symbola" size=8>\u2610</font>'
+    filled_checkbox = '<font face="Symbola" size=8>\u2611</font>'
+
+    if protocol_data["Беременность"] == 'Да':
+        pregnancy = f'{filled_checkbox},'
+        term_pregnancy = f'Срок {protocol_data["Срок беременности (недель)"]} недель'
+    else:
+        pregnancy = f'{empty_checkbox}'
+        term_pregnancy = ''
+
+    if protocol_data["Аллергия"] == 'Нет':
+        allergy = f'{empty_checkbox} Нет'
+        allergy_reason = ''
+    else:
+        allergy = f'{filled_checkbox} Есть'
+        allergy_reason = f'на {protocol_data["Аллергия на"]}'
+
     table_data = [
         [
-            Paragraph('Данные пациента', style_left)
+            Paragraph('Данные пациента', style_left_bold)
         ],
         [
-            Paragraph('Инициалы пациента (код пациента)  Пол Вес', style_left)
+            Paragraph(f'Инициалы пациента (код пациента) {patient_data["fio"]} Пол {patient_data["sex"]} Вес {protocol_data["Вес(кг)"]} кг', style_left)
         ],
         [
-            Paragraph('Возраст___________________ Беременность  □, срок _____ недель', style_left)
+            Paragraph(f'Возраст: {patient_data["age"]} Беременность {pregnancy} {term_pregnancy}', style_left)
         ],
         [
-            Paragraph('Аллергия    □ Нет    □ Есть, на ______________________________________ ', style_left)
+            Paragraph(f'Аллергия {allergy} {allergy_reason} ', style_left)
         ],
         [
-            Paragraph('Лечение    □    амбулаторное     □    стационарное    □    самолечение            ', style_left)
+            Paragraph(f'Лечение {protocol_data["Лечение"]}', style_left)
         ],
     ]
 
     custom_style = [
-        ('BACKGROUND', (0, 0), (-1, 0))
+        ('BACKGROUND', (0, 0), (-1, 0), colors.HexColor('#99ccff')),
     ]
-    objs.append(gen_table(table_data, full_grid=True))
+    objs.append(gen_table(table_data, full_grid=True, custom_style=custom_style))
 
     list_bad_medicaments = [
         {"title": "", "manufacturer": "", "serial_number": "", "dose_and_method": "", "start_date": "", "end_date": "", "reason": ""},
@@ -231,7 +256,8 @@ def form_01(direction: Napravleniya, iss: Issledovaniya, fwb, doc, leftnone, use
         ('SPAN', (0, 5), (0, 8)),
         ('GRID', (1, 0), (1, -1), 0.75, colors.black),
         ('GRID', (0, 0), (0, 0), 0.75, colors.black),
-        ('BOX', (0, 0), (0, -1), 0.75, colors.black)
+        ('BOX', (0, 0), (0, -1), 0.75, colors.black),
+        ('BACKGROUND', (0, 0), (0, 0), colors.HexColor('#99ccff'))
     ]
     objs.append(gen_table(table_data, column_params, False, False, custom_style))
 
@@ -262,7 +288,11 @@ def form_01(direction: Napravleniya, iss: Issledovaniya, fwb, doc, leftnone, use
         ],
     ]
 
-    objs.append(gen_table(table_data, full_grid=True))
+    custom_style = [
+        ('BACKGROUND', (0, 0), (-1, 0), colors.HexColor('#99ccff')),
+        ('BACKGROUND', (0, 4), (-1, 4), colors.HexColor('#99ccff'))
+    ]
+    objs.append(gen_table(table_data, full_grid=True, custom_style=custom_style))
 
     objs.append(PageBreak())
     objs.append(Spacer(1, space * 2))
@@ -322,7 +352,11 @@ def form_01(direction: Napravleniya, iss: Issledovaniya, fwb, doc, leftnone, use
         ],
     ]
 
-    objs.append(gen_table(table_data, full_grid=True))
+    custom_style = [
+        ('BACKGROUND', (0, 0), (-1, 0), colors.HexColor('#99ccff')),
+    ]
+
+    objs.append(gen_table(table_data, full_grid=True, custom_style=custom_style))
     objs.append(Paragraph(f'{space_symbol * 12} * поле обязательно к заполнению', style))
     objs.append(Spacer(1, space * 2))
     objs.append(Paragraph('Сообщение может быть отправлено:', style))
