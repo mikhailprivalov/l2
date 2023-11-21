760c7380fb1c1e29e22a6abaab45b286de24a5e6
Well <72241044+Wellheor1@users.noreply.github.com>
2023-11-21 10:14:49 +0800

    Update api/views.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- api/views.py
+++ api/views.py
@@ -2661,8 +2661,9 @@ def update_price(request):
             )
         elif request_data.get("typePrice") == "Внешний исполнитель":
             hospital = Hospitals.objects.filter(pk=int(request_data["company"])).first()
-            current_price = PriceName(title=request_data["title"], symbol_code=request_data["code"], date_start=request_data["start"], date_end=request_data["end"], hospital=hospital,
-                                      external_performer=True)
+            current_price = PriceName(
+                title=request_data["title"], symbol_code=request_data["code"], date_start=request_data["start"], date_end=request_data["end"], hospital=hospital, external_performer=True
+            )
         if current_price:
             current_price.save()
             Log.log(
