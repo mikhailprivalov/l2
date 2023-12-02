960701af05f8ec459fbad42a7f2eaed730effb29
Sergei Kasianenko <monitor84@list.ru>
2023-12-02 22:34:47 +0800

    .

--- xml_generate/views.py
+++ xml_generate/views.py
@@ -79,7 +79,7 @@ def gen_resul_cpp_file_1(iss: Issledovaniya, used_cpp_template_files, data):
     shutil.rmtree(tmp_dir)
 
 
-def gen_resul_cpp_file(iss: Issledovaniya, used_cpp_template_files, data):
+def gen_resul_cpp_file_2(iss: Issledovaniya, used_cpp_template_files, data):
     print(data)
     d = DirectionDocument.objects.filter(direction=iss.napravleniye, is_archive=False, file_type="cpp").first()
     d.file.delete()
@@ -132,6 +132,62 @@ def gen_resul_cpp_file(iss: Issledovaniya, used_cpp_template_files, data):
         #     fp.write(msg)
 
     result = os.path.join(MEDIA_ROOT, 'directions', str(iss.doc_confirmation.hospital.code_tfoms), str(iss.napravleniye.pk), "Выгрузка")
+    shutil.make_archive(f"{result}", 'zip', tmp_dir)
+    DirectionDocument.objects.create(direction=iss.napravleniye, last_confirmed_at=iss.napravleniye.last_confirmed_at, file_type="cpp", file=f"{result}.zip")
+    shutil.rmtree(tmp_dir)
+
+
+def gen_resul_cpp_file(iss: Issledovaniya, used_cpp_template_files, data):
+    d = DirectionDocument.objects.filter(direction=iss.napravleniye, is_archive=False, file_type="cpp").first()
+    d.file.delete()
+    d.delete()
+    tmp_dir = '/tmp/sample/'
+
+    try:
+        shutil.rmtree(tmp_dir)
+    except:
+        pass
+    os.mkdir(tmp_dir)
+
+    data = {
+        "PMOUUID": "121212",
+        "EmployeeUUID": "1212",
+        "PositionUUID": "1212",
+        "SubdivisionUUID": "1212",
+        "ContractUUID": "12121",
+        "PMONumber": "1212",
+        "PMODate": "01.10.2023",
+        "PMORestrictions": "нет",
+        "PMOResult": "допущен",
+        "RiskGroup": "Группа1",
+        "PMORecommendations": "Группа2",
+        "DateGroupHealthDateGroupHealth": "01.12.2023",
+        "GroupHealth": "Группа23",
+        "FactorUUID": ["UUID-1111", "UUID-2222", "UUID-33333", "UUID-4444"],
+        "CertificateDate": "10.12.2023",
+    }
+    cpp_template_files = json.loads(used_cpp_template_files)
+    file_loader = FileSystemLoader(os.path.join(BASE_DIR, 'xml_generate', CPP_TEMPLATE_XML_DIRECTORY))
+    env = Environment(loader=file_loader, trim_blocks=True, lstrip_blocks=True)
+    output_filename = f'{iss.napravleniye.pk}-{datetime.strftime(iss.napravleniye.last_confirmed_at, "%y%m%d%H%M%S%f")[:-3]}'
+    for k, file_name in sorted(cpp_template_files.items()):
+        template_file_name = f"{file_name.get('template')}.xml"
+        tm = env.get_template(template_file_name)
+        msg = tm.render(data=data)
+        print(msg)
+
+    # with open('/tmp/result11.xml', 'w') as fp:
+        if file_name.get('file_name'):
+            xml_file_name = file_name.get('file_name')
+        else:
+            xml_file_name = file_name.get('template')
+        with open(f'{tmp_dir}{xml_file_name}-{output_filename}.xml', 'w') as fp:
+            fp.write(msg)
+        with open(f'{tmp_dir}{xml_file_name}-{output_filename}_2.xml', 'w') as fp:
+            fp.write(msg)
+
+    zip_file_name = f"{iss.napravleniye.pk}-cpp"
+    result = os.path.join(MEDIA_ROOT, 'directions', str(iss.doc_confirmation.hospital.code_tfoms), str(iss.napravleniye.pk), zip_file_name)
     shutil.make_archive(f"{result}", 'zip', tmp_dir)
     DirectionDocument.objects.create(direction=iss.napravleniye, last_confirmed_at=iss.napravleniye.last_confirmed_at, file_type="cpp", file=f"{result}.zip")
     shutil.rmtree(tmp_dir)
