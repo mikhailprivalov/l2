dc02fb808a7575d2f296bb1681fe248482c0750d
Well <Well.heor@yandex.ru>
2024-01-05 17:26:46 +0800

    * вёрстка v2

--- l2-frontend/src/construct/ConstructLaboratory.vue
+++ l2-frontend/src/construct/ConstructLaboratory.vue
@@ -1,39 +1,38 @@
 <template>
   <div class="two-col">
     <div class="sidebar">
-      <div class="picker">
-        <Treeselect
-          v-model="department"
-          :options="departments"
-          class="treeselect-nbr"
-          placeholder="Выберите подразделение"
-        />
-      </div>
+      <Treeselect
+        v-model="department"
+        :options="departments"
+        class="treeselect-noborder"
+        :clearable="false"
+        placeholder="Выберите подразделение"
+      />
       <input
         v-model.trim="search"
         class="form-control"
         style="padding-top: 7px; padding-bottom: 7px"
         placeholder="Фильтр по названию"
       >
-       <div
+      <div
         class="sidebar-content"
       >
-        <div>
-          Не найдено
+        <div
+          v-for="research in filteredResearches"
+          :key="research.id"
+          class="research"
+        >
+          {{ research.label }}
         </div>
         <div
-          v-for="row in researches_list_filtered"
-          :key="row.pk"
-          class="research"
-          :class="{ rhide: row.hide }"
-          @click="open_editor(row.pk)"
+          v-if="filteredResearches.length === 0"
+          class="empty-list"
         >
-          {{ row.title }}
+          Не найдено
         </div>
       </div>
       <button
-        class="btn btn-blue-nb sidebar-footer"
-        @click="open_editor(-1)"
+        class="btn btn-blue-nb nbr"
       >
         <i class="glyphicon glyphicon-plus" />
         Добавить
@@ -44,17 +43,27 @@
 </template>
 
 <script setup lang="ts">
-import {computed, ref} from 'vue';
+import { computed, ref } from 'vue';
 import Treeselect from '@riophae/vue-treeselect';
 import '@riophae/vue-treeselect/dist/vue-treeselect.css';
 
-const department = ref(-1);
-const departments = ref([]);
+const department = ref(null);
+const departments = ref([
+  { id: 1, label: 'отделение1' },
+]);
 
 const search = ref('');
 
-const researches = ref([])
-const filteredResearched = computed()
+const researches = ref([
+  { id: 1, label: 'Гле' },
+  { id: 2, label: 'Анастаси' },
+  { id: 3, label: 'Прив' },
+]);
+const filteredResearches = computed(() => researches.value.filter(research => {
+  const researchTitle = research.label?.toLowerCase();
+  const searchTerm = search.value.toLowerCase();
+  return researchTitle.includes(searchTerm);
+}));
 </script>
 
 <style scoped lang="scss">
@@ -72,17 +81,53 @@ const filteredResearched = computed()
 
   .form-control {
     border-radius: 0;
-    border-top: none;
     border-left: none;
     border-right: none;
   }
 }
-.picker ::v-deep .btn {
+.empty-list {
+  height: 20px;
+  width: 100px;
+  margin: 20px auto;
+}
+
+.sidebar-content {
+  height: 100%;
+}
+
+.research {
+  background-color: #fff;
+  padding: 5px;
+  margin: 10px;
+  border-radius: 4px;
+  cursor: pointer;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.2s cubic-bezier(0.25, 0.8, 0.25, 1);
+  position: relative;
+
+  &.rhide {
+    background-image: linear-gradient(#6c7a89, #56616c);
+    color: #fff;
+  }
+
+  &:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+    z-index: 1;
+    transform: scale(1.008);
+  }
+}
+
+.research:not(:first-child) {
+  margin-top: 0;
+}
+
+.research:last-child {
+  margin-bottom: 0;
+}
+.sidebar-footer {
   border-radius: 0;
-  border-top: none;
-  border-left: none;
-  border-right: none;
-  border-top: 1px solid #fff;
+  margin: 0;
+  flex: 0 0 34px;
 }
 
 .content-construct {
