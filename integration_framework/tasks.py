d93a78a9f4dd5422d91b9682daa163e654454f78
Mikhail Privalov <n5250@ya.ru>
2024-01-10 08:41:28 +0300

    Подтверждение телефона

--- integration_framework/tasks.py
+++ integration_framework/tasks.py
@@ -1,4 +1,5 @@
 import logging
+import traceback
 from datetime import timedelta
 
 import firebase_admin
@@ -7,6 +8,7 @@ from firebase_admin import messaging
 from firebase_admin import credentials
 
 from clients.models import Phones
+from integration_framework.idgtl import IDGTLApi
 from integration_framework.models import IndividualAuth
 from laboratory.celery import app
 from laboratory.settings import FCM_CERT_PATH
@@ -55,3 +57,51 @@ def send_has_result(self, feed_unique_id: str):
             logger.error(f"FCM error: {resp.exception}")
         else:
             logger.info(f"FCM success: {resp.message_id}")
+
+
+@app.task(bind=True)
+def send_code_cascade(self, phone: str, auth_id: int):
+    individual_auth = IndividualAuth.objects.get(pk=auth_id)
+
+    if individual_auth.is_confirmed:
+        logger.error(f"Individual already confirmed: {phone}")
+        return
+
+    if not individual_auth.individuals.exists():
+        logger.error(f"Individual not found: {phone}")
+        return
+
+    api = IDGTLApi()
+
+    if individual_auth.confirmation_message_id:
+        try:
+            api.stop([individual_auth.confirmation_message_id])
+        except Exception as e:
+            logger.error(f"IDGTL error: {e}")
+            logger.error(traceback.format_exc())
+    try:
+        resp = api.send_code_cascade(phone, individual_auth.confirmation_code)
+        logger.info(f"IDGTL response: {resp.json()}")
+        message_id = resp.json()['items'][0]['messageUuid']
+        individual_auth.confirmation_message_id = message_id
+        individual_auth.save(update_fields=['confirmation_message_id'])
+    except Exception as e:
+        logger.error(f"IDGTL error: {e}")
+        logger.error(traceback.format_exc())
+
+
+@app.task(bind=True)
+def stop_code_cascade(self, auth_id: int):
+    individual_auth = IndividualAuth.objects.get(pk=auth_id)
+
+    if not individual_auth.confirmation_message_id:
+        return
+
+    api = IDGTLApi()
+
+    try:
+        resp = api.stop([individual_auth.confirmation_message_id])
+        logger.info(f"IDGTL response: {resp.json()}")
+    except Exception as e:
+        logger.error(f"IDGTL error: {e}")
+        logger.error(traceback.format_exc())
