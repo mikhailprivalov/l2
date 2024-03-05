97d10d7b21bd60fd77673377af54bbdd56f4292e
Well <Well.heor@yandex.ru>
2024-03-05 08:00:33 +0800

    fix

--- api/working_time/views.py
+++ api/working_time/views.py
@@ -2,7 +2,7 @@ import json
 
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from employees.models import Department, EmployeeWorkTime, WorkTimeDocument
+from employees.models import Department
 from laboratory.decorators import group_required
 from utils.response import status_response
 
@@ -17,24 +17,21 @@ def get_departments(request):
 @login_required()
 @group_required('График рабочего времени')
 def get_work_time(request):
-    request_data = json.loads(request.body)
-    work_time = EmployeeWorkTime.get_work_time(request_data["documentId"])
+    work_time = []
     return JsonResponse({"result": work_time})
 
 
 @login_required()
 @group_required('График рабочего времени')
 def get_document(request):
-    request_data = json.loads(request.body)
-    document = WorkTimeDocument.get_document(request_data["year"], request_data["month"], request_data["department"])
+    document = []
     return JsonResponse({"result": document})
 
 
 @login_required()
 @group_required('График рабочего времени')
 def create_document(request):
-    request_data = json.loads(request.body)
-    document = WorkTimeDocument.create_document(request_data["year"], request_data["month"], request_data["department"])
+    document = []
     if document:
         return status_response(True)
     return status_response(False)
