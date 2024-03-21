70465866c031b2c957d1531c525ef2590913fad0
sergei kasianenko <monitor84@list.ru>
2024-03-21 14:01:38 +0800

    fix

--- api/dashboards/urls.py
+++ api/dashboards/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('listdashboard', views.dashboard_list),
     path('dashboard-charts', views.dashboard_charts),
+    path('employees-cash-reister', views.dashboard_charts),
+
 ]
