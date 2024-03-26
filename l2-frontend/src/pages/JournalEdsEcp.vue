a02d8490359f030a47cf62435789243db177a02e
Well <Well.heor@yandex.ru>
2024-03-26 08:56:00 +0800

    Создан компонент

---
+++ l2-frontend/src/pages/JournalEdsEcp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      <div><h4>Заголовок</h4></div>
+    </div>
+    <div>
+      <h4>Таблица</h4>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
