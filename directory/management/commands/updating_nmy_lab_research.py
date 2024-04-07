0d9a1a37272d51a1915809f8113f516e4843e721
Well <Well.heor@yandex.ru>
2024-04-07 14:50:58 +0800

    xlsx с результатами обновления

--- directory/management/commands/updating_nmy_lab_research.py
+++ directory/management/commands/updating_nmy_lab_research.py
@@ -1,4 +1,7 @@
 from django.core.management.base import BaseCommand
+from openpyxl.workbook import Workbook
+
+from appconf.manager import SettingManager
 from directory.models import Researches
 from openpyxl import load_workbook
 
@@ -14,14 +17,19 @@ class Command(BaseCommand):
         fp = kwargs["path"]
         wb = load_workbook(filename=fp)
         ws = wb[wb.sheetnames[0]]
+        result_wb = Workbook()
+        result_ws = result_wb[result_wb.sheetnames[0]]
+        result_ws.append(['НМУ', 'Название', 'Обновлено'])
+
         starts = False
-        internal_code, nmy_cod, = '', ''
+        internal_code, nmy_cod, title = '', '', ''
         for row in ws.rows:
             cells = [str(x.value) for x in row]
             if not starts:
                 if "Код по прайсу" in cells:
                     internal_code = cells.index("Код по прайсу")
                     nmy_cod = cells.index("Код ОКМУ")
+                    title = cells.index("Наименование услуги")
                     starts = True
             else:
                 if cells[internal_code] != "None" and cells[nmy_cod] != "None":
@@ -30,4 +38,10 @@ class Command(BaseCommand):
                         current_research.code = cells[nmy_cod]
                         current_research.save()
                         self.stdout.write(f"Услуге: {current_research.title} присвоен код НМУ: {current_research.code}")
-
+                        result_ws.append([current_research.code, current_research.title, '+'])
+                    else:
+                        result_ws.append([cells[nmy_cod], cells[title], '-'])
+                else:
+                    result_ws.append([cells[nmy_cod], cells[title], '-'])
+        dir_tmp = SettingManager.get("dir_param")
+        result_ws.save(f"{dir_tmp}/result_update_nmy.xlsx")
