90fad567fa3b0d4cd7b6d71ba11b87ca7be9f56c
Well <Well.heor@yandex.ru>
2024-04-23 12:14:20 +0800

    price_code

--- contracts/management/commands/import_prices.py
+++ contracts/management/commands/import_prices.py
@@ -11,13 +11,13 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         """
         :param path - xlsx файл  со столбцами:
-        Плательщик/Контрагент, Тариф, Действует С
+        Плательщик/Контрагент, Тариф, Действует С, Код
         """
         fp = kwargs["path"]
         wb = load_workbook(filename=fp)
         ws = wb[wb.sheetnames[0]]
         starts = False
-        company_idx, price_name_idx, price_start_idx = '', '', ''
+        company_idx, price_name_idx, price_start_idx, code_idx = '', '', '', ''
         for row in ws.rows:
             cells = [str(x.value) for x in row]
             if not starts:
@@ -25,20 +25,25 @@ class Command(BaseCommand):
                     company_idx = cells.index("Плательщик/Контрагент")
                     price_name_idx = cells.index("Тариф")
                     price_start_idx = cells.index("Действует С")
+                    code_idx = cells.index("Код")
                     starts = True
             else:
-                price_title = None
-                current_price = None
                 price_start = cells[price_start_idx].split(" ")[0]
-                company_string = cells[company_idx].strip()
-                company = Company.objects.filter(Q(title__iexact=company_string) | Q(short_title__iexact=company_string)).first()
-                if company:
-                    price_title = f"{cells[price_name_idx].strip()}-{company.short_title}"
-                if price_title:
-                    current_price = PriceName.objects.filter(title__iexact=price_title)
-                if not current_price.exists():
-                    new_price = PriceName(title=price_title, date_start=price_start, company_id=company.pk)
-                    new_price.save()
-                    self.stdout.write(f"Прайс {price_title} создан")
+                price_code = cells[code_idx].strip()
+                if price_code != "None":
+                    company_string = cells[company_idx].strip()
+                    company = Company.objects.filter(Q(title__iexact=company_string) | Q(short_title__iexact=company_string)).first()
+                    if company:
+                        price_title = f"{cells[price_name_idx].strip()}-{company.short_title}"
+                    else:
+                        self.stdout.write(f"Компании {company_string} нет")
+                        continue
+                    current_price = PriceName.objects.filter(Q(title__iexact=price_title) | Q(symbol_code=price_code))
+                    if not current_price.exists():
+                        new_price = PriceName(title=price_title, date_start=price_start, company_id=company.pk, symbol_code=price_code)
+                        new_price.save()
+                        self.stdout.write(f"Прайс {price_title} создан")
+                    else:
+                        self.stdout.write(f"Такой прайс уже есть - {price_title}")
                 else:
-                    self.stdout.write(f"Такой прайс уже есть - {price_title}")
+                    self.stdout.write("нет кода прайса")
