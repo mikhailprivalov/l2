1be6dcd00bdcb2e345038777d7708108255d16d6
Well <Well.heor@yandex.ru>
2024-04-23 09:42:36 +0800

    прайс с поля "тариф", фикс поиска

--- contracts/management/commands/import_prices.py
+++ contracts/management/commands/import_prices.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         """
         :param path - xlsx файл  со столбцами:
-        Плательщик/Контрагент, Тарифный план, Действует С
+        Плательщик/Контрагент, Тариф, Действует С
         """
         fp = kwargs["path"]
         wb = load_workbook(filename=fp)
@@ -23,21 +23,24 @@ class Command(BaseCommand):
             if not starts:
                 if "Тарифный план" in cells:
                     company_idx = cells.index("Плательщик/Контрагент")
-                    price_name_idx = cells.index("Тарифный план")
+                    price_name_idx = cells.index("Тариф")
                     price_start_idx = cells.index("Действует С")
                     starts = True
             else:
-                price_title = cells[price_name_idx].strip()
+                price_title = None
+                current_price = None
                 price_start = cells[price_start_idx].split(" ")[0]
                 company_string = cells[company_idx].strip()
-                current_price = PriceName.objects.filter(title__iexact=price_title)
+                company = Company.objects.filter(Q(title__iexact=company_string) | Q(short_title__iexact=company_string)).first()
+                if company:
+                    price_title = f"{cells[price_name_idx].strip()}-{company.short_title}"
+                if price_title:
+                    current_price = PriceName.objects.filter(title__iexact=price_title)
                 if not current_price.exists():
-                    company = Company.objects.filter(Q(title__iexact=company_string) | Q(short_title__iexact=company_string)).first()
                     if company:
-                        title_new_price = f"{price_title}-{company.short_title}"
-                        new_price = PriceName(title=title_new_price, date_start=price_start, company_id=company.pk)
+                        new_price = PriceName(title=price_title, date_start=price_start, company_id=company.pk)
                         new_price.save()
-                        self.stdout.write(f"Прайс {title_new_price} создан")
+                        self.stdout.write(f"Прайс {price_title} создан")
                     else:
                         self.stdout.write(f"Такого контрагента нет - {company_string}")
                 else:
