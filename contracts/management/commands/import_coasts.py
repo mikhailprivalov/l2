5c8c43a4f01e5c55ede24b610d85ffbea3164cfe
Well <Well.heor@yandex.ru>
2024-04-24 08:09:36 +0800

    Импорт цен v1

--- contracts/management/commands/import_coasts.py
+++ contracts/management/commands/import_coasts.py
@@ -1,7 +1,9 @@
 from django.core.management.base import BaseCommand
 from openpyxl import load_workbook
-from contracts.models import Company, PriceName
+from contracts.models import Company, PriceName, PriceCoast
 from django.db.models import Q
+
+from directory.models import Researches
 
 
 class Command(BaseCommand):
@@ -11,18 +13,51 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         """
         :param path - xlsx файл  со столбцами:
-        Плательщик/Контрагент, Тарифный план, Действует С
+        Код прайса, Услуга, Код ОКМУ, далее - динамические колонки (коды прайсов)
+
         """
         fp = kwargs["path"]
         wb = load_workbook(filename=fp)
         ws = wb[wb.sheetnames[0]]
         starts = False
-        company_idx = ''
+        internal_code_idx, service_name_idx, nmy_code_idx = '', '', ''
+        not_price_col = []
+        columns = {}
         for row in ws.rows:
             cells = [str(x.value) for x in row]
             if not starts:
-                if "Тарифный план" in cells:
-                    company_idx = cells.index("Плательщик/Контрагент")
-                    price_name_idx = cells.index("Тарифный план")
-                    price_start_idx = cells.index("Действует С")
+                if "Код прайса" in cells:
+                    internal_code_idx = cells.index("Код прайса")
+                    service_name_idx = cells.index("Услуга")
+                    nmy_code_idx = cells.index("Код ОКМУ")
+                    not_price_col = [internal_code_idx, service_name_idx, nmy_code_idx]
+                    for idx, column in enumerate(cells):
+                        columns[idx] = column.strip()
                     starts = True
+            else:
+                internal_code = cells[internal_code_idx].strip()
+                if internal_code == "None":
+                    self.stdout.write('Нет внутреннего кода')
+                    continue
+                service: Researches = Researches.objects.filter(internal_code=internal_code).first()
+                if not service:
+                    self.stdout.write('Услуга не найдена')
+                    continue
+                for idx, cell in enumerate(cells):
+                    if idx in not_price_col:
+                        continue
+                    service_coast = cell.strip()
+                    price_code = columns[idx]
+                    price: PriceName = PriceName.objects.filter(symbol_code=price_code).first()
+                    if not price:
+                        self.stdout.write('Нет такого прайса')
+                        continue
+                    current_price_coasts: PriceCoast = PriceCoast.objects.filter(price_name_id=price.pk, research_id=service.pk).first()
+                    if current_price_coasts:
+                        current_price_coasts.coast = service_coast
+                        self.stdout.write(f'Цена услуги {service.title} в прайсе {price.title} обновлена')
+                        # current_price_coasts.save()
+                    else:
+                        new_price_coasts: PriceCoast = PriceCoast(price_name_id=price.pk, research_id=service.pk, coast=service_coast)
+                        self.stdout.write(f'Добавлена услуга {service.title} в прайс {price.title}')
+                        # new_price_coasts.save()
