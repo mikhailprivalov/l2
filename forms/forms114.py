cf9236f75ebb4e3b1e01a74ad22d54a857e4929a
Well <Well.heor@yandex.ru>
2024-04-25 20:54:44 +0800

    Цены по прайсам - v1.1

--- forms/forms114.py
+++ forms/forms114.py
@@ -12,7 +12,7 @@ def form_01(request_data) -> Workbook:
     work_sheet = work_book[work_book.sheetnames[0]]
     current_day = datetime.datetime.now()
     active_prices = PriceName.objects.filter(Q(date_end__gte=current_day) | Q(date_end=None)).order_by('title')
-    researches = Researches.objects.filter(hide=False, internal_code__isnull=False).order_by('internal_code')
+    researches = Researches.objects.filter(hide=False).exclude(internal_code='').order_by('internal_code')
     column = {"internal_code": "Код по прайсу", "title": "Услуга", "code": "Код ОКМУ"}
     price = {price.symbol_code: f"{price.title}-{price.symbol_code}" for price in active_prices}
     column.update(price)
