ffa92f869ed54bfac6612d90adb266dd6331d860
Well <Well.heor@yandex.ru>
2024-04-25 23:10:09 +0800

    fix

--- forms/forms114.py
+++ forms/forms114.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 
 from django.db.models import Q
 from openpyxl import Workbook
@@ -9,14 +10,18 @@ from forms.sql_func import get_researches, get_coasts
 
 
 def form_01(request_data) -> Workbook:
+    start_time = time.time()
     work_book = Workbook()
     work_sheet = work_book[work_book.sheetnames[0]]
 
     current_day = datetime.datetime.now()
     researches = get_researches()
     prices = PriceName.objects.filter(Q(date_end__gte=current_day) | Q(date_end__isnull=True)).order_by('id')
-    price_template = {price.pk: 0 for price in prices}
-    price_title = [price.title for price in prices]
+    price_template = {}
+    price_titles = []
+    for price in prices:
+        price_template[price.pk] = 0
+        price_titles.append(price.title)
     research_dict = {research.id: {"internal_code": research.internal_code, "research_title": research.title, "research_code": research.code, **price_template} for research in researches}
 
     coasts = get_coasts(current_day)
@@ -24,9 +29,10 @@ def form_01(request_data) -> Workbook:
         research_dict[coast.research_id][coast.price_name_id] = str(coast.coast)
 
     headers = ["Код по прайсу", "Услуга", "Код ОКМУ"]
-    headers.extend(price_title)
+    headers.extend(price_titles)
     work_sheet.append(headers)
     for value in research_dict.values():
         work_sheet.append(list(value.values()))
 
+    print("--- %s seconds ---" % (time.time() - start_time))
     return work_book
