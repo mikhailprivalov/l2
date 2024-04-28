9e0cbb7c6bd21d0245b3da296171f490b1e80a4b
Well <Well.heor@yandex.ru>
2024-04-28 19:06:58 +0800

    initial

---
+++ l2-frontend/src/components/UploadFile.vue
@@ -0,0 +1,11 @@
+<template>
+  <h4>Загрузка файла</h4>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ l2-frontend/src/modals/UploadFileModal.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <a
+      @click.prevent="openModal">{{ titleLocal }}
+    </a>
+    <Modal
+      v-if="open"
+      show-footer="true"
+      ignore-body
+      white-bg="true"
+      max-width="710px"
+      width="100%"
+      margin-left-right="auto"
+      @close="open = false"
+    >
+      <span slot="header">{{ titleLocal }}</span>
+      <div slot="body">
+        <UploadFile />
+      </div>
+      <div slot="footer">
+        <div class="row">
+          <div class="col-xs-4">
+            <button
+              class="btn btn-primary-nb btn-blue-nb"
+              type="button"
+              :disabled="loading"
+              @click="open = false"
+            >
+              Закрыть
+            </button>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+import Modal from "@/ui-cards/Modal.vue";
+import UploadFile from "@/components/UploadFile.vue";
+import Component from "vue-class-component";
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: false,
+  }
+})
+
+const titleLocal = computed(() => {
+  return props.title ? props.title : 'Загрузка файла'
+})
+
+const open = ref(false);
+const openModal = () => {
+  open.value = true;
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
