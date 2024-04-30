bab7fb8df474d62f93e6b407f7d5d76ae6c5c53b
Well <Well.heor@yandex.ru>
2024-04-30 15:23:24 +0800

    .

--- api/parse_file/forms101.py
+++ api/parse_file/forms101.py
@@ -6,11 +6,5 @@ def form_01(request_data):
     Cтруктура:
     Код по прайсу (internal_code Researches), Услуга (title_researches), следующее поле - любое текстовое название прайса (priceCoasts.coast)
     """
-    file = request_data.get("file")
-    selected_form = request_data.get("selectedForm")
-    entity_id = request_data.get("entityId")
-    other_need_data = request_data.get("otherNeedData")
-    user = request_data.get('user')
-    hospital = request_data.get('hospital')
 
     return []
