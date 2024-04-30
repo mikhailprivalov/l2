a268d37f7f6f0257305100e649ac299bd8f7f1a4
Well <Well.heor@yandex.ru>
2024-04-30 12:50:27 +0800

    Прайсы "работадатель" - "Заказчики"

--- contracts/management/commands/import_prices.py
+++ contracts/management/commands/import_prices.py
@@ -3,6 +3,8 @@ from openpyxl import load_workbook
 from contracts.models import Company, PriceName
 from django.db.models import Q
 
+from hospitals.models import Hospitals
+
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -17,12 +19,12 @@ class Command(BaseCommand):
         wb = load_workbook(filename=fp)
         ws = wb[wb.sheetnames[0]]
         starts = False
-        company_idx, price_name_idx, price_start_idx, code_idx = '', '', '', ''
+        hospital_idx, price_name_idx, price_start_idx, code_idx = '', '', '', ''
         for row in ws.rows:
             cells = [str(x.value) for x in row]
             if not starts:
                 if "Тарифный план" in cells:
-                    company_idx = cells.index("Плательщик/Контрагент")
+                    hospital_idx = cells.index("Плательщик/Контрагент")
                     price_name_idx = cells.index("Тариф")
                     price_start_idx = cells.index("Действует С")
                     code_idx = cells.index("Код")
@@ -31,16 +33,16 @@ class Command(BaseCommand):
                 price_start = cells[price_start_idx].split(" ")[0]
                 price_code = cells[code_idx].strip()
                 if price_code != "None":
-                    company_string = cells[company_idx].strip()
-                    company = Company.objects.filter(Q(title__iexact=company_string) | Q(short_title__iexact=company_string)).first()
-                    if company:
-                        price_title = f"{company.short_title}-{cells[price_name_idx].strip()}-{price_code}"
+                    hospital_string = cells[hospital_idx].strip()
+                    hospital = Hospitals.objects.filter(Q(title__iexact=hospital_string) | Q(short_title__iexact=hospital_string)).first()
+                    if hospital:
+                        price_title = f"{hospital.short_title}-{cells[price_name_idx].strip()}-{price_code}"
                     else:
-                        self.stdout.write(f"Компании {company_string} нет")
+                        self.stdout.write(f" Больницы {hospital_string} нет")
                         continue
                     current_price = PriceName.objects.filter(symbol_code=price_code)
                     if not current_price.exists():
-                        new_price = PriceName(title=price_title, date_start=price_start, company_id=company.pk, symbol_code=price_code)
+                        new_price = PriceName(title=price_title, date_start=price_start, hospital_id=hospital.pk, subcontract=True, symbol_code=price_code)
                         new_price.save()
                         self.stdout.write(f"Прайс {price_title} создан")
                     else:
