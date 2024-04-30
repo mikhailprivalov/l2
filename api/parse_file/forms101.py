2dc16d92a7208cc8714ce006d27e337822966aef
Well <Well.heor@yandex.ru>
2024-04-30 15:10:11 +0800

    передача доп. параметров

--- api/parse_file/forms101.py
+++ api/parse_file/forms101.py
@@ -1,7 +1,16 @@
 
-
-
 def form_01(request_data):
-    print(request_data)
-    print('МЫЗАШЛИ В НУЖНУЮ')
+    """
+    На входе:
+    Файл XLSX с ценами прайса, структура:
+    Код по прайсу (internal_code Researches, Услуга (title_researches), следующее поле - любое текстовое название прайса
+    """
+    file = request_data.get("file")
+    selected_form = request_data.get("selectedForm")
+    entity_id = request_data.get("entityId")
+    other_need_data = request_data.get("otherNeedData")
+    user = request_data.get('user')
+    hospital = request_data.get('hospital')
+    print(selected_form)
+
     return []
