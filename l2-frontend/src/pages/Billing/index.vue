d560adb2cde32a16140baa8f23d5a0fc86c703d9
Well <Well.heor@yandex.ru>
2024-05-01 12:23:25 +0800

    Компонент

---
+++ l2-frontend/src/pages/Billing/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div><h4>Выставление сетов</h4></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const selected = ref('');
+</script>
+
+<style scoped lang="scss">
+.flex {
+  display: flex;
+}
+</style>
