5f01d7d15869ebd8b3b9019cabb9918842e7ae2f
sergei kasianenko <monitor84@list.ru>
2024-05-03 15:54:28 +0800

    fix

--- api/contracts/urls.py
+++ api/contracts/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('create-billing', views.create_billing),
     path('update-billing', views.update_billing),
     path('confirm-billing', views.confirm_billing),
-    path('get_data_for_confirmed_billing', views.get_data_for_confirmed_billing),
     path('get-billings', views.get_billings),
     path('get-billing', views.get_billing),
 ]
