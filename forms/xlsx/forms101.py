4f7dd6105d33fc0e56b8db925d032571aaf93c65
Sergei Kasianenko <41939763+urchinpro@users.noreply.github.com>
2024-05-11 14:28:53 +0800

    Update forms/xlsx/forms101.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- forms/xlsx/forms101.py
+++ forms/xlsx/forms101.py
@@ -5,7 +5,6 @@ from contracts.models import BillingRegister
 from forms.xlsx.billing import billing_func
 
 
-
 def form_01(request_data) -> Workbook:
     billing_id = request_data.get("billingId")
     wb = openpyxl.Workbook()
