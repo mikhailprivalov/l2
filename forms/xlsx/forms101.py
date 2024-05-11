ad8113cd6194b2a364ac199a7d33dbbb35af37b0
sergei kasianenko <monitor84@list.ru>
2024-05-11 14:30:06 +0800

    fix pep

--- forms/xlsx/forms101.py
+++ forms/xlsx/forms101.py
@@ -15,6 +15,6 @@ def form_01(request_data) -> Workbook:
     billing_data = BillingRegister.objects.filter(pk=billing_id).first()
     data_confirm_billing = get_confirm_data_for_billing(billing_data.price_id, billing_id)
     structure_data = structure_table(data_confirm_billing)
-    ws = billing_func.billing_base(ws, structure_data.get("columns"))
+    ws = billing_func.billing_base(ws)
     ws = billing_func.fill_billing(ws, structure_data.get("tableData"))
     return wb
