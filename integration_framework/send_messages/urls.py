7d57eef20d2f2c287688f94fc23fa09f16ec34e5
sergei kasianenko <monitor84@list.ru>
2024-06-10 12:01:23 +0800

    get pdf send massage

--- integration_framework/send_messages/urls.py
+++ integration_framework/send_messages/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('get-directions-for-mail-send', views.get_directions_for_mail_send),
+    path('get-pdf-for-mail-send', views.get_pdf_for_mail_send),
 ]
