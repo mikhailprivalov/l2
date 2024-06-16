f8b954235d44a1a8e60b5a2ba3ed87a20e71e98e
sergei kasianenko <monitor84@list.ru>
2024-06-16 20:21:25 +0800

    recieve tubes - query sql method

--- directions/sql_func.py
+++ directions/sql_func.py
@@ -99,3 +99,47 @@ def get_researches_by_number_directions(direction_numbers):
         )
         rows = namedtuplefetchall(cursor)
     return rows
+
+
+def get_tube_registration(d_start, d_end, doctorprofile_id):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+            SELECT
+            directions_tubesregistration.id as tube_id,
+            directions_tubesregistration.number as tube_number,
+            directions_tubesregistration.daynum as tube_daynum,
+            directions_tubesregistration.defect_text as tube_defect_text,
+            directions_tubesregistration.is_defect as tube_is_defect,
+            di.napravleniye_id as direction_number,
+            dn.external_executor_hospital_id,
+            dn.external_order_id,
+            dr.title as research_title,
+            pp.title as department_title,
+            plan_org.title as plan_external_perform_org,
+            externalordre_hosp.title as hosp_external_order_api,
+            himself_input_external_hosp.title as himself_input_external_hosp_title,
+            restubes.title as tube_title,
+            restubes.color as tube_color
+            
+            FROM directions_tubesregistration
+            LEFT JOIN directions_issledovaniya_tubes dit on directions_tubesregistration.id = dit.tubesregistration_id
+            LEFT JOIN directory_releationsft drft on drft.id = directions_tubesregistration.type_id
+            LEFT JOIN researches_tubes restubes on drft.tube_id = restubes.id 
+            LEFT JOIN directions_issledovaniya di on dit.issledovaniya_id = di.id
+            LEFT JOIN directions_napravleniya dn on di.napravleniye_id = dn.id
+            LEFT JOIN directory_researches dr on di.research_id = dr.id
+            LEFT JOIN podrazdeleniya_podrazdeleniya pp on dr.podrazdeleniye_id = pp.id
+            LEFT JOIN hospitals_hospitals plan_org on dr.plan_external_performing_organization_id = plan_org.id
+            LEFT JOIN directions_registeredorders dregorder on dn.external_order_id = dregorder.id
+            LEFT JOIN hospitals_hospitals externalordre_hosp on dregorder.organization_id = externalordre_hosp.id
+            LEFT JOIN hospitals_hospitals himself_input_external_hosp on dn.hospital_id=himself_input_external_hosp.id
+            
+            WHERE directions_tubesregistration.doc_recive_id = %(doctorprofile_id)s
+            AND directions_tubesregistration.time_recive AT TIME ZONE %(tz)s BETWEEN %(d_start)s AND %(d_end)s
+            ORDER BY directions_tubesregistration.daynum DESC
+            """,
+            params={'doctorprofile_id': doctorprofile_id, 'd_start': d_start, 'd_end': d_end, 'tz': TIME_ZONE},
+        )
+        rows = namedtuplefetchall(cursor)
+    return rows
