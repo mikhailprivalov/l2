9ece047e22ae45e7144c8dfe7fbf97c108a15b53
Well <Well.heor@yandex.ru>
2024-06-19 20:22:42 +0800

    fix

--- directory/models.py
+++ directory/models.py
@@ -685,7 +685,7 @@ class Researches(models.Model):
             research.laboratory_material_id = research_data.get("laboratoryMaterialId", None)
             research.sub_group_id = research_data.get("subGroupId", None)
             research.laboratory_duration = research_data["laboratoryDuration"]
-            research.volume_for_tube = research_data["volumeForTube"]
+            research.volume_for_tube = research_data["volumeForTube"] if research_data["volumeForTube"] else 0
             research.save()
             fractions = Fractions.objects.filter(research_id=research.pk)
         elif research_title:
