346c8b8c03014fc74cbdffce9c9c2ca03249fae7
sergei kasianenko <monitor84@list.ru>
2024-06-22 20:51:46 +0800

    get direction for person send email

--- integration_framework/send_messages/urls.py
+++ integration_framework/send_messages/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get-directions-for-mail-send', views.get_directions_for_mail_send),
+    path('get-directions-for-org-mail-send', views.get_directions_for_org_mail_send),
+    path('get-directions-for-person-mail-send', views.get_directions_for_person_mail_send),
     path('get-pdf-for-mail-send', views.get_pdf_for_mail_send),
 ]
--- integration_framework/send_messages/views.py
+++ integration_framework/send_messages/views.py
@@ -1,7 +1,7 @@
 from dateutil.relativedelta import relativedelta
 from rest_framework.response import Response
 
-from api.directions.sql_func import get_confirm_direction_by_hospital
+from api.directions.sql_func import get_confirm_direction_by_hospital, get_lab_podr, get_confirm_direction, get_direction_data_by_directions_id, get_total_confirm_direction
 from appconf.manager import SettingManager
 from directions.models import Napravleniya
 from hospitals.models import Hospitals
@@ -9,6 +9,8 @@ from integration_framework.common_func import directions_pdf_result
 from laboratory.utils import current_time, TZ
 from rest_framework.decorators import api_view
 import simplejson as json
+
+from results.sql_func import get_not_confirm_direction
 from utils.response import status_response
 from django.core.files.base import ContentFile
 import base64
@@ -16,7 +18,7 @@ from slog.models import Log
 
 
 @api_view()
-def get_directions_for_mail_send(request):
+def get_directions_for_org_mail_send(request):
     if not hasattr(request.user, "hospitals"):
         return Response({"ok": False, "message": "Некорректный auth токен"})
 
@@ -46,16 +48,68 @@ def get_directions_for_mail_send(request):
     return Response(result)
 
 
+@api_view()
+def get_directions_for_person_mail_send(request):
+    if not hasattr(request.user, "hospitals"):
+        return Response({"ok": False, "message": "Некорректный auth токен"})
+    request_data = json.loads(request.body)
+
+    days_ago = SettingManager.get("days_before_hosp", default="30", default_type="i")
+    d_end_time = current_time()
+    d_start_time = d_end_time + relativedelta(days=-days_ago)
+
+    d_end = d_end_time.strftime("%Y-%m-%d %H:%M:%S")
+    d_start = d_start_time.strftime("%Y-%m-%d %H:%M:%S")
+
+    is_lab = request_data.get('is_lab', False) == 'is_lab'
+    is_paraclinic = request_data.get('is_paraclinic', False) == 'is_paraclinic'
+    is_doc_refferal = request_data.get('is_doc_refferal', False) == 'is_doc_refferal'
+
+    if not is_lab and not is_doc_refferal and not is_paraclinic:
+        return Response({"results": []})
+
+    if is_lab:
+        lab_podr = get_lab_podr()
+        lab_podr = [i[0] for i in lab_podr]
+    else:
+        lab_podr = [-1]
+
+    confirm_direction = get_total_confirm_direction(d_start, d_end, lab_podr, is_lab, is_paraclinic, is_doc_refferal)
+    if not confirm_direction:
+        return Response({"results": []})
+    result_direction = [i for i in confirm_direction.napravleniye_id]
+    direction_data = get_direction_data_by_directions_id(tuple(result_direction))
+    direction_structure_by_client = {}
+
+    for row in direction_data:
+        if row.email_with_results_sent_to_person or not row.patient_email:
+            continue
+
+        if not direction_structure_by_client.get(row.client_id):
+            direction_structure_by_client[row.client_id] = {"mail": row.patien_email, "fio": f"{row.family} {row.name} {row.patronymic}", "directions": [row.direction_id]}
+        else:
+            direction_structure_by_client[row.client_id]["directions"].append(row.direction_id)
+
+
+    result = [{"clientId": k, "mail": v["mail"], "dirs": v["directions"]} for k, v in direction_structure_by_client]
+
+    return Response(result)
+
+
 @api_view(['POST'])
 def get_pdf_for_mail_send(request):
     body = json.loads(request.body)
     hospital_id = body.get("hospitalId")
     directions_ids = body.get("directionsIds")
+    is_person_send = body.get("isPersonSend", "0") == "1"
+    client_id = body.get("clientId")
 
     for direction_id in directions_ids:
         n = Napravleniya.objects.get(pk=direction_id)
-        if n.hospital_id != hospital_id:
+        if not is_person_send and (n.hospital_id != hospital_id):
             return Response(f"Направление №{n.pk} не принадлежит организации {hospital_id}")
+        if is_person_send and n.client_id != client_id:
+            return Response(f"Направление №{n.pk} не принадлежит карте пациента с id {client_id}")
         if not n.total_confirmed:
             return Response(f"Направление №{n.pk} не подтверждено")
 
