afde2d71e946a6ca1db0758954d5fbb2f780eb07
Well <Well.heor@yandex.ru>
2024-06-23 12:52:47 +0800

    fix all

--- api/researches/views.py
+++ api/researches/views.py
@@ -237,7 +237,8 @@ def get_researches(request, last_used=False):
                     for t in tpls:
                         has_templates[t['pk']] = True
             deps['-109999' if last_used else r.reversed_type].append(research_data)
-            deps['all'].append(research_data)
+            if r.podrazdeleniye.p_type in [2, 3, 4]:  # Выводить в Все только лаб, пар. конс.
+                deps['all'].append(research_data)
 
         for dk in deps:
             if last_used:
