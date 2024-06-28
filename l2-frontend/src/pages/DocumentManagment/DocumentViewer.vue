69fc7797fa5114cbb0eaf2c85d01f4708fc24275
Well <Well.heor@yandex.ru>
2024-06-28 20:06:09 +0800

    inital vue

---
+++ l2-frontend/src/pages/DocumentManagment/DocumentViewer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h4>Едитор</h4>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({});
+</script>
+<style scoped lang="scss">
+.editor-main {
+  width: 30px;
+}
+</style>
