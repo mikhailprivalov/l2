30ee2b2b0e90a3956228b510d9491d362c7efe10
Well <Well.heor@yandex.ru>
2024-07-03 19:31:32 +0800

    fix

--- api/laboratory/forms100.py
+++ api/laboratory/forms100.py
@@ -1,13 +1,9 @@
 from directions.models import Issledovaniya
 
-
 def form_01(request_data):
     file = request_data.get("file")
     issledovanie_id = request_data.get("entity_id")
     file = request_data.get("file")
     issledovanie: Issledovaniya = Issledovaniya.objects.filter(pk=issledovanie_id).select_related('research').first()
-    print(file)
-    print(issledovanie_id)
-    print(issledovanie)
-    print(issledovanie.research.title)
+    Issledovaniya.add
     return {"ok": True, "result": [], "message": ""}
--- api/parse_file/views.py
+++ api/parse_file/views.py
@@ -901,7 +901,7 @@ def get_parts_fio(fio_data):
         patronymic_data = fio_data[2]
     return family_data, name_data, patronymic_data
 
-
+@login_required()
 def upload_file(request):
     # todo - Логирование загрузки файлов
     try:
