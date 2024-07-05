68d17a2e3646bafd3f8823606bb4255a92f0668b
Well <Well.heor@yandex.ru>
2024-07-05 17:25:53 +0800

    rename filters name v2

--- l2-frontend/src/pages/DocumentManagement/DocumentsFilters.vue
+++ l2-frontend/src/pages/DocumentManagement/DocumentsFilters.vue
@@ -38,7 +38,8 @@ import { ref } from 'vue';
 import RadioFieldById from '@/fields/RadioFieldById.vue';
 
 const filterButtons = ref([
-  { id: 'my', label: 'Мои' },
+  { id: 'iCreated', label: 'Я создал' },
+  { id: 'iDoing', label: 'Я исполняю' },
   { id: 'wrote', label: 'Я поручил' },
   { id: 'onControl', label: 'Я контролирую' },
   { id: 'toBeAgreed', label: 'Мне согласовать' },
