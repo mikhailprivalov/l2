59d1aaae17e52ffdaf4a6223640d3f73a19171cf
Well <Well@yandex.ru>
2024-07-09 22:58:25 +0800

    v3

--- directory/management/commands/import_refs_by_external_code.py
+++ directory/management/commands/import_refs_by_external_code.py
@@ -20,6 +20,7 @@ class Command(BaseCommand):
         для импорта референсов в фракции по внешнему коду
         """
         parser.add_argument('path', type=str)
+        parser.add_argument('path1', type=str)
 
     def handle(self, *args, **kwargs):
         fp = kwargs["path"]
@@ -28,11 +29,9 @@ class Command(BaseCommand):
 
         starts = False
         code_idx, conditions_idx, start_ref_idx, end_ref_idx = None, None, None, None
-        fraction = None
-        result_wb = Workbook()
-        result_ws = result_wb[result_wb.sheetnames[0]]
-        result_ws.append(['Внешний код', 'Название фракции (теста)', 'Статус'])
+        current_code = None
         ref_m, ref_f = [], []
+        result_first_file = {}
         for row in ws.rows:
             cells = [str(x.value) for x in row]
             if not starts:
@@ -47,6 +46,7 @@ class Command(BaseCommand):
                 conditions = cells[conditions_idx].strip()
                 start = cells[start_ref_idx].strip()
                 end = cells[end_ref_idx].strip()
+            
                 if start == "None" or end == "None":
                     continue
                 tmp_cond_str = conditions.split("Пол: ")
@@ -68,49 +68,57 @@ class Command(BaseCommand):
                     try:
                         age_start = float(age_range[0].strip()) * 365
                         age_end = float(age_range[1].strip()) * 365
-                        age = f"дней {age_start}-{age_end}"
+                        age = f"дней {int(age_start)}-{int(age_end)}"
                     except Exception:
                         self.stdout.write("Не удалось преобразовать в дни")
                         continue
                 elif not age:
                     age = "Все"
-
-                if code != "None":
-                    if fraction and (len(ref_m) > 0 or len(ref_f) > 0):
+                if code != "None" and code != current_code:
+                    
+                    if len(ref_m) > 0 or len(ref_f) > 0:
                         ref_m, ref_f = Fractions.convert_ref(ref_m, ref_f, True)
-                        if len(ref_m) > 0:
-                            fraction.ref_m = ref_m
-                        if len(ref_f) > 0:
-                            fraction.ref_f = ref_f
-                        fraction.save()
-                        result_ws.append([fraction.external_code, fraction.title, '+'])
-                        self.stdout.write(f"Референсы {fraction.title} обновлены")
-
-                    ref_m, ref_f = [], []
-                    fraction = Fractions.objects.filter(external_code__iexact=code).first()
-                    if not fraction:
-                        continue
-
+                        result_first_file[current_code] = { "fsli": "", "ref_m": ref_m, "ref_f": ref_f }
+                        ref_m, ref_f = [], []
+                    
                     if gender.lower() == "общий":
-                        if len(str(fraction.ref_m)) <= 2:
-                            ref_m.append({"age": age, "value": f"{start}-{end}"})
-                        if len(str(fraction.ref_f)) <= 2:
-                            ref_f.append({"age": age, "value": f"{start}-{end}"})
-                    elif gender.lower() == "мужской" and len(str(fraction.ref_m)) <= 2:
                         ref_m.append({"age": age, "value": f"{start}-{end}"})
-                    elif gender.lower() == "женский" and len(str(fraction.ref_f)) <= 2:
                         ref_f.append({"age": age, "value": f"{start}-{end}"})
-
-                elif code == "None" and fraction:
+                    elif gender.lower() == "мужской":
+                        ref_m.append({"age": age, "value": f"{start}-{end}"})
+                    elif gender.lower() == "женский":
+                        ref_f.append({"age": age, "value": f"{start}-{end}"})
+                        
+                    current_code = code
+                else:
                     if gender.lower() == "общий":
-                        if len(str(fraction.ref_m)) <= 2:
-                            ref_m.append({"age": age, "value": f"{start}-{end}"})
-                        if len(str(fraction.ref_f)) <= 2:
-                            ref_f.append({"age": age, "value": f"{start}-{end}"})
-                    elif gender.lower() == "мужской" and len(str(fraction.ref_m)) <= 2:
                         ref_m.append({"age": age, "value": f"{start}-{end}"})
-                    elif gender.lower() == "женский" and len(str(fraction.ref_f)) <= 2:
                         ref_f.append({"age": age, "value": f"{start}-{end}"})
+                    elif gender.lower() == "мужской":
+                        ref_m.append({"age": age, "value": f"{start}-{end}"})
+                    elif gender.lower() == "женский":
+                        ref_f.append({"age": age, "value": f"{start}-{end}"})
+            
+        # for key, value in result_first_file.items():
+        #     print(key, "key", value)
+        fp1 = kwargs["path1"]
+        print(fp1)
+        wb1 = load_workbook(filename=fp)
+        ws1 = wb1[wb.sheetnames[0]]
 
-        dir_tmp = SettingManager.get("dir_param")
-        result_wb.save(f"{dir_tmp}/result_import_refs.xlsx")
+        starts = False
+        code_idx, conditions_idx, start_ref_idx, end_ref_idx = None, None, None, None
+        current_code = None
+        ref_m, ref_f = [], []
+        result_first_file = {}
+        for row in ws1.rows:
+            cells = [str(x.value) for x in row]
+            if not starts:
+                if "Условия" in cells:
+                    code_idx = cells.index("Код")
+                    conditions_idx = cells.index("Условия")
+                    start_ref_idx = cells.index("Нижняя Гр.")
+                    end_ref_idx = cells.index("Верхняя Гр.")
+                    starts = True
+            else:
+                code = cells[code_idx].strip()
