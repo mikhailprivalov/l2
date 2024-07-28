fb82e8cd3046307c5cfee52b5d800608d02cb4e4
Well <Well@yandex.ru>
2024-07-28 16:21:03 +0800

    admin

--- cash_registers/admin.py
+++ cash_registers/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
+from cash_registers.models import CashRegister, Shift
+
+
 # Register your models here.
+class CashRegisterAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'title', 'ip_address', 'port']
+
+    search_fields = (
+        'pk',
+        'title',
+    )
+
+
+class ShiftAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'cash_register', 'open_at', 'close_at', 'operator']
+
+    search_fields = (
+        'pk',
+        'operator',
+        'cash_register'
+    )
+
+
+admin.site.register(CashRegister, CashRegisterAdmin)
+admin.site.register(Shift, ShiftAdmin)
