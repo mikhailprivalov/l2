31472c7c0df3841a5b2128c9807bab43f3e1133f
Well <Well.heor@yandex.ru>
2024-07-29 15:02:48 +0800

    fix

--- cash_registers/atol.py
+++ cash_registers/atol.py
@@ -3,5 +3,10 @@ import requests
 from laboratory.settings import CASH_REGISTER_SERVER_ADDRESS
 
 
-def send_request():
-    request = requests.post(CASH_REGISTER_SERVER_ADDRESS)
+def send_request(json_data: dict):
+    response = requests.post(CASH_REGISTER_SERVER_ADDRESS, json=json_data)
+    response_data = response.json()
+    return response_data
+
+
+
