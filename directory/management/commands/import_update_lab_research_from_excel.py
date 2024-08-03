aae1707fd0c778292de323f4dc76bdac18e7675b
sergei kasianenko <monitor84@list.ru>
2024-08-03 22:32:29 +0800

    imports data from xlsx

--- directory/management/commands/import_update_lab_research_from_excel.py
+++ directory/management/commands/import_update_lab_research_from_excel.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from directory.models import Fractions, LaboratoryMaterial, Researches, ReleationsFT
+from directory.models import Fractions, LaboratoryMaterial, Researches, ReleationsFT, SubGroupDirectory
 from openpyxl import load_workbook
 
 from podrazdeleniya.models import Podrazdeleniya
@@ -18,7 +18,7 @@ class Command(BaseCommand):
         wb = load_workbook(filename=fp)
         ws = wb[wb.sheetnames[0]]
         starts = False
-        internal_code, title, material, container, department, laboratory_duration = '', '', '', '', '', ''
+        internal_code, title, material, container, department, laboratory_duration, portion, sub_group = '', '', '', '', '', '', '', ''
         step = 0
         for row in ws.rows:
             cells = [str(x.value) for x in row]
@@ -30,24 +30,43 @@ class Command(BaseCommand):
                     container = cells.index("Контейнер")
                     department = cells.index("Подразделение")
                     laboratory_duration = cells.index("Готовность")
+                    portion = cells.index("Доля")
+                    sub_group = cells.index("Подгруппа")
                     starts = True
             else:
-                if Researches.objects.filter(title=cells[title].strip()):
-                    continue
                 material_obj = LaboratoryMaterial.objects.filter(title=cells[material].strip()).first()
                 department_obj = Podrazdeleniya.objects.filter(title=cells[department].strip()).first()
+                subgroup_obj = SubGroupDirectory.objects.filter(title=cells[department].strip()).first()
                 if cells[laboratory_duration] == "None":
                     cells[laboratory_duration] = ''
-                research = Researches(
-                    title=cells[title].strip(),
-                    internal_code=cells[internal_code],
-                    laboratory_material=material_obj,
-                    podrazdeleniye=department_obj,
-                    laboratory_duration=cells[laboratory_duration]
-                )
+                if float(cells[portion]) > 1:
+                    portion = 0
+                else:
+                    portion = float(cells[portion])
+                is_new_research = False
+                if Researches.objects.filter(title=cells[internal_code].strip()).exists():
+                    research = Researches.objects.filter(title=cells[internal_code].strip()).first()
+                    research.title = cells[title].strip()
+                    research.laboratory_material = material_obj
+                    research.podrazdeleniye = department_obj
+                    research.sub_group = subgroup_obj
+                    research.laboratory_duration = cells[laboratory_duration]
+                    research.count_volume_material_for_tube = portion
+                else:
+                    research = Researches(
+                        title=cells[title].strip(),
+                        internal_code=cells[internal_code],
+                        laboratory_material=material_obj,
+                        podrazdeleniye=department_obj,
+                        laboratory_duration=cells[laboratory_duration],
+                        count_volume_material_for_tube=portion,
+                        sub_group=subgroup_obj
+                    )
+                    is_new_research = True
                 research.save()
 
                 tube = Tubes.objects.filter(title=cells[container].strip()).first()
+
                 fraction_data, relation_f = None, None
                 if ReleationsFT.objects.filter(tube=tube).exists():
                     relation_tmp = None
@@ -72,11 +91,19 @@ class Command(BaseCommand):
                     relation_f = ReleationsFT(tube=tube)
                     relation_f.save()
 
-                fraction = Fractions(
-                    research=research,
-                    title=cells[title],
-                    relation=relation_f
-                )
-                fraction.save()
+                if is_new_research:
+                    fraction = Fractions(
+                        research=research,
+                        title=cells[title],
+                        relation=relation_f
+                    )
+                    fraction.save()
+                    self.stdout.write(f'{step}-Услуга добавлена - {research.title}, фракция - {fraction.title}')
+                else:
+                    fraction = Fractions.objects.filter(research=research).first()
+                    fraction.relation = relation_f
+                    fraction.save()
+                    self.stdout.write(f'{step}-Услуга обновленая - {research.title}, фракция - {fraction.title}')
                 step += 1
-                self.stdout.write(f'{step}-Услуга добавлена - {research.title}, фракция - {fraction.title}')
+
+
