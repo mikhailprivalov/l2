5776c46dfdbd530e8d1708540d40d7089bf6b582
Well <Well@yandex.ru>
2024-08-04 19:59:39 +0800

    get shift data fix

--- cash_registers/views.py
+++ cash_registers/views.py
@@ -40,38 +40,42 @@ def close_shift(cash_register_id: int, doctor_profile_id: int):
 
 
 def get_shift_data(doctor_profile_id: int):
-    """Проверка смены, status 0 - смена открывается, 1 - смена открыта, 2 - смена закрывается, -1 - Смена закрыта,"""
+    """Проверка смены, открывается, открыта, закрывается, закрыта"""
+    data = {"shiftId": None, "cashRegisterId": None, "cashRegisterTitle": "", "status": "Смена закрыта"}
+    result = {"ok": True, "data": data}
     shift: Shift = Shift.objects.filter(operator_id=doctor_profile_id, close_status=False).select_related('cash_register').last()
     uuid_data = None
-    status = None
-    if not shift:
-        data = {"shiftId": None, "cashRegisterId": None, "cashRegisterTitle": "", "status": -1}
-        return {"ok": True, "data": data}
-    if not shift.open_status and shift.open_uuid:
-        status = 0
-        uuid_data = shift.open_uuid
-    elif shift.open_status and not shift.close_uuid:
-        status = 1
-    elif shift.open_status and shift.close_uuid:
-        status = 2
-        uuid_data = shift.close_uuid
-    if not uuid_data:
-        data = {"shiftId": shift.pk, "cashRegisterId": shift.cash_register_id, "cashRegisterTitle": shift.cash_register.title, "status": status}
-        return {"ok": True, "data": data}
+    status = "Смена закрыта"
+    if shift:
+        result["data"] = {"shiftId": shift.pk, "cashRegisterId": shift.cash_register_id, "cashRegisterTitle": shift.cash_register.title, "status": ""}
+        if not shift.open_status and shift.open_uuid:
+            status = "Смена открывается"
+            uuid_data = shift.open_uuid
+        elif shift.open_status and not shift.close_uuid:
+            status = "Смена открыта"
+            result["data"]["status"] = status
+        elif shift.open_status and shift.close_uuid:
+            status = "Смена закрывается"
+            uuid_data = shift.close_uuid
 
-    cash_register_data = CashRegister.get_meta_data(shift.cash_register_id)
-    check_cash_register = cash_req.check_cash_register(cash_register_data)
-    if not check_cash_register["ok"]:
-        return check_cash_register
-    job_result = cash_req.get_job_status(str(uuid_data), cash_register_data)
-    if not job_result["ok"]:
-        return {"ok": False, "message": "Ошибка проверки задания"}
-    job_status = job_result["data"]["results"][0]
-    if job_status["status"] == "ready" and status == 0:
-        shift.confirm_open_shift()
-        status = 1
-    elif job_status["status"] == "ready" and status == 2:
-        shift.confirm_close_shift()
-        status = -1
-    data = {"shiftId": shift.pk, "cashRegisterId": shift.cash_register_id, "cashRegisterTitle": shift.cash_register.title, "status": status}
-    return {"ok": True, "data": data}
+        if uuid_data:
+            cash_register_data = CashRegister.get_meta_data(shift.cash_register_id)
+            check_cash_register = cash_req.check_cash_register(cash_register_data)
+            if check_cash_register["ok"]:
+                job_result = cash_req.get_job_status(str(uuid_data), cash_register_data)
+                if job_result["ok"]:
+                    job_status = job_result["data"]["results"][0]
+                    if job_status["status"] == "ready" and status == "Смена открывается":
+                        shift.confirm_open_shift()
+                        status = "Смена открыта"
+                        result["data"]["status"] = status
+                    elif job_status["status"] == "ready" and status == "Смена закрывается":
+                        shift.confirm_close_shift()
+                        status = "Смена закрыта"
+                        result["data"]["status"] = status
+                else:
+                    result = job_result
+            else:
+                result = check_cash_register
+
+    return result
