9efb9ed40ac0e613f949c4f1cadf7cc93e000af6
Well <Well@yandex.ru>
2024-08-04 19:03:42 +0800

    fix

--- cash_registers/sql_func.py
+++ cash_registers/sql_func.py
@@ -11,3 +11,23 @@ def get_cash_registers():
         )
         rows = namedtuplefetchall(cursor)
     return rows
+
+
+def check_shift(cash_register_id, doctor_profile_id, close_status):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+            SELECT
+            operator_id,
+            cash_register_id,
+            close_status
+            FROM cash_registers_shift
+            WHERE
+            (operator_id=$(doctor_profile_id)s or cash_register_id=$(cash_register_id)s)
+            and
+            close_status = False
+            """,
+            params={"cash_register_id": cash_register_id, "doctor_profile_id": doctor_profile_id}
+        )
+        rows = namedtuplefetchall(cursor)
+    return rows
