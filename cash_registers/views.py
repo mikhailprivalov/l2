3721ba41ea0c7740467edcac226195aa280d633b
Well <Well@yandex.ru>
2024-08-04 19:28:28 +0800

    open shift views fix

--- cash_registers/views.py
+++ cash_registers/views.py
@@ -8,16 +8,19 @@ def get_cash_registers():
 
 
 def open_shift(cash_register_id: int, doctor_profile_id: int):
+    result = {"ok": True, "message": ""}
     shift_job_data = Shift.get_shift_job_data(doctor_profile_id, cash_register_id)
     operator_data, cash_register_data, uuid_data = shift_job_data["operator_data"], shift_job_data["cash_register_data"], shift_job_data["uuid_data"]
     check_cash_register = cash_req.check_cash_register(cash_register_data)
-    if not check_cash_register["ok"]:
-        return check_cash_register
-    job_result = cash_req.open_shift(uuid_data, cash_register_data, operator_data)
-    if not job_result["ok"]:
-        return job_result
-    Shift.open_shift(str(uuid_data), cash_register_id, doctor_profile_id)
-    return {"ok": True, "message": ""}
+    if check_cash_register["ok"]:
+        job_result = cash_req.open_shift(uuid_data, cash_register_data, operator_data)
+        if job_result["ok"]:
+            Shift.open_shift(str(uuid_data), cash_register_id, doctor_profile_id)
+        else:
+            result = job_result
+    else:
+        result = check_cash_register
+    return result
 
 
 def close_shift(cash_register_id: int, doctor_profile_id: int):
