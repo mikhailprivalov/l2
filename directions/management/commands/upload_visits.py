e1426d48e2bdea8b8687bf9fd99de4e2967d551b
well <Well.heor@yandex.ru>
2024-09-16 13:54:55 +0800

    upload_visits script manage py

---
+++ directions/management/commands/upload_visits.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from api.parse_file.forms100 import form_02
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('path', type=str)
+
+    def handle(self, *args, **kwargs):
+        """
+        :param path - xlsx файл со столбцами:
+        Контейнер
+        """
+
+        fp = kwargs['path']
+        result = form_02({"file": fp})
+        if result["ok"]:
+            self.stdout.write('Файл выгружен')
+        else:
+            self.stdout.write("Ошибка выгрузки файла")
+
+
