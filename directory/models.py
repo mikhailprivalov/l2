5a1e0d94c7c6cb2dd5f122a0c6e76d6fe7029cbb
Well <72241044+Wellheor1@users.noreply.github.com>
2024-09-18 17:02:14 +0800

    Update directory/models.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- directory/models.py
+++ directory/models.py
@@ -1214,6 +1214,7 @@ class ParaclinicFieldTemplateDepartment(models.Model):
     """
     Шаблоны подразделений на поля
     """
+
     paraclinic_field = models.ForeignKey(ParaclinicInputField, verbose_name="Поле в кротоколе", on_delete=models.CASCADE)
     research = models.ForeignKey(Researches, verbose_name="Услуга", on_delete=models.CASCADE, db_index=True)
     department = models.ForeignKey(Podrazdeleniya, verbose_name="Подразделение", on_delete=models.CASCADE, db_index=True)
