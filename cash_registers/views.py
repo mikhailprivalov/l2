029c89751f6a2d1a14b670f89a824bd46f4adbf6
well <Well.heor@yandex.ru>
2024-09-22 18:26:34 +0800

    Проверка смены на кассе

--- cash_registers/views.py
+++ cash_registers/views.py
@@ -18,7 +18,7 @@ def open_shift(cash_register_id: int, doctor_profile_id: int):
     shift_job_data = Shift.get_shift_job_data(doctor_profile_id, cash_register_id)
     operator_data, cash_register_data, uuid_data = shift_job_data["operator_data"], shift_job_data["cash_register_data"], shift_job_data["uuid_data"]
     job_body = Shift.create_job_json(cash_register_data, uuid_data, operator_data)
-    check_cash_register = cash_req.check_cash_register(cash_register_data)
+    check_cash_register = cash_req.check_cash_register(cash_register_data, job_open_shift=True)
     if check_cash_register["ok"]:
         job_result = cash_req.send_job(job_body)
         if job_result["ok"]:
@@ -35,7 +35,7 @@ def close_shift(cash_register_id: int, doctor_profile_id: int):
     shift_job_data = Shift.get_shift_job_data(doctor_profile_id, cash_register_id)
     operator_data, cash_register_data, uuid_data = shift_job_data["operator_data"], shift_job_data["cash_register_data"], shift_job_data["uuid_data"]
     job_body = Shift.create_job_json(cash_register_data, uuid_data, operator_data, "closeShift")
-    check_cash_register = cash_req.check_cash_register(cash_register_data)
+    check_cash_register = cash_req.check_cash_register(cash_register_data, job_close_shift=True)
     if check_cash_register["ok"]:
         job_result = cash_req.send_job(job_body)
         if job_result["ok"]:
