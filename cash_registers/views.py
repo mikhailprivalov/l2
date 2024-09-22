3475ea09db800c17dce67b888c6843c8c4d1c482
well <Well.heor@yandex.ru>
2024-09-22 18:40:25 +0800

    fix shift open and close

--- cash_registers/views.py
+++ cash_registers/views.py
@@ -15,35 +15,41 @@ def get_cash_registers():
 
 def open_shift(cash_register_id: int, doctor_profile_id: int):
     result = {"ok": True, "message": ""}
-    shift_job_data = Shift.get_shift_job_data(doctor_profile_id, cash_register_id)
-    operator_data, cash_register_data, uuid_data = shift_job_data["operator_data"], shift_job_data["cash_register_data"], shift_job_data["uuid_data"]
-    job_body = Shift.create_job_json(cash_register_data, uuid_data, operator_data)
-    check_cash_register = cash_req.check_cash_register(cash_register_data, job_open_shift=True)
-    if check_cash_register["ok"]:
-        job_result = cash_req.send_job(job_body)
-        if job_result["ok"]:
-            Shift.open_shift(str(uuid_data), cash_register_id, doctor_profile_id)
+    if cash_register_id:
+        shift_job_data = Shift.get_shift_job_data(doctor_profile_id, cash_register_id)
+        operator_data, cash_register_data, uuid_data = shift_job_data["operator_data"], shift_job_data["cash_register_data"], shift_job_data["uuid_data"]
+        job_body = Shift.create_job_json(cash_register_data, uuid_data, operator_data)
+        check_cash_register = cash_req.check_cash_register(cash_register_data, job_open_shift=True)
+        if check_cash_register["ok"]:
+            job_result = cash_req.send_job(job_body)
+            if job_result["ok"]:
+                Shift.open_shift(str(uuid_data), cash_register_id, doctor_profile_id)
+            else:
+                result = job_result
         else:
-            result = job_result
+            result = check_cash_register
     else:
-        result = check_cash_register
+        result = {"ok": False, "message": "Не выбрана касса"}
     return result
 
 
 def close_shift(cash_register_id: int, doctor_profile_id: int):
     result = {"ok": True, "message": ""}
-    shift_job_data = Shift.get_shift_job_data(doctor_profile_id, cash_register_id)
-    operator_data, cash_register_data, uuid_data = shift_job_data["operator_data"], shift_job_data["cash_register_data"], shift_job_data["uuid_data"]
-    job_body = Shift.create_job_json(cash_register_data, uuid_data, operator_data, "closeShift")
-    check_cash_register = cash_req.check_cash_register(cash_register_data, job_close_shift=True)
-    if check_cash_register["ok"]:
-        job_result = cash_req.send_job(job_body)
-        if job_result["ok"]:
-            Shift.close_shift(uuid_data, cash_register_id, doctor_profile_id)
+    if cash_register_id:
+        shift_job_data = Shift.get_shift_job_data(doctor_profile_id, cash_register_id)
+        operator_data, cash_register_data, uuid_data = shift_job_data["operator_data"], shift_job_data["cash_register_data"], shift_job_data["uuid_data"]
+        job_body = Shift.create_job_json(cash_register_data, uuid_data, operator_data, "closeShift")
+        check_cash_register = cash_req.check_cash_register(cash_register_data, job_close_shift=True)
+        if check_cash_register["ok"]:
+            job_result = cash_req.send_job(job_body)
+            if job_result["ok"]:
+                Shift.close_shift(uuid_data, cash_register_id, doctor_profile_id)
+            else:
+                result = job_result
         else:
-            result = job_result
+            result = check_cash_register
     else:
-        result = check_cash_register
+        result = {"ok": False, "message": "Не выбрана касса"}
     return result
 
 
