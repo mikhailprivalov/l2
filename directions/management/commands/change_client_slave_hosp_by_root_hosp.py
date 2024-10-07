f93eafcd11ee4faf03995e8f7ed2248c54454160
well <Well.heor@yandex.ru>
2024-10-07 19:55:56 +0800

    create

---
+++ directions/management/commands/change_client_slave_hosp_by_root_hosp.py
@@ -0,0 +1,39 @@
+import re
+
+from django.core.management.base import BaseCommand
+from openpyxl import load_workbook
+
+from directions.models import Napravleniya, Issledovaniya
+from researches.models import Tubes
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('history_id', type=str)
+
+    def handle(self, *args, **kwargs):
+        """
+        :param path - xlsx файл со столбцами:
+        Контейнер
+        """
+
+        history_id = kwargs['history_id']
+        need_change_history_ids = set()
+        direction: Napravleniya = Napravleniya.objects.filter(pk=history_id).first()
+        if not direction:
+            self.stdout.write('Такого направления нет')
+            return None
+        issledovanie: Issledovaniya = Issledovaniya.objects.filter(napravleniye_id=direction.pk).select_related('research').first()
+        if not issledovanie.research.is_hospital:
+            self.stdout.write('Данное направление не является историей болезни')
+            return None
+        slave_hospital_directions: issledovanie = Napravleniya.objects.filter(parent_id=issledovanie.pk)
+        for slave_direction in slave_hospital_directions:
+            slave_direction.client_id = direction.client_id
+            slave_direction.save()
+            self.stdout.write(f'У направления {slave_direction.pk} изменена карта на {direction.client_id}')
+            slave_direction_issledovanie: Issledovaniya = Issledovaniya.objects.filter(napravleniye_id=slave_direction.pk).select_related('research').first()
+            if slave_direction_issledovanie.research.is_hospital:
+                need_change_history_ids.add(slave_direction.pk)
+        self.stdout.write(f'Следующие направления - истории болезни (при необходимости повторите скрипт с этими направлениями) \n'
+                          f'{need_change_history_ids}')
