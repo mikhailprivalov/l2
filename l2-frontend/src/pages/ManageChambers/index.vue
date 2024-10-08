5b2b2d137e81e75559c5c8d153e51748d3fbcbd9
well <Well.heor@yandex.ru>
2024-10-08 11:11:38 +0800

    fix растягивания ghost

--- l2-frontend/src/pages/ManageChambers/index.vue
+++ l2-frontend/src/pages/ManageChambers/index.vue
@@ -781,6 +781,7 @@ onMounted(init);
     background-color: #fff;
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
     border-radius: 4px;
+    align-self: flex-start;
   }
   &-beds,
   &-doctor {
