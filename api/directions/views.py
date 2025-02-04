dbca22273e76903723e1e589fff615c20a2853f1
sergei kasianenko <monitor84@list.ru>
2025-02-04 17:08:47 +0800

    fix

--- api/directions/views.py
+++ api/directions/views.py
@@ -1880,7 +1880,8 @@ def directions_paraclinic_form(request):
                         }
                     )
 
-                if not force_form and (iss["research"]["is_doc_refferal"] or iss["research"]["is_gistology"] or iss["research"]["is_paraclinic"] or iss["research"]["is_form"]):
+                if not force_form and (
+                        iss["research"]["is_doc_refferal"] or iss["research"]["is_gistology"] or iss["research"]["is_paraclinic"] or iss["research"]["is_form"] or iss["research"]["is_aux"]):
                     iss = {
                         **iss,
                         "purpose": i.purpose_id or -1,
