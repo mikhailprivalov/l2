10749b36ec4d24a6004275601be2208b482705b2
sergei kasianenko <monitor84@list.ru>
2025-02-04 15:23:15 +0800

    fix paraclinic

--- l2-frontend/src/pages/ResultsParaclinic.vue
+++ l2-frontend/src/pages/ResultsParaclinic.vue
@@ -784,9 +784,9 @@
             :patient="data.patient"
             :change_mkb="change_mkb(row)"
             :pk="row.pk"
-            :price_category="row.price_category || -1"
+            :price_category="row.price_category"
             :is_gistology="row.research.is_gistology"
-            :paid_fin_source="paidFinSource(row.fin_source, row.fin_source_list) || false"
+            :paid_fin_source="paidFinSource(row.fin_source, row.fin_source_list)"
           />
           <div
             v-if="
@@ -963,7 +963,7 @@
             </div>
           </div>
           <div
-            v-if="(row.research.is_doc_refferal || row.research.is_gistology) && stat_btn && !is_operator_protocol"
+            v-if="(row.research.is_doc_refferal || row.research.is_gistology || row.research.is_paraclinic ) && stat_btn && !is_operator_protocol"
             class="group"
           >
             <div class="group-title">
