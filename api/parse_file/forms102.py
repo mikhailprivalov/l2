71baa75bbbfd08617e79ef6819373c78800876e7
well <Well.heor@yandex.ru>
2025-02-04 22:05:16 +0800

    возврат ошибочных пробирок

--- api/parse_file/forms102.py
+++ api/parse_file/forms102.py
@@ -9,7 +9,6 @@ def check_need_col(cols: list, need_cols: set):
         return False
     return True
 
-
 def form_01(request_data):
     """
     Загрузка цен по прайсу
@@ -20,32 +19,11 @@ def form_01(request_data):
     Наименование (Tube.title), Краткое наименование (Tube.short_title), Цвет (красный, зеленый)
     """
 
-    colors = {
-        "красн": "#FF0000",
-        "оранж": "#FF8C00",
-        "желт": "#FFFF00",
-        "жёлт": "#FFFF00",
-        "зелен": "#008000",
-        "голуб": "#00FFFF",
-        "синий": "#0000FF",
-        "синего": "#0000FF",
-        "фиолет": "#9400D3",
-        "сирен": "#c8a2c8",
-        "розов": "#FF1493",
-        "белый": "#FFFFFF",
-        "белая": "#FFFFFF",
-        "белого": "#FFFFFF",
-        "белой": "#FFFFFF",
-        "серый": "#808080",
-        "серая": "#808080",
-        "серого": "#808080",
-        "серой": "#808080",
-        "корич": "#8B4513",
-    }
-
     file = request_data.get("file")
     wb = load_workbook(filename=file)
     ws = wb[wb.sheetnames[0]]
+    columns = [{"field": 'title', "key": 'title', "title": 'Пробирка', "align": 'left', "width": 250}, {"field": 'reason', "key": 'reason', "title": 'Причина ошибки'}]
+    incorrect_tubes = []
     title_idx, short_title_idx, color_idx = (
         '',
         '',
@@ -69,10 +47,15 @@ def form_01(request_data):
             color = cells[color_idx].strip()
             valid = Tubes.check_tube(title, short_title, color)
             if not valid:
-                return {"ok": False, "result": {}, "message": "Валидация не пройдена"}
+                incorrect_tubes.append({"title": title, "reason": "Валидация не пройдена"})
+                continue
             new_tube = Tubes(title=title, short_title=short_title, color=color)
             new_tube.save()
+    result = {
+        "colData": columns,
+        "data": incorrect_tubes,
+    }
 
     if not starts:
         return {"ok": False, "result": [], "message": "Не найдены колонка 'цвет (rgb, hex)'"}
-    return {"ok": True, "result": [], "message": ""}
+    return {"ok": True, "result": result, "message": ""}
