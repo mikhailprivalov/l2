d75cb213d8d0c68d5d4cca7015940ae333a7c46a
sergei kasianenko <monitor84@list.ru>
2025-02-08 23:29:49 +0800

    fix

--- integration_framework/statistic/views.py
+++ integration_framework/statistic/views.py
@@ -11,7 +11,10 @@ def get_statistic_research(request):
     data = json.loads(request.body)
     request_obj = HttpRequest()
     request_obj.GET = data
-    request_obj.user = request.user
-    xlsx_content = statistic_xls(request_obj)
-    result_b64 = base64.b64encode(xlsx_content.content).decode("utf-8")
-    return Response({"data": result_b64})
+    if not request.user.unlimited_access:
+        result = "Доступ запрещён"
+    else:
+        request_obj.user = request.user
+        xlsx_content = statistic_xls(request_obj)
+        result = base64.b64encode(xlsx_content.content).decode("utf-8")
+    return Response({"data": result})
