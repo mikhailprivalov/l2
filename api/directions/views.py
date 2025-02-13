2bc1da31eeb3d7f1567353a19bfd485980af2912
Sergei Kasianenko <41939763+urchinpro@users.noreply.github.com>
2025-02-13 11:37:53 +0800

    Update api/directions/views.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- api/directions/views.py
+++ api/directions/views.py
@@ -3178,7 +3178,6 @@ def last_field_result(request):
             field_pks = [i for i in field_pks]
             result = field_get_link_data_by_cda(tuple(field_pks), client_pk, use_parent_iss='1', days_ago=days_ago)
 
-
     elif request_data["fieldPk"].find('%control_param#') != -1:
         # %control_param#code#period#find_val
         data = request_data["fieldPk"].split('#')
