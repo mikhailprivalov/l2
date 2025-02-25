4cbe7c93812e67c4ae032ce95bb8f081396ca95e
well <Well.heor@yandex.ru>
2025-02-25 14:22:26 +0800

    fix

--- api/parse_file/forms103.py
+++ api/parse_file/forms103.py
@@ -224,6 +224,7 @@ def update_organization_employee_positions(organization_id: int, employees):
     departments = Department.get_active_departments(organization_id)
     positions = Position.get_active_positions(organization_id)
     employment_forms = TypeWorkTimeEmployee.objects.all()
+    incorrent_employees = []
     for employee in employees:
         current_employee = Employee.find_by_snils(employee["snils"], organization_id)
         ## todo - если найдено, надо ли обнолять фио?
@@ -234,7 +235,8 @@ def update_organization_employee_positions(organization_id: int, employees):
         current_position = positions.get(name=employee["position_title"])
         current_employment_form = employment_forms.filter(title=employee["employment_form"])
         if not current_employment_form:
-            return {"ok": False, "message": "", "data": {"fio": employee["fio"], "reason": "Нет такого вид занятости в справочнике"}}
+            incorrent_employees.append({"fio": employee["fio"], "reason": "Нет такого вид занятости в справочнике"})
+            continue
         current_employee_position = EmployeePosition.objects.filter(is_active=True, employee_id=current_employee.pk, position_id=current_position.pk, department_id=current_department.pk,
                                                                     tabel_number=employee["tabel_number"]).first()
         if current_employee_position and employee["date_dismissal"]:
@@ -249,7 +251,7 @@ def update_organization_employee_positions(organization_id: int, employees):
             new_employee_position = EmployeePosition(is_active=True, employee_id=current_employee.pk, position_id=current_position.pk, department_id=current_department.pk,
                                                      tabel_number=employee["tabel_number"], rate=employee["rate"])
             new_employee_position.save()
-        return {"ok": True, "message": ""}
+    return {"ok": True, "message": "", "data": incorrent_employees}
 
 
 def form_01(request_data):
@@ -282,7 +284,7 @@ def form_01(request_data):
     update_organization_positions(organization_id, positions_titles)
     result_update = update_organization_employee_positions(organization_id, employees)
     if not result_update["ok"]:
-        incorrect_employees.append(result_update["data"])
+        incorrect_employees.extend(result_update["data"])
     columns = [{"field": "fio", "key": "fio", "title": "Сотрудник", "align": "left", "width": 250}, {"field": "reason", "key": "reason", "title": 'Причина ошибки'}]
     result = {
         "colData": columns,
