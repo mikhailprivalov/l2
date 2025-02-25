ff6f6ba7b406aaa17e30aa880541611b232e93b5
well <Well.heor@yandex.ru>
2025-02-25 13:53:37 +0800

    fix rate save

--- api/parse_file/forms103.py
+++ api/parse_file/forms103.py
@@ -231,7 +231,7 @@ def update_organization_employee_positions(organization_id: int, employees):
             continue
         if not current_employee_position:
             new_employee_position = EmployeePosition(is_active=True, employee_id=current_employee.pk, position_id=current_position.pk, department_id=current_department.pk,
-                                                     tabel_number=employee["tabel_number"])
+                                                     tabel_number=employee["tabel_number"], rate=employee["rate"])
             new_employee_position.save()
 
 
