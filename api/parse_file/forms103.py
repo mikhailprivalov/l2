0b6b6b0babe9e4e87cf19e6f25c296c7279b25b7
well <Well.heor@yandex.ru>
2025-02-25 14:31:09 +0800

    вид занятости и дата приема на работу - сохранять

--- api/parse_file/forms103.py
+++ api/parse_file/forms103.py
@@ -249,7 +249,8 @@ def update_organization_employee_positions(organization_id: int, employees):
             continue
         elif not current_employee_position:
             new_employee_position = EmployeePosition(is_active=True, employee_id=current_employee.pk, position_id=current_position.pk, department_id=current_department.pk,
-                                                     tabel_number=employee["tabel_number"], rate=employee["rate"])
+                                                     tabel_number=employee["tabel_number"], rate=employee["rate"], type_work_time_id=current_employment_form.pk,
+                                                     date_employment=employee["date_employment"])
             new_employee_position.save()
     return {"ok": True, "message": "", "data": incorrent_employees}
 
