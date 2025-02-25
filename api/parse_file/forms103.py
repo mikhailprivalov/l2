58459fb8142fc5bae2e91fe4612004bf6aa9e458
well <Well.heor@yandex.ru>
2025-02-25 13:47:39 +0800

    tod

--- api/parse_file/forms103.py
+++ api/parse_file/forms103.py
@@ -113,6 +113,8 @@ def normalize_employee_data(employment_form, snils, tabel_number, fio, departmen
         "date_employment": None,
         "date_dismissal": None,
     }
+
+    ## todo возможно стоит получать дискт и перебирая его ключи нормализовывать как в валидации
     if string_not_empty(employment_form):
         result["employment_form"] = remove_spaces(employment_form)
     if string_not_empty(snils):
