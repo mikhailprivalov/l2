89576c0560ad196c337e3237943743233d4e6f8f
well <Well.heor@yandex.ru>
2025-03-05 17:05:05 +0800

    fix kwargs

--- api/parse_file/forms103.py
+++ api/parse_file/forms103.py
@@ -72,17 +72,18 @@ def parse_work_sheet(ws: Worksheet):
                 date_dismissal_idx = cells.index("Дата увольнения")
                 starts = True
         else:
-            normalized_employee_data = normalize_employee_data(
-                employment_form=cells[employment_form_idx],
-                snils=cells[snils_idx],
-                tabel_number=cells[tabel_number_idx],
-                fio=cells[employee_fio_idx],
-                department_title=cells[department_title_idx],
-                position_title=cells[position_title_idx],
-                rate=cells[rate_idx],
-                date_employment=cells[date_employment_idx],
-                date_dismissal=cells[date_dismissal_idx],
-            )
+            employee_data = {
+                "employment_form": cells[employment_form_idx],
+                "snils": cells[snils_idx],
+                "tabel_number": cells[tabel_number_idx],
+                "fio": cells[employee_fio_idx],
+                "department_title": cells[department_title_idx],
+                "position_title": cells[position_title_idx],
+                "rate": cells[rate_idx],
+                "date_employment": cells[date_employment_idx],
+                "date_dismissal": cells[date_dismissal_idx]
+            }
+            normalized_employee_data = normalize_employee_data(employee_data)
             validation_result = validate_employee_data(normalized_employee_data)
             if not validation_result.get("ok") and validation_result.get("empty"):
                 continue
@@ -98,7 +99,7 @@ def parse_work_sheet(ws: Worksheet):
     return result
 
 
-def normalize_employee_data(**kwargs):
+def normalize_employee_data(employee_data: dict):
     result = {
         "employment_form": None,
         "snils": None,
@@ -125,10 +126,10 @@ def normalize_employee_data(**kwargs):
         "date_dismissal": ["normalize_date"],
     }
 
-    for key in kwargs.keys():
+    for key in employee_data.keys():
         actions = actions_list.get(key)
         if actions:
-            action_result = normalize_values(kwargs[key], actions)
+            action_result = normalize_values(employee_data[key], actions)
             result[key] = action_result
 
     if result["fio"]:
