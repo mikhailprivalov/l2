a2699e1ccdc050765c54152ff82b65102aa8d264
Sergei Kasianenko <41939763+urchinpro@users.noreply.github.com>
2025-04-08 09:45:35 +0800

    Update integration_framework/views.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- integration_framework/views.py
+++ integration_framework/views.py
@@ -389,9 +389,7 @@ def issledovaniye_data(request):
     need_ecp_code = request.GET.get("needEcpCode") == "true"
     i = directions.Issledovaniya.objects.get(pk=pk)
     if i.research.podrazdeleniye__p_type != 2:
-        return Response(
-            {"ok": False, "ignore_sample": ignore_sample, "type": "is not laboratory"}
-        )
+        return Response({"ok": False, "ignore_sample": ignore_sample, "type": "is not laboratory"})
 
     sample = directions.TubesRegistration.objects.filter(issledovaniya=i, time_get__isnull=False).first()
     if ignore_fsli:
