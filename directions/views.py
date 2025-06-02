e9ca032c5fb7bdc8e808262cdbf68ebe55724a39
sergei kasianenko <monitor84@list.ru>
2025-06-02 08:41:17 +0800

    fix

--- directions/views.py
+++ directions/views.py
@@ -1181,8 +1181,7 @@ def create_case_by_cards(cards):
                 case_by_direction=True,
                 plan_start_date=plan_start_date_case,
             )
-        print("result")
-        print(result)
+
         if number_directons:
             number_directons = [i for i in number_directons]
         else:
