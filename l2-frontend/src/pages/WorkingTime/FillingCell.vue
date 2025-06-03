cf1f78a0d6d903cdb82e40cc3b28701a1ead4480
well <Well.heor@yandex.ru>
2025-06-03 20:56:54 +0800

    create cell

---
+++ l2-frontend/src/pages/WorkingTime/FillingCell.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button class="btn btn-blue-nb">
+      Заполнить по шаблону
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
