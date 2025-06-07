4e3e2ea538865c8083159a58db87c19d6810b8f2
well <Well.heor@yandex.ru>
2025-06-08 01:25:14 +0800

    сортировка tooltip + treeselect

--- l2-frontend/src/pages/WorkingTime/PositionHeader.vue
+++ l2-frontend/src/pages/WorkingTime/PositionHeader.vue
@@ -16,19 +16,18 @@
       :class="selectedPositionIds.length > 0 ? 'fa-filter-circle-xmark': 'fa-filter'"
     />
     <i
-      v-if="currentSort === 'none'"
+      v-tippy="{
+        html: `#tempPositionSorted`,
+        arrow: true,
+        reactive: true,
+        interactive: true,
+        animation: 'fade',
+        duration: 0,
+        theme: 'light',
+        placement: 'bottom',
+        trigger: 'click',
+      }"
       class="fa fa-sort column-icon"
-      @click="changeSort"
-    />
-    <i
-      v-else-if="currentSort === 'asc'"
-      class="fa fa-sort-up column-icon"
-      @click="changeSort"
-    />
-    <i
-      v-else
-      class="fa fa-sort-down column-icon"
-      @click="changeSort"
     />
     <div
       id="tempPositionFilter"
@@ -52,6 +51,19 @@
           class="treeselect-options"
           :title="node.label"
         > {{ node.label }}</label>
+      </Treeselect>
+    </div>
+    <div
+      id="tempPositionSorted"
+      class="tp"
+    >
+      <Treeselect
+        v-model="currentSort"
+        :options="sortVariants"
+        class="treeselect-34px"
+        placeholder="Сортировать"
+        :always-open="true"
+      >
       </Treeselect>
     </div>
   </div>
@@ -94,16 +106,12 @@ const normalizer = (node) => ({
   label: node.position,
 });
 
-const sortVariant = ref(['asc', 'desc', 'none']);
+const sortVariants = ref([
+  { id: 'none', label: 'Нет' },
+  { id: 'asc', label: 'А-Я' },
+  { id: 'desc', label: 'Я-А' },
+]);
 const currentSort = ref('none');
-const changeSort = () => {
-  const currentIndex = sortVariant.value.indexOf(currentSort.value);
-  if (currentIndex === 2) {
-    [currentSort.value] = sortVariant.value;
-  } else {
-    currentSort.value = sortVariant.value[currentIndex + 1];
-  }
-};
 
 watch(currentSort, () => {
   emit('sort', currentSort.value);
