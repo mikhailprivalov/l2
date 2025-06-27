5416ae9b902ed2c9da38b37602909f21cdc41f0d
Sergei Kasianenko <41939763+urchinpro@users.noreply.github.com>
2025-06-27 10:48:05 +0800

    Update api/directions/views.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- api/directions/views.py
+++ api/directions/views.py
@@ -86,7 +86,8 @@ from laboratory.settings import (
     SHOW_EXAMINATION_DATE_IN_PARACLINIC_RESULT_PAGE,
     DICOM_SERVERS,
     TUBE_MAX_RESEARCH_WITH_SHARE,
-    CDA_ID_FOR_DATE_CLOSE_CASE, WEB_PLUGIN_LINK_STUDY,
+    CDA_ID_FOR_DATE_CLOSE_CASE,
+    WEB_PLUGIN_LINK_STUDY,
 )
 from laboratory.utils import current_year, strdateru, strdatetime, strdate, strdatetimeru, strtime, tsdatetime, start_end_year, strfdatetime, current_time, replace_tz
 from pharmacotherapy.models import ProcedureList, ProcedureListTimes, Drugs, FormRelease, MethodsReception
