56806804ac29efd957555dc888581cd65b7a8707
well <Well.heor@yandex.ru>
2025-09-16 21:49:17 +0800

    Удали лишние import

--- forms/forms300.py
+++ forms/forms300.py
@@ -5,12 +5,9 @@ from io import BytesIO
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.units import mm
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.platypus import SimpleDocTemplate, Paragraph
 
 from forms.utils import register_fonts
-from laboratory.settings import FONTS_FOLDER
 
 
 def form_01(request_data):
