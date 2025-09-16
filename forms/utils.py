a9ff604d773db06389c35d619e3055631b5efcdb
well <Well.heor@yandex.ru>
2025-09-16 21:47:33 +0800

    Создай функцию регистрации шрифтов
    Добавлена общая функция регистрации шрифтов в reportlab

---
+++ forms/utils.py
@@ -0,0 +1,17 @@
+import os
+from typing import List, Dict, TypedDict
+
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+from laboratory.settings import FONTS_FOLDER
+
+
+def register_fonts(fonts: List[Dict[str, str]] = None) -> None:
+    if fonts:
+        for font in fonts:
+            pdfmetrics.registerFont(TTFont(font.get("name"), os.path.join(FONTS_FOLDER, font.get("filename"))))
+    else:
+        pdfmetrics.registerFont(TTFont('PTAstraSerifBold', os.path.join(FONTS_FOLDER, 'PTAstraSerif-Bold.ttf')))
+        pdfmetrics.registerFont(TTFont('PTAstraSerifReg', os.path.join(FONTS_FOLDER, 'PTAstraSerif-Regular.ttf')))
+    return None
