42147cf7d07c7a3d4636fbba54d95b3ace20bb71
well <Well.heor@yandex.ru>
2025-09-17 21:55:07 +0800

    Добавь стили для печатной формы графика
    Добавлены стили из старого варианта графика с изменениями

--- forms/forms300.py
+++ forms/forms300.py
@@ -11,7 +11,17 @@ def form_01(request_data):
     Создание печатной формы графика рабочего времени
     """
     register_fonts()
-    style = create_style()
+    style = create_style(font_size=10, alignment="justify")
+    style_center = create_style(style, alignment="center")
+    style_center_bold = create_style(style_center, "PTAstraSerifBold")
+    style_center_title = create_style(style_center, font_size=7)
+    style_center_sup = create_style(style_center, font_size=5)
+    style_center_data = create_style(style_center, font_size=8)
+    style_center_data_bold = create_style(style_center_bold)  # TODO Заменить его в коде на style_center_bold
+    style_center_data_title = create_style(style_center_title)  # TODO Заменить его в коде на style_center_title
+    style_left = create_style(font_size=6)
+    style_right = create_style(style_left, alignment="right")
+    style_right_bold = create_style(style_right, "PTAstraSerifBold")
 
     objs = []
     objs.append(Paragraph('Привет', style))
