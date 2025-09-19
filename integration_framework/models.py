d2e872e58e59483986585287462fd184f3598710
Sergei Kasianenko <41939763+urchinpro@users.noreply.github.com>
2025-09-19 20:21:22 +0800

    Update integration_framework/models.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- integration_framework/models.py
+++ integration_framework/models.py
@@ -233,7 +233,7 @@ class EquipmentReceive(models.Model):
         equipment_model = Equipment.objects.filter(sequence_study_instance_uid=equipment_title).first()
         eqr = EquipmentReceive(
             tag_patient_name=data.get("tag_patient_name"),
-            tag_study_date= data.get("tag_study_date"),
+            tag_study_date=data.get("tag_study_date"),
             tag_station_name=data.get("tag_station_name"),
             tag_patient_sex =data.get("tag_patient_sex"),
             tag_patient_birthdate=data.get("tag_patient_birthdate"),
