e4cee2a87146cceb1570d13accd936812506081f
Sergei Kasianenko <41939763+urchinpro@users.noreply.github.com>
2025-10-06 20:24:59 +0800

    Update integration_framework/models.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- integration_framework/models.py
+++ integration_framework/models.py
@@ -245,10 +245,7 @@ class EquipmentReceive(models.Model):
             tag_institution_name = data.get("tag_institution_name")
             tag_station_name = data.get("tag_station_name")
             equipment_model = Equipment.objects.filter(
-                institution_name=tag_institution_name,
-                manufacturer=tag_manufacturer,
-                manufacturer_model_name=tag_manufacturer_model_name,
-                station_name=tag_station_name
+                institution_name=tag_institution_name, manufacturer=tag_manufacturer, manufacturer_model_name=tag_manufacturer_model_name, station_name=tag_station_name
             ).first()
             if equipment_model:
                 eqr = EquipmentReceive(
