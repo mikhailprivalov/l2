e77389d3d14d325707952fd1df293f02b64f308b
well <Well.heor@yandex.ru>
2025-10-30 20:45:17 +0800

    Добавь функцию получения первого числа из строчки

--- api/parse_file/normalization.py
+++ api/parse_file/normalization.py
@@ -42,6 +42,15 @@ def normalize_date(value: str):
     return normalized_value
 
 
+def get_first_number(value: str):
+    """Получает из строки первое число """
+    values = value.split(" ")
+    for value in values:
+        if value.isnumeric():
+            return int(value)
+    return None
+
+
 def normalize_values(value: str, actions: set):
     """
     Перебирает действия по нормализации
