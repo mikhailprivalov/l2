4c7079683201e96364b75c2c5a7d1092f59c564c
well <Well.heor@yandex.ru>
2025-11-03 14:40:57 +0800

    Добавь кнопки "По умолчанию" "из предыдущего"
    Добавлены две кнопки, при вызове будет возникать диалоговое окно "дописать" "заменить"

--- l2-frontend/src/pages/WorkingTime/FillingCell.vue
+++ l2-frontend/src/pages/WorkingTime/FillingCell.vue
@@ -1,23 +1,41 @@
 <template>
-  <div class="flex">
-    <button
-      class="btn btn-blue-nb"
-      @click="fill"
-    >
-      Заполнить по шаблону
-    </button>
-    <button
-      class="btn btn-blue-nb"
-      @click="clear"
-    >
-      Очистить шаблон
-    </button>
+  <div class="fill-template">
+    <div class="flex">
+      <button
+        class="btn btn-blue-nb row-item"
+        @click="fill"
+      >
+        Заполнить по шаблону
+      </button>
+      <button
+        class="btn btn-blue-nb row-item"
+        @click="clear"
+      >
+        Очистить шаблон
+      </button>
+    </div>
+    <div class="flex">
+      <button
+        class="btn btn-blue-nb row-item"
+        @click="fillByEmployeesTemplate"
+      >
+        По умолчанию
+      </button>
+      <button
+        class="btn btn-blue-nb row-item"
+      >
+        Из предыдущего
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 
+import { getCurrentInstance } from 'vue';
+
 const emit = defineEmits(['fill', 'clear']);
+const root = getCurrentInstance().proxy.$root;
 
 const fill = () => {
   emit('fill');
@@ -25,14 +43,30 @@ const fill = () => {
 const clear = () => {
   emit('clear');
 };
+
+const fillByEmployeesTemplate = async () => {
+  try {
+    await root.$dialog.confirm('Выберите вариант заполнения', {
+      okText: 'Дописать',
+      cancelText: 'Заменить',
+    });
+    console.log('выбрано дописать');
+  } catch (_) {
+    console.log('выбрано заменить');
+  }
+};
 </script>
 
 <style scoped lang="scss">
 .flex {
   display: flex;
-  justify-content: center;
   gap: 5px;
-  height: 100%;
-  align-items: flex-start;
+  margin: 2px;
+}
+.row-item {
+  flex: 1;
+}
+.fill-template {
+  white-space: normal;
 }
 </style>
