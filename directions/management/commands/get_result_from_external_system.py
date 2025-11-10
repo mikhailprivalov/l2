586d4f06127e15d1389b53722eda5ee033a6a74b
sergei kasianenko <monitor84@list.ru>
2025-11-10 14:54:20 +0800

    fix

--- directions/management/commands/get_result_from_external_system.py
+++ directions/management/commands/get_result_from_external_system.py
@@ -2,7 +2,6 @@ from external_rest_integration.utils import rest_api_pull_result
 from django.core.management.base import BaseCommand
 
 
-
 class Command(BaseCommand):
     help = "Получить ре-таты извне"
 
