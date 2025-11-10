2b455ef20350ad7122fa5d1351c3c7065661c954
Sergei Kasianenko <41939763+urchinpro@users.noreply.github.com>
2025-11-10 15:00:08 +0800

    Update rest_api_pull_result_start.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- rest_api_pull_result_start.py
+++ rest_api_pull_result_start.py
@@ -4,7 +4,7 @@ import django  # noqa E402
 
 django.setup()
 
-from external_rest_integration.utils import process_rest_api_pull_result_star # noqa E402
+from external_rest_integration.utils import process_rest_api_pull_result_star  # noqa E402
 
 if __name__ == "__main__":
     process_rest_api_pull_result_star()
