890d1d94b46a0e957540fd66af8c911557736246
sergei kasianenko <monitor84@list.ru>
2025-11-10 14:39:04 +0800

    fix

---
+++ rest_api_pull_result_start.py
@@ -0,0 +1,10 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "laboratory.settings")
+import django  # noqa E402
+
+django.setup()
+
+from external_rest_integration.utils import process_rest_api_pull_result_star # noqa E402
+
+if __name__ == "__main__":
+    process_rest_api_pull_result_star()
