c1f15d016d5602412012e5c0a611a1a026f9286c
sergei kasianenko <monitor84@list.ru>
2025-11-13 10:01:10 +0800

    fix api

--- integration_framework/admission_cert/views.py
+++ integration_framework/admission_cert/views.py
@@ -36,6 +36,9 @@ def get_med_protocols(request):
     closed_case_need_send = get_closed_case_by_company(companies_id, limit)
     cases_iss = set([i.case_issledovaniye_id for i in closed_case_need_send])
     result_iss_id = directions_by_parent_cases_issledovaniye_only_research_id_final_report(tuple(cases_iss))
+    if not result_iss_id:
+        return Response({"result": "None", "comment": "No new results"})
+
     direction_iss = {
         i.iss_id: {
             "dir": i.napravleniye_id,
