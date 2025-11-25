6a5f75b44c507b850fa2abcb1af9ad64264f832e
Sergei Kasianenko <41939763+urchinpro@users.noreply.github.com>
2025-11-25 22:48:00 +0800

    Update medical_certificates/forms/forms380.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- medical_certificates/forms/forms380.py
+++ medical_certificates/forms/forms380.py
@@ -2185,7 +2185,9 @@ def form_22(request_data):
     fwb.append(
         Paragraph(
             f'4. Дата выдачи медицинского заключения: число <u>{space_symbol * 2} {date_result[0]} {space_symbol * 2}</u> месяц <u>{space_symbol * 2}{date_result[1]}{space_symbol * 2}</u>'
-            f'год<u>{space_symbol * 2}{date_result[2]}{space_symbol * 2}</u>', style,)
+            f'год<u>{space_symbol * 2}{date_result[2]}{space_symbol * 2}</u>',
+            style,
+        )
     )
     fwb.append(Spacer(1, 2 * mm))
     fwb.append(Paragraph(f'5. Медицинское заключение: {result_protocol}', style))
