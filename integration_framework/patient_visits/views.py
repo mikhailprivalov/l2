99f32d779ae30f157970bee31429023cb6af7f59
sergei kasianenko <monitor84@list.ru>
2025-12-16 12:22:02 +0800

    structure response

--- integration_framework/patient_visits/views.py
+++ integration_framework/patient_visits/views.py
@@ -1,8 +1,12 @@
+import codecs
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from api.models import Application
 from directions.models import Napravleniya
 import simplejson as json
+from results.sql_func import get_paraclinic_results_by_direction
+from xml_generate.views import gen_result_cda_files
 
 
 @api_view(['POST'])
@@ -12,7 +16,6 @@ def data_by_direction(request):
     if not token:
         return Response({"message": "token is empty"})
     token_is_not_valid = False
-    app = None
     try:
         app = Application.objects.filter(active=True, key=token).first()
         if not app:
@@ -25,7 +28,66 @@ def data_by_direction(request):
 
     data = json.loads(request.body)
     direction_id = data.get("directionId")
-
     direction = Napravleniya.objects.filter(pk=direction_id).first()
+    result_l2 = get_direction_data_by_cda_group(direction.pk)
 
-    return Response({"direction": direction.pk})
+    result_tempalte = gen_result_cda_files("protocol/proto.js", result_l2)
+
+    result = {
+        "patient": {
+            "family": "",
+            "name": "",
+            "patronymic": "",
+            "birthday": "",
+        },
+        "service": {
+            "date": "",
+            "time": "",
+            "protocol": "",
+            "mainDiagnos": "",
+            "code": "",
+        },
+        "doctor": {
+            "additionalInfo": ""
+        },
+    }
+
+    return Response({"direction": result_tempalte})
+
+
+def get_direction_data_by_cda_group(direction_pk):
+    result = get_paraclinic_results_by_direction(direction_pk)
+    data = {}
+    for i in result:
+        if i.cda_field_code and i.value:
+            if not data.get(i.cda_field_code):
+                data[i.cda_field_code] = [{i.title: i.value}]
+            else:
+                data[i.cda_field_code].append({i.title: i.value})
+            continue
+        if i.cda_group_code and i.value:
+            if not data.get(i.cda_group_code):
+                data[i.cda_group_code] = [{i.title: i.value}]
+            else:
+                data[i.cda_group_code].append({i.title: i.value})
+    temp_result = {}
+    for k, v in data.items():
+        s = ""
+        for j in v:
+            for key, val in j.items():
+                if key:
+                    s = f"{s}{key}: {val};"
+                else:
+                    s = f"{s}{val};"
+        temp_result[k] = s
+    final_result = {str(k): string_to_unicode_escape(v) for k, v in temp_result.items()}
+
+    return {"data": final_result}
+
+
+def string_to_unicode_escape(text):
+    symbols_data = ''.join(f'\\u{ord(char):04x}' for char in text)
+    return symbols_data
+
+def from_escape(escaped_text):
+    return codecs.decode(escaped_text, 'unicode_escape')
