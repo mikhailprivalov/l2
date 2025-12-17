7b75c3cf1c292dc675f858f0a4c29aa920ebaceb
Sergei Kasianenko <41939763+urchinpro@users.noreply.github.com>
2025-12-17 15:23:16 +0800

    Update contracts/models.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- contracts/models.py
+++ contracts/models.py
@@ -54,7 +54,9 @@ class PriceName(models.Model):
 
     @staticmethod
     def get_hospital_price_by_date(hospital_id, date_start, date_end, is_subcontract=False, external_performer=False):
-        return PriceName.objects.filter(hospital_id=hospital_id, date_start__lte=date_start, date_end__gte=date_end, subcontract=is_subcontract, external_performer=external_performer).first()
+        return PriceName.objects.filter(
+            hospital_id=hospital_id, date_start__lte=date_start, date_end__gte=date_end, subcontract=is_subcontract, external_performer=external_performer
+        ).first()
 
     @staticmethod
     def get_hospital_many_prices_by_date(hospital_id, date_start, date_end, is_subcontract=False):
