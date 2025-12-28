df404bb2c2d28d50c73373d7d2566d27b2a2d45d
Sergei Kasianenko <41939763+urchinpro@users.noreply.github.com>
2025-12-28 14:30:38 +0800

    Update results/schema_docx/forms100.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- results/schema_docx/forms100.py
+++ results/schema_docx/forms100.py
@@ -58,7 +58,7 @@ def form_01(direction: Napravleniya, iss: Issledovaniya, fwb, doc, leftnone, use
             "protocol_number": direction.pk,
             "research": iss.research.title,
             "hosp_confirmation": iss.doc_confirmation.hospital.title,
-            "license_data": iss.doc_confirmation.hospital.license_data
+            "license_data": iss.doc_confirmation.hospital.license_data,
         }
         context = {
             **meta_info,
