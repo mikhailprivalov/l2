02cb8642db5dbb6503a9853a8faad1f3bda29c87
sergei kasianenko <monitor84@list.ru>
2026-03-01 21:46:22 +0800

    fix

--- directions/management/commands/lab_result_send_ecp.py
+++ directions/management/commands/lab_result_send_ecp.py
@@ -29,9 +29,9 @@ class Command(BaseCommand):
             date = current_time() + relativedelta(days=-2)
             d_qs = Napravleniya.objects.filter(total_confirmed=True, ecp_direction_number=None, rmis_resend_services=False, last_confirmed_at__gte=date)
         directions = [i.pk for i in d_qs]
-        use_exchange_name = RMQ_AUTH_PARAM.get("lab_exchange_name")
-        use_routing_key = RMQ_AUTH_PARAM.get("lab_routing_key")
         if SettingManager.use_rmq_for_sendlabresultecp():
+            use_exchange_name = RMQ_AUTH_PARAM.get("lab_exchange_name")
+            use_routing_key = RMQ_AUTH_PARAM.get("lab_routing_key")
             for i in directions:
                 broker_publish_msg(i.pk, use_exchange_name=use_exchange_name, use_routing_key=use_routing_key)
                 i.need_resend_ecp = True
