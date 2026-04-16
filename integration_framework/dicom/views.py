d5f558ab0dd740a73256feb27c0ab8d351dbb748
sergei kasianenko <monitor84@list.ru>
2026-04-16 09:40:30 +0800

    fix

--- integration_framework/dicom/views.py
+++ integration_framework/dicom/views.py
@@ -30,7 +30,6 @@ def get_meta_tags(request):
 
 @api_view(['POST'])
 def dcm_order_create(request):
-    body = json.loads(request.body)
     if not hasattr(request.user, "hospitals"):
         return Response({"ok": False, "message": "Некорректный auth токен"})
 
