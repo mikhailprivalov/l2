400dd928033ba8bc272ced65e7e83700b5632f49
Sergei Kasianenko <41939763+urchinpro@users.noreply.github.com>
2026-06-11 13:33:21 +0800

    Update users/admin.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- users/admin.py
+++ users/admin.py
@@ -11,7 +11,7 @@ from .models import (
     DistrictResearchLimitAssign,
     PermissionHospitalProtocolDoctorProfile,
     DoctorProfileEmployeePosition,
-    DoctorProfileEcpPosition
+    DoctorProfileEcpPosition,
 )
 
 
