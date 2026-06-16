18b66276d82710d03dcbc4e0345420ce8b4df62b
sergei kasianenko <monitor84@list.ru>
2026-06-16 14:46:22 +0800

    xml over IF

--- integration_framework/urls.py
+++ integration_framework/urls.py
@@ -39,6 +39,7 @@ urlpatterns = [
     path('get-direction-data-by-period', views.get_direction_data_by_period),
     path('get-directions-data', views.get_direction_data_by_num),
     path('protocol-result', views.get_protocol_result),
+    path('', include('integration_framework.docx_protocol.urls')),
     path('eds/get-user-data', views.eds_get_user_data),
     path('eds/get-cda-data', views.eds_get_cda_data),
     path('external/check-result', views.external_check_result),
