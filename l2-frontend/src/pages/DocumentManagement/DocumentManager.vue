9326c0af0eeca20624942eab9db87374f302940e
sergei kasianenko <monitor84@list.ru>
2026-09-16 20:48:01 +0800

    fix - padding top panel

--- l2-frontend/src/pages/DocumentManagement/DocumentManager.vue
+++ l2-frontend/src/pages/DocumentManagement/DocumentManager.vue
@@ -1,51 +1,49 @@
 <template>
-  <PageInnerLayout>
-    <TwoSidedLayout
-      :left-width-px="leftWidthPx"
-      :min-left-width-px="MIN_LEFT_WIDTH_PX"
-      :min-right-width-px="MIN_RIGHT_WIDTH_PX"
-      resizable
-      @update:left-width-px="onLeftWidthChange"
-    >
-      <template #left>
-        <div class="pane">
-          <DocumentsFilters
-            class="filters-top"
-            part="search"
-            :filter="roleFilter"
-            @update:filter="roleFilter = $event"
-            @search="onSearch"
-          />
-          <DocumentsExplorer
-            class="explorer-fill"
-            :role-filter="roleFilter"
-            :list-refresh="listRefresh"
-            :count-refresh="countRefresh"
-            :found-document="foundDocument"
-            @select="selectedDocumentId = $event"
-            @update:filter="roleFilter = $event"
+  <TwoSidedLayout
+    :left-width-px="leftWidthPx"
+    :min-left-width-px="MIN_LEFT_WIDTH_PX"
+    :min-right-width-px="MIN_RIGHT_WIDTH_PX"
+    resizable
+    @update:left-width-px="onLeftWidthChange"
+  >
+    <template #left>
+      <div class="pane">
+        <DocumentsFilters
+          class="filters-top"
+          part="search"
+          :filter="roleFilter"
+          @update:filter="roleFilter = $event"
+          @search="onSearch"
+        />
+        <DocumentsExplorer
+          class="explorer-fill"
+          :role-filter="roleFilter"
+          :list-refresh="listRefresh"
+          :count-refresh="countRefresh"
+          :found-document="foundDocument"
+          @select="selectedDocumentId = $event"
+          @update:filter="roleFilter = $event"
+        />
+      </div>
+    </template>
+    <template #right>
+      <div class="pane pane-right">
+        <DocumentsFilters
+          class="filters-top"
+          part="filters"
+          :filter="roleFilter"
+          @update:filter="roleFilter = $event"
+        />
+        <div class="viewer">
+          <DocumentViewer
+            :document-id="selectedDocumentId"
+            @visibility-change="onVisibilityChange"
+            @reviewed="onReviewed"
           />
         </div>
-      </template>
-      <template #right>
-        <div class="pane pane-right">
-          <DocumentsFilters
-            class="filters-top"
-            part="filters"
-            :filter="roleFilter"
-            @update:filter="roleFilter = $event"
-          />
-          <div class="viewer">
-            <DocumentViewer
-              :document-id="selectedDocumentId"
-              @visibility-change="onVisibilityChange"
-              @reviewed="onReviewed"
-            />
-          </div>
-        </div>
-      </template>
-    </TwoSidedLayout>
-  </PageInnerLayout>
+      </div>
+    </template>
+  </TwoSidedLayout>
 </template>
 
 <script setup lang="ts">
@@ -54,7 +52,6 @@ import { getCurrentInstance, ref, watch } from 'vue';
 import { useStore } from '@/store';
 import * as actions from '@/store/action-types';
 import api from '@/api';
-import PageInnerLayout from '@/layouts/PageInnerLayout.vue';
 import TwoSidedLayout from '@/layouts/TwoSidedLayout.vue';
 import DocumentsExplorer from '@/pages/DocumentManagement/DocumentsExplorer.vue';
 import DocumentsFilters from '@/pages/DocumentManagement/DocumentsFilters.vue';
